c53af79f238097834f934cdff1895ee1fe10c808
Jesus Gonzalez <gzus@Leonardos-iMac.hsd1.ga.comcast.net>
2021-06-25 10:18:31 -0400

    AT-6298 testing auto lint

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import "@babel/polyfill";
+import "mutationobserver-shim";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+const webApp = createApp(App).use(store).use(router).mount("#app");
