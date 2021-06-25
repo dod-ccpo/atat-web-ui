9025aa6a265cbd618cf67c904de0871798ddb4fc
Tony Sumner <tony.sumner@hunterstrategy.net>
2021-06-25 16:14:47 -0400

    adding theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,36 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
+const vuetifyOpts = {
+  rtl: false,
+  theme: {
+        dark: false,
+        themes: {
+        dark: {
+            primary: "#21CFF3",
+            accent: "#FF4081",
+            secondary: "#ffe18d",
+            success: "#4CAF50",
+            info: "#2196F3",
+            warning: "#FB8C00",
+            error: "#FF5252"
+        },
+        light: {
+            primary: "#1976D2",
+            accent: "#e91e63",
+            secondary: "#30b1dc",
+            success: "#4CAF50",
+            info: "#2196F3",
+            warning: "#FB8C00",
+            error: "#FF5252"
+        }
+        }
+    }
+  }
+  
+
 export default new Vuetify({
+    vuetifyOpts
 });
