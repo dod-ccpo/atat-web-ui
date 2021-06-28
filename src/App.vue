78f16ff393c893fd9c3d61335b201d6e441ad07f
Jesus Gonzalez <jesus.gonzalez-ctr@ccpo.mil>
2021-06-28 19:37:06 -0400

    AT-6301 add footer as an reusable component

--- src/App.vue
+++ src/App.vue
@@ -16,59 +16,23 @@
     <v-main>
       <router-view/>
     </v-main>
-    <v-footer class="app-footer">
-      <v-container>
-        <v-row>
-          <v-col>.</v-col>
-          <v-col class="text-right">
-            Last login: {{ getToday() }}
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-footer>
+    <Footer />
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Footer from './components/Footer.vue';
 
 
 export default Vue.extend({
   name: 'App',
+  components: {
+    Footer,
+  },
   data: () => {
     //
     return {};
   },
-  methods:{
-    getToday(){
-      const today= new Date();
-      console.log(today)
-      return today;
-    },
-  }
 });
 </script>
-
-<style scoped>
-.theme--light.v-footer,
-v-footer.app-footer{
-  z-index: 3;
-  background-color: #fff;
-  border-top: 1px solid #f1f1f1;
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  align-items: center;
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  height: 4rem;
-  color: #323a45;
-  font-size: 1rem;
-  padding: 0 1.2rem;
-}
-
-</style>
