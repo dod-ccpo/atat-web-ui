aa3b21870e04bc33b5d24511c6fa1644c2122348
Jesus Gonzalez <jesus.gonzalez-ctr@ccpo.mil>
2021-06-28 19:12:13 -0400

    AT-6301 add footer

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+/* eslint-disable */
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import vuetify from './plugins/vuetify'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
