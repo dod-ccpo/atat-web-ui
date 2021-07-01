3e67b412550283a78f9dc5997ec5217b04a9f3e1
Jesus Gonzalez <jesus.gonzalez-ctr@ccpo.mil>
2021-07-01 14:24:29 -0400

    AT-6325 create a e2e test with nightwatch

---
+++ tests/e2e/specs/test.js
@@ -0,0 +1,18 @@
+// For authoring Nightwatch tests, see
+// https://nightwatchjs.org/guide
+
+module.exports = {
+  "default e2e tests": (browser) => {
+    browser
+      .init()
+      .waitForElementVisible("#app")
+      .assert.elementPresent("#hello-world-view")
+      .assert.containsText("h1", "Hello World")
+      .assert.elementCount(".v-image__image", 1)
+      .end();
+  },
+
+  // "example e2e test using a custom command": (browser) => {
+  //   browser.openHomepage().assert.elementPresent(".hello").end();
+  // },
+};
