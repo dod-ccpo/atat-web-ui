5dbec5b28c21b90792b523c5e6e6bfd99573308c
Tony Sumner <tony.sumner-ctr@ccpo.mil>
2021-07-02 15:49:36 -0400

    AT-6332 Resolve date in footer

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import moment from 'moment';
 
 Vue.config.productionTip = false;
+Vue.prototype.moment = moment;
 
 new Vue({
   router,
