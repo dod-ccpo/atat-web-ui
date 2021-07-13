f98f60cdb3f764ec3168e1586200a8c4a7a49030
Jesus Gonzalez <jesus.gonzalez-ctr@ccpo.mil>
2021-07-12 21:43:49 -0400

    AT-6300 add the drower for the USWDC disclamer of official goverment sit

--- src/components/ATATHeader.vue
+++ src/components/ATATHeader.vue
@@ -15,20 +15,47 @@
       </v-row>
       <v-row class="USWDC-official-banner ">
         <v-col>
-          <span class="USWDC-official-banner__flag">
-            <img src="../../public/img/us_flag_small.png"
-                 height="11"
-                 width="16"/>
-          </span>
-          <span class="USWDC-official-banner__text">
-            U.S. flag An official website of the United States government
-          </span>
-          <v-btn class="USWDC-official-banner__link blue--text"
-                 x-small
-                 text>
-            <v-icon color="blue" small class="USWDC-official-banner__link_icon" x-small>mdi-help-circle-outline</v-icon>
-            <span class="USWDC-official-banner__link_msg"> Here’s how you know </span>
-          </v-btn>
+          <v-expansion-panels accordion class="USWDC-official-banner_drawer">
+            <v-expansion-panel>
+              <v-expansion-panel-header class="USWDC-official-banner_drawer__header text-left">
+
+                <span class="USWDC-official-banner__flag">
+                  <img src="../../public/img/us_flag_small.png"
+                       height="11"
+                       width="16"/>
+                </span>
+                <span class="USWDC-official-banner__text">
+                  U.S. flag An official website of the United States government
+                </span>
+                <span class="USWDC-official-banner__link blue--text ">
+
+                        <v-icon color="blue" small class="USWDC-official-banner__link_icon" x-small>mdi-help-circle-outline</v-icon>
+                        <span class="USWDC-official-banner__link_msg text-decoration-underline"> Here’s how you know </span>
+                </span>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content class="USWDC-official-banner_drawer__content black white--text">
+                <v-row align="center">
+                  <v-col cols="1">
+                    <img src="../../public/img/icon-dot-gov.svg"
+                         width="30" class="USWDC-official-banner_drawer__content__icon1"/>
+                  </v-col>
+                  <v-col cols="4">
+                      <h3>Official websites use .gov</h3>
+                      <p>A .gov website belongs to an official government organization in the United States.</p>
+                  </v-col>
+                  <v-col cols="1">
+                    <img src="../../public/img/icon-https.svg"
+                         width="30" class="USWDC-official-banner_drawer__content__icon2"/>
+                  </v-col>
+                  <v-col cols="4">
+                    <h3>Official websites use .gov</h3>
+                    <p>A .gov website belongs to an official government organization in the United States.</p>
+                  </v-col>
+                </v-row>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+
+          </v-expansion-panels>
         </v-col>
       </v-row>
       <v-row class="atat-header-nav primary_darken white--text font-weight-bold">
@@ -83,8 +110,30 @@ export default class ATATHeader extends Vue {
      font-size: .7rem;
    }
    .USWDC-official-banner .col{
-     padding: .2rem;
+     padding: 0;
      font-size: .7rem;
+     .USWDC-official-banner_drawer{
+       box-shadow: none;
+       border-radius: 0;
+       .USWDC-official-banner_drawer__header{
+
+         font-size: .7rem;
+         padding: .2rem .5em;
+         min-height: 10px;
+       }
+       .USWDC-official-banner_drawer__content{
+         position: relative;
+         padding-top: 2rem;
+         min-height: 96px;
+         .USWDC-official-banner_drawer__content__icon2,
+         .USWDC-official-banner_drawer__content__icon1 {
+           position: relative;
+           top: -1.5em;
+           right: -2em;
+         }
+       }
+     }
+
      .USWDC-official-banner__flag {
        position: relative;
        top: 3px;
