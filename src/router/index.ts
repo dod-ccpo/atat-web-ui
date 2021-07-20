b138754f7383d59b0b3f1446ae5c63f208bc0612
Jesus Gonzalez <jesus.gonzalez-ctr@ccpo.mil>
2021-07-20 13:26:52 -0400

    AT-6372 routing portfolio defould to step 1

--- src/router/index.ts
+++ src/router/index.ts
@@ -31,6 +31,16 @@ const routes: Array<RouteConfig> = [
     component: () =>
       import(/* webpackChunkName: "style" */ "../views/sample/Style.vue"),
   },
+  {
+    path: "/wizard/step-1",
+    name: "step1",
+    component: () =>
+        import(/* webpackChunkName: "style" */ "../wizard/Step_1/views/Step_1.vue"),
+  },
+  {
+    path: "/portfolios",
+    redirect: { name: 'step1' }
+  },
 ];
 
 const router = new VueRouter({
