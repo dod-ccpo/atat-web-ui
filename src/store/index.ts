8998dfb358c3b01b17c5d673177a82298598b7f2
Jesus Gonzalez <jesus.gonzalez-ctr@ccpo.mil>
2021-07-19 20:27:42 -0400

    AT-6394 making state persistand using local storage and Vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,20 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersist from "vuex-persist";
 
 Vue.use(Vuex);
 
+const vuexLocalStorage = new VuexPersist({
+  key: "vuex", // The key to store the state on in the storage provider.
+  storage: window.localStorage, // or window.sessionStorage or localForage
+  // Function that passes the state and returns the state with only the objects you want to store.
+  // reducer: state => state,
+  // Function that passes a mutation and lets you decide if it should update the state in localStorage.
+  // filter: mutation => (true)
+});
+
 export default new Vuex.Store({
+  plugins: [vuexLocalStorage.plugin],
   state: {
     loginStatus: false,
   },
