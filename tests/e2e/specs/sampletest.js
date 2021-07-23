704b1e396e7bd716c4e7e7c3bba7c4406c6083ff
Prasanthi Alaparthi <prasanthi.alaparthi-ctr@ccpo.mil>
2021-07-23 13:21:26 -0400

    feature/AT-6325-updated the sampletest

--- tests/e2e/specs/sampletest.js
+++ tests/e2e/specs/sampletest.js
@@ -1,15 +1,16 @@
+require('dotenv').config()
 module.exports = {
   "verifying the textfield test": (browser) => {
     browser
       .init()
-      .url("http://localhost:8080/")
+      .url(process.env.E2E_TEST)
       .pause(10000)
       .waitForElementVisible("#app")
-      .assert.elementPresent(".hello-world-view")
       .assert.containsText("h1", "Hello World")
+      .windowMaximize()
       .assert.elementPresent("#plain-text-box_text_field")
       .setValue("#plain-text-box_text_field", "TestPortfolio 12344224200000uniquename")
-      .pause(12000)
+      .pause(10000)
       .assert.value("#plain-text-box_text_field", "TestPortfolio 12344224200000uniquename")
       .assert.elementPresent("#success_text_field")
       .pause(12000)
