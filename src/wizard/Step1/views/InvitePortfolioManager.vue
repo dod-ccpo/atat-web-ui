08e5dfc8fcc5f2741f18f227e8aefcfd5fa392bc
Jesus Gonzalez <jesus.gonzalez-ctr@ccpo.mil>
2021-08-03 16:06:46 -0400

    AT-6380 merge develop

---
+++ src/wizard/Step1/views/InvitePortfolioManager.vue
@@ -0,0 +1,20 @@
+<template>
+  <InvitePortfolioManagersTable>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import CreatePortfolioForm from "../components/CreatePorfolioForm.vue";
+
+@Component({
+  components: {
+    CreatePortfolioForm,
+  },
+})
+export default class Step_1 extends Vue {
+  $refs!: {
+    createPortfolioForm: CreatePortfolioForm;
+  };
+}
+</script>
