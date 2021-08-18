99190268d87e3d52fcfa99c57bc9f8d142236e51
Tony Sumner <tony.sumner-ctr@ccpo.mil>
2021-08-18 18:27:02 -0400

    ATATDatePicker.vue
    [x] styled Datepicker
    [x] added 2 datepickers in menu popup
    [x] [WIP] navigating datepickers left to right
    
    Task: AT-6423

--- src/components/ATATDatePicker.vue
+++ src/components/ATATDatePicker.vue
@@ -6,6 +6,7 @@
         :close-on-content-click="true"
         :nudge-right="40"
         transition="scale-transition"
+        class="two-date-pickers"
         offset-y
         min-width="auto"
       >
@@ -25,17 +26,35 @@
             :success="isFieldValid"
             :error="isFieldValid"
             :height="42"
-            v-model="date"
             readonly
             v-bind="attrs"
             v-on="on"
+            :value="getDate"
           />
         </template>
         <v-date-picker
+          ref="firstMonth"
           :min="minDate"
           :max="maxDate"
-          v-model="date"
+          :showCurrent="false"
+          v-model="selectedDate"
           @input="menu = false"
+          no-title
+          scrollable
+          :reactive="true"
+          :picker-date.sync="firstMonth"
+        />
+        <v-date-picker
+          ref="secondMonth"
+          :min="minDate"
+          :max="maxDate"
+          :show-current="false"
+          v-model="selectedDate"
+          @input="menu = false"
+          no-title
+          :reactive="true"
+          scrollable
+          :picker-date.sync="secondMonth"
         />
       </v-menu>
     </v-col>
@@ -45,10 +64,12 @@
 <script lang="ts">
 import Vue from "vue";
 import moment from "moment";
-import { Component, Prop, PropSync } from "vue-property-decorator";
-import { TaskOrderFile } from "../../types/Wizard";
+import { Component, Prop, PropSync, Watch } from "vue-property-decorator";
 @Component({})
 export default class ATATDatePicker extends Vue {
+
+  
+
   @Prop({ default: "auto" }) private hideDetails!: boolean | string;
   @Prop({ default: true }) private dense!: boolean;
   @Prop({ default: "color" }) private color!: string;
@@ -57,11 +78,100 @@ export default class ATATDatePicker extends Vue {
   @Prop({ default: "Form Field Label" }) private label!: string;
   @Prop({ default: false }) private optional!: boolean;
   @Prop({ default: "" }) private date!: string;
-  @Prop({ default: false }) private menu!: boolean;
 
   private minDate = "2020-09-01";
   private maxDate = "2022-10-31";
+  private menu = false;
+  private firstMonth = moment(new Date()).format("YYYY-MM-DD");
+  private secondMonth = moment(this.firstMonth)
+    .add(1, "M")
+    .format("YYYY-MM-DD");
+  private selectedDate = moment(new Date()).format("YYYY-MM-DD");
   private isFieldValid = false;
+
+  get getDate() : string {
+    return this.selectedDate;
+  }
+
+  private firstMonthComp: any = this.$refs["firstMonth"];
+  private secondMonthComp: any = this.$refs["secondMonth"];
+
+
+ @Watch("firstMonth")
+  protected getFirstMonth (newVal: string, oldVal: string): void {
+    newVal = newVal.length === 7 ? newVal + "-01" : newVal;
+    oldVal = oldVal.length === 7 ? oldVal + "-01" : oldVal;
+    
+    console.log("FirstMonth> new Val" + newVal );
+    console.log("FirstMonth> old Val" + oldVal );
+    
+      this.firstMonth = moment(newVal).format("YYYY-MM-DD");
+    if (newVal !== oldVal){
+      this.secondMonth = moment(oldVal).format("YYYY-MM-DD");
+    }
+    console.log(this.firstMonth);
+    console.log(this.secondMonth);
+  }
+
+// @Watch("secondMonth")
+//   protected getSecondMonth (newVal: string, oldVal: string): void {
+//     newVal = newVal.length === 7 ? newVal + "-01" : newVal;
+//     oldVal = oldVal.length === 7 ? oldVal + "-01" : oldVal;
+    
+//     console.log("secondMonth> new Val" + newVal );
+//     console.log("SecondMonth> old Val" + oldVal );
+    
+//       this.secondMonth = moment(newVal).format("YYYY-MM-DD");
+//     if (oldVal !== this.secondMonth){
+//       this.firstMonth = moment(oldVal).format("YYYY-MM-DD");
+//     }
+//     console.log(this.firstMonth);
+//     console.log(this.secondMonth);
+//   }
+
+
+  // @Watch("secondMonth")
+  // protected getSecondMonth (newVal: string, oldVal: string): void {
+  //   newVal = newVal.length === 7 ? newVal + "-01" : newVal;
+  //   oldVal = oldVal.length === 7 ? oldVal + "-01" : oldVal;
+  //   console.log("secondMOnth" + newVal + ":" + oldVal);
+  //   console.log(this.firstMonth);
+  //   debugger;
+  //   if (moment().month(this.secondMonth).isSame(moment().month(oldVal))){
+  //     this.firstMonth = moment(oldVal).format("YYYY-MM-DD");
+  //   }
+  //   // firstMonthComp.$props["picker-date"] = this.firstMonth;
+  // }
+
+
+  // @Watch("selectedDate")
+  // protected(newVal: string, oldVal: string): void {
+  //   newVal = newVal.length === 7 ? newVal + "-01" : newVal;
+  //   oldVal = oldVal.length === 7 ? oldVal + "-01" : oldVal;
+
+  //   let firstMonthComp: any = this.$refs["firstMonth"];
+  //   let secondMonthComp: any = this.$refs["secondMonth"];
+
+  //   if (newVal < oldVal) {
+  //     this.firstMonth = moment(newVal).format("YYYY-MM-DD");
+  //     this.secondMonth = moment(oldVal).format("YYYY-MM-DD");
+
+  //     // this.selectedDate = moment(new Date()).format("YYYY-MM-DD");
+  //   } else if (newVal > oldVal) {
+  //     this.firstMonth = moment(oldVal).format("YYYY-MM-DD");
+  //     this.secondMonth = moment(newVal).format("YYYY-MM-DD");
+     
+  //   }
+  //   firstMonthComp.$props["picker-date"] = this.firstMonth;
+  //   secondMonthComp.$props["picker-date"] = this.secondMonth;
+
+  //   console.log(this.firstMonth);
+  //   console.log(this.secondMonth);
+  //   // debugger;
+  //   console.log(firstMonthComp);
+  //   console.log(secondMonthComp);
+  // }
+
   private getStatusIcon() {
     // if the rules property isn't set we won't display an icon
     // when the rules property is populated (i.e when the parent form is saved)
