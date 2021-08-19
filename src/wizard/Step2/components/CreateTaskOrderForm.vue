53e32440340f03cfe482c2ac0292ca3d9e1f7fff
robertmccardell-CCPO <robert.mccardell-crt@ccpo.mil>
2021-08-18 20:06:07 -0500

    Revert "Revert "Updates to dropdown""
    This reverts commit 003a1f0f433608a0245c370fa98a69295c86a990.

--- src/wizard/Step2/components/CreateTaskOrderForm.vue
+++ src/wizard/Step2/components/CreateTaskOrderForm.vue
@@ -113,7 +113,7 @@
                 <v-divider class="mt-7"></v-divider>
               </v-col>
             </v-row>
-            <clins-card :value.sync="_clins" />
+            <clins-card-list ref="clinsCards" :clins="_clins"></clins-card-list>
           </div>
         </v-col>
       </v-row>
@@ -182,6 +182,10 @@ export default class CreateTaskOrderForm extends Vue {
         "Please select Yes or No below to verify your Task Order";
     }
     validated.push(this.signedTaskOrder !== "");
+
+    const clinsCards = this.$refs.clinsCards as ClinsCardList;
+
+    validated.push(await clinsCards.validate());
 
     await this.$nextTick(() => {
       validated.push(this.Form.validate());
--- src/wizard/Step2/views/Step2.vue
+++ src/wizard/Step2/views/Step2.vue
@@ -37,12 +37,12 @@ export default class Step_2 extends Vue {
     },
     clins: [
       {
-        clin_number: "",
+        clin_number: "0001",
         idiq_clin: "",
-        total_clin_value: 0,
-        obligated_funds: 0,
-        pop_start_date: "",
-        pop_end_date: "",
+        total_clin_value: 200000,
+        obligated_funds: 10000,
+        pop_start_date: "2021-09-01",
+        pop_end_date: "2022-09-01",
       },
     ],
   };
