3cd6f51c7477750d5aee6f1b442fa2f0b3ba7dea
Jesus Gonzalez <jesus.gonzalez-ctr@ccpo.mil>
2021-08-23 12:52:56 -0400

    Add the basic elements for creating a generic data table base on Vuetify
    as requete on the ticket on comment https://ccpo.atlassian.net/browse/AT-6503?focusedCommentId=319289
    I am creating an generic table object that recives as props Data and sorting
    Set up the data type to match the structure of the defoult V-data-table object on vuetify.
    Crea a simple tamplate that recive this params to create the table
    And add an style file for customize the styling and get it to look as requested.
    
    Status: this changes have not yet been tested and it constitude my firts aproach to creating this feacture.
    
     AT-6503

---
+++ src/components/ATATTable.vue
@@ -0,0 +1,44 @@
+<template>
+    <VDataTable class="ATATTable" 
+        :items="data.items"
+        :headers="data.headers"
+        :must-sort="sorting"
+    >
+
+    </VDataTable>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+import { Component, Prop } from "vue-property-decorator";
+import { VDataTable } from "vuetify/lib/components";
+
+export interface DataTableHeader {
+    text: string,
+    value: string,
+    align?: 'start' | 'center' | 'end',
+    sortable?: boolean,
+    filterable?: boolean,
+    groupable?: boolean,
+    divider?: boolean,
+    class?: string | string[],
+    cellClass?: string | string[],
+    width?: string | number,
+    filter?: (value: any, search: string, item: any) => boolean,
+    sort?: (a: any, b: any) => number
+}
+
+export interface ATATTableData {
+    headers: DataTableHeader;
+    items: [];
+    title: string;
+}
+
+
+@Component({})
+export default class ATATTable extends VDataTable {
+  @Prop({ default: true }) private sorting!: boolean;
+  @Prop({ default: [] }) private data!: ATATTableData;
+
+}
+</script>
