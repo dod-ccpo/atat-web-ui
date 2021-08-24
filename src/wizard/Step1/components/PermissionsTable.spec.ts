3a00b89eb15d73869802234f28fbc2f5b96d4ec9
Jesus Gonzalez <jesus.gonzalez-ctr@ccpo.mil>
2021-08-24 17:30:53 -0400

    Adding Jest Test for PermissionsTable.vue
    - Add a coverage test for the  PermissionsTable.spec.ts
    - Load the wrapper with the props for name and data.
    - Testing that the component displays the name of the application.
    - Testing that the component loads the table with data and at least one of the members.
    
    Status: It passes the PermissionsTable.spec.ts test with 100%. Now I will get some feedback on the styles and get this ready for PR.
    
    feature/AT-6503

--- src/wizard/Step1/components/PermissionsTable.spec.ts
+++ src/wizard/Step1/components/PermissionsTable.spec.ts
@@ -2,3 +2,30 @@ import Vue from "vue";
 import Vuetify from "vuetify";
 import { createLocalVue, mount } from "@vue/test-utils";
 import PermissionsTable from "@/wizard/Step1/components/PermissionsTable.vue";
+import { applicationMembersMock } from "@/store/mocks/portfoliosMockData";
+
+Vue.use(Vuetify);
+
+describe("testing render component", () => {
+  const localVue = createLocalVue();
+  let vuetify: any;
+  let wrapper: any;
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    wrapper = mount(PermissionsTable, {
+      localVue,
+      vuetify,
+      propsData: {
+        data: applicationMembersMock,
+        name: "Global Positioning System",
+      },
+    });
+  });
+
+  it("have the right name", () => {
+    expect(wrapper.html()).toContain("Global Positioning System");
+  });
+  it("have the a member", () => {
+    expect(wrapper.html()).toContain("john.smith@mail.mil");
+  });
+});
