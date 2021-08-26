1cf8abbb97c44a223eb665a5e592f8ecf07487e7
robertmccardell-CCPO <robert.mccardell-crt@ccpo.mil>
2021-08-26 01:43:07 -0500

    stubbing out portfolio validation summary component
    stubbing out portfolio validation summary component

--- src/wizard/Step5/components/PortfolioSummary.vue
+++ src/wizard/Step5/components/PortfolioSummary.vue
@@ -7,11 +7,16 @@
     </v-row>
     <v-row>
       <v-col cols="10">
-        <p class="">
+        <p class="" v-if="!invalidStepsExist">
           In this last step, we will review the information that you provided to
           make sure everything is complete and accurate. Once you have verified
           your Portfolio details, we will be able to provision your cloud
           resources.
+        </p>
+        <!-- Invalid steps found -->
+        <p v-else>
+          We found a few details that you need to review before we can provision
+          your cloud resources. Let's take care of these details now.
         </p>
       </v-col>
     </v-row>
@@ -33,5 +38,9 @@ import SummaryStepper from "./SummaryStepper.vue";
     SummaryStepper,
   },
 })
-export default class PortfolioSummary extends Vue {}
+export default class PortfolioSummary extends Vue {
+  public async invalidStepsExist(): Promise<boolean> {
+    return false;
+  }
+}
 </script>
---
+++ src/wizard/Step5/components/PortfolioValidationSummary.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card></v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component({})
+export default class PortfolioValidationSummary extends Vue {}
+</script>
