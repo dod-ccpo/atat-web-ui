1c75e3abbf33edd38955c2ba3d91188c4c2c637a
Tony Sumner <tony.sumner-ctr@ccpo.mil>
2021-08-28 07:43:48 -0400

    ReviewStepper
    [x] transformed grouped items w/left blue bar from table to divs to
         pass SonarCloud
    
    Task: AT-6402

--- src/wizard/Step5/components/PortfolioSummaryCard.vue
+++ src/wizard/Step5/components/PortfolioSummaryCard.vue
@@ -7,24 +7,18 @@
     </template>
 
     <template slot="summary-body">
-      <table
-        class="
-          body-lg
-          black--text
-          border-0 border-solid border-l-4 border-accent-cool
-          pl-2
-          width-60
-        "
+      <div
+        class="body-lg d-flex justify-start black--text grouped-items"
+        v-for="(item, idx) in items"
+        :key="idx"
       >
-        <tr v-for="(item, idx) in items" :key="idx">
-          <td v-if="item.value" class="pr-4">
-            <strong>{{ item.prefix }}</strong>
-          </td>
-          <td class="text-left" v-if="item.value">
-            {{ item.value }}
-          </td>
-        </tr>
-      </table>
+        <div class="mx-3">
+          <strong>{{ item.prefix }}</strong>
+        </div>
+        <div class="">
+          {{ item.value }}
+        </div>
+      </div>
     </template>
   </summary-card>
 </template>
