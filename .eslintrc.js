4f74c4bbe88dd5d0a879b7ef8acc5edfe6ae29ee
Tony Sumner <tony.sumner-ctr@ccpo.mil>
2021-08-30 08:00:59 -0400

    ReviewStepper - modifed eslintrc.js file
    [x] removed 'prettier' attribute from  eslintrc.js file > rules object
         in the effort to successully build
    
    Task AT-6402

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,12 +21,6 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "valid-typeof": "warn",
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
   },
   overrides: [
     {
