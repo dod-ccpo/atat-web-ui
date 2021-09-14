9296f3e897f91ac92bea8d8decb689ddab814f01
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2021-09-14 12:32:22 -0600

    fixing duplication

--- src/wizard/Step1/components/CloudServiceProviderForm.spec.ts
+++ src/wizard/Step1/components/CloudServiceProviderForm.spec.ts
@@ -1,31 +1,17 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
-import Vuex from "vuex";
 import { createLocalVue, mount } from "@vue/test-utils";
 import CloudServiceProviderForm from "@/wizard/Step1/components/CloudServiceProviderForm.vue";
-import axios from "axios";
-import VueAxios from "vue-axios";
 
 Vue.use(Vuetify);
 
 describe("Testing CloudServiceProviderForm Component", () => {
   const localVue = createLocalVue();
-  localVue.use(Vuex);
-  localVue.use(VueAxios, axios);
   let vuetify: any;
   let wrapper: any;
-  let store: any;
-  const actions: any = {
-    updateWizardStep: jest.fn(),
-  };
-
   beforeEach(() => {
     vuetify = new Vuetify();
-    store = new Vuex.Store({
-      actions,
-    });
     wrapper = mount(CloudServiceProviderForm, {
-      store,
       localVue,
       vuetify,
       stubs: ["atat-button-card"],
--- src/wizard/Step1/components/CreatePortfolioForm.spec.ts
+++ src/wizard/Step1/components/CreatePortfolioForm.spec.ts
@@ -1,36 +1,23 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
-import Vuex from "vuex";
 import { createLocalVue, mount } from "@vue/test-utils";
 import CreatePortfolioForm from "@/wizard/Step1/components/CreatePorfolioForm.vue";
-import axios from "axios";
-import VueAxios from "vue-axios";
 
 Vue.use(Vuetify);
 
 describe("Testing CreatePortfolioForm Component", () => {
   const localVue = createLocalVue();
-  localVue.use(Vuex);
-  localVue.use(VueAxios, axios);
   let vuetify: any;
   let wrapper: any;
-  let store: any;
-  const actions: any = {
-    updateWizardStep: jest.fn(),
-  };
 
   beforeEach(() => {
     vuetify = new Vuetify();
-    store = new Vuex.Store({
-      actions,
-    });
     wrapper = mount(CreatePortfolioForm, {
-      store,
       localVue,
       vuetify,
       stubs: ["atat-text-field", "atat-text-area"],
       propsData: {
-        name: "",
+        name: undefined,
         description: "testDescription",
         dod_components: undefined,
       },
@@ -45,7 +32,14 @@ describe("Testing CreatePortfolioForm Component", () => {
     wrapper.setProps({
       dod_components: ["air force", "army"],
     });
-    wrapper.setData({ rules: { test: ["2"] } });
+    wrapper.setData({ rules: { portfolioName: ["2"] } });
+    expect(wrapper.vm.dod_components).toBeDefined();
+  });
+
+  it("renders dod_components with no rules", async () => {
+    wrapper.setProps({
+      dod_components: ["air force", "army"],
+    });
     expect(wrapper.vm.dod_components).toBeDefined();
   });
 
