10d2d56b17d61164546a60d7c732b24d8f3d8818
Prasanthi Alaparthi <prasanthi.alaparthi-ctr@ccpo.mil>
2021-09-21 12:58:27 -0400

    Removed the hardcoded Url Extended steps to cover all the functionality in the test case

--- tests/e2e/page_tests/test_login.ts
+++ tests/e2e/page_tests/test_login.ts
@@ -1,10 +1,13 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { NightwatchBrowser } from "nightwatch";
 
+const url = process.env.VUE_DEV_SERVER_URL;
+
 const signInPage = {
-  "Verifying the atatSigninpage": (browser: NightwatchBrowser) => {
+  "Verifying the ATAT Signin page 1": (browser: NightwatchBrowser) => {
     const home = browser.page.atatHome();
 
-    home.navigate("http://localhost:8080").pause(10000);
+    home.navigate(url).pause(10000);
     home.expect.element("@classificationBanner").to.be.present.before(1000);
     home.expect
       .element("@classificationBanner_text")
@@ -12,19 +15,56 @@ const signInPage = {
     home.expect
       .element("@classificationBanner_link_msg")
       .text.to.equal("Here’s how you know");
+    home.expect
+      .element("@atatImage")
+      .to.have.attribute("alt")
+      .which.startsWith("A")
+      .and.endsWith("TAT logo");
     home.expect.element("@atatText").text.to.contain("Access the ATAT Cloud");
+    home.expect.element("@atatlogo").to.be.visible;
     home.expect
       .element("@certSelectionText")
       .text.to.contain("Certificate Selection");
+    home.getText(
+      "@certSubText",
+      function (result: { status: any; value: any }) {
+        home.assert.equal(typeof result, "object");
+        home.assert.strictEqual(result.status, 0);
+        const certTextBody =
+          "When you are prompted to select a certificate, please select an\n" +
+          "Authentication (Identification) Certificate from the provided choices.";
+        home.assert.equal(result.value, certTextBody);
+      }
+    );
+    home.expect.element("@lastLoginFooter").to.be.visible;
+    home.getText(
+      "@lastLoginFooter",
+      function (result: { status: number; value: any }) {
+        home.assert.equal(typeof result, "object");
+        home.assert.strictEqual(result.status, 0);
+        console.log(result.value);
+        home.assert.containsText("@lastLoginFooter", "Last login:");
+      }
+    );
+  },
+
+  "Clicking on the Signin Button 2": (browser: NightwatchBrowser) => {
+    const home = browser.page.atatHome();
     home.expect.element("@signInButton").to.be.present.before(2000);
     home.click("@signInButton");
+  },
 
+  "Navigating to landing page which is DashBoard 3": (
+    browser: NightwatchBrowser
+  ) => {
+    const home = browser.page.atatHome();
     const dashboard = browser.page.dashBoard();
+    dashboard.assert.urlContains("dashboard");
     dashboard.expect.element("@logoutTab").to.be.present.before(1000);
     dashboard.getText(
       "@atatHeaderText",
       function (result: { status: any; value: any }) {
-        if (typeof result == "object" && result.status == 0) {
+        if (typeof result === "object" && result.status === 0) {
           home.assert.equal(result.value, "ATAT Cloud Services");
         }
       }
@@ -32,7 +72,12 @@ const signInPage = {
     dashboard.getText(
       "@welcomeSubText",
       function (result: { status: any; value: any }) {
+        home.assert.equal(typeof result, "object");
+        home.assert.strictEqual(result.status, 0);
         console.log(result.value);
+        const WelcomeSubText =
+          "Welcome to the Account Tracking and Automation Tool, Maria!";
+        home.assert.equal(result.value, WelcomeSubText);
       }
     );
     browser.end();
