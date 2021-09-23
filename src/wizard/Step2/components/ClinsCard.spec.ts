91a2550820339beef16399781d7b887da9662473
robertmccardell-CCPO <robert.mccardell-crt@ccpo.mil>
2021-09-22 23:31:13 -0500

    update to clins test
    updates to clins test

--- src/wizard/Step2/components/ClinsCard.spec.ts
+++ src/wizard/Step2/components/ClinsCard.spec.ts
@@ -2,14 +2,34 @@ import Vue from "vue";
 import Vuetify from "vuetify";
 import { createLocalVue, mount } from "@vue/test-utils";
 import ClinsCard from "@/wizard/Step2/components/ClinsCard.vue";
-import { create } from "ts-node";
+import moment from "moment";
 
 Vue.use(Vuetify);
+
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+});
+
+const formatDate = (value: string) => {
+  return moment(new Date(value)).format("MMMM DD, YYYY");
+};
 
 describe("Testing Create ClinsCard Component", () => {
   const localVue = createLocalVue();
   let vuetify: any;
   let wrapper: any;
+
+  const propsData = {
+    card_number: 1,
+    clin_number: "0001",
+    idiq_clin: "IDIQ CLIN 0001 Unclassified IaaS/PaaS",
+    total_clin_value: 200000,
+    obligated_funds: 10000,
+    pop_start_date: "2021-09-01",
+    pop_end_date: "2022-09-01",
+  };
 
   beforeEach(() => {
     vuetify = new Vuetify();
@@ -17,16 +37,10 @@ describe("Testing Create ClinsCard Component", () => {
       localVue,
       vuetify,
       stubs: ["atat-text-field", "atat-select", "atat-date-picker"],
-      propsData: {
-        card_number: 1,
-        clin_number: "0001",
-        idiq_clin: "IDIQ CLIN 0001 Unclassified IaaS/PaaS",
-        total_clin_value: 200000,
-        obligated_funds: 10000,
-        pop_start_date: "2021-09-01",
-        pop_end_date: "2022-09-01",
-      },
+      propsData: propsData,
     });
+
+    wrapper.vm.calculateObligatedPercent();
   });
 
   it("renders successfully", async () => {
@@ -134,9 +148,12 @@ describe("Testing Create ClinsCard Component", () => {
   // pop start rules
   it("pop start rules should not be null or empty", async () => {
     const clinRules = wrapper.vm.popEndRules;
+    wrapper.setProps({pop_start_date: ""});
     expect(clinRules).not.toBeNull();
     expect(clinRules.length).toBe(5);
   });
+
+
   const firstPopStartRule = createTestDescription("1st pop start rule");
   const secondPopStartRule = createTestDescription("2nd pop start rule");
   const thirdPopStartRules = createTestDescription("3rd pop start rule");
@@ -224,6 +241,7 @@ describe("Testing Create ClinsCard Component", () => {
   // pop end rules
   it("pop end rules should not be null or empty", async () => {
     const rules = wrapper.vm.popEndRules;
+    wrapper.setProps({pop_end_date: ""});
     expect(rules).not.toBeNull();
     expect(rules.length).toBe(5);
   });
@@ -307,6 +325,22 @@ describe("Testing Create ClinsCard Component", () => {
     expect(rule).toBe(true);
   });
 
+  
+  // Obligated Funds Rules
+  it("Obligated Funds Rules is not null and has expected rules", async () => {
+    await Vue.nextTick();
+    const rules = wrapper.vm.obligatedFundRules;
+    expect(rules).not.toBe(null);
+    expect(rules).toBeInstanceOf(Array);
+    const rule1 = rules[0]("");
+    expect(rule1).toBe("Please enter your obligated Funds");
+    const rule2 = rules[1]("");
+    expect(rule2).toBe("Please enter a valid number");
+    const rule3 = rules[2](50000000000);
+    expect(rule3).toBe("Obligated Funds cannot exceed total CLIN Values");
+    expect(rules.length).toBe(3);
+  });
+
   // correspondingIDIQRules
   it("corresponding IDIQ Rules is not null", async () => {
     await Vue.nextTick();
@@ -332,22 +366,69 @@ describe("Testing Create ClinsCard Component", () => {
     expect(isValid).toBe(true);
   });
 
-  it("obligated percent is correct", async () => {
-    const obligatedPercent = wrapper.vm.obligatedPercent;
-    console.log(obligatedPercent);
+  it("form exists", async () => {
+    await wrapper.vm.$nextTick();
+    const formWrapper = wrapper.findComponent({ ref: "form" });
+    expect(formWrapper.exists()).toBe(true);
   });
 
-//   it("calculateObligatedPercent is called on update", () => {
-//     // wrapper.vm.calculateObligatedPercent  = jest.fn();
-//     // wrapper.vm.updated();
-//     wrapper.vm.FundFields = {
-//       validate : jest.fn(),
-//     };
+  it("correctly calculates obligated percentage", async () => {
+    wrapper.vm.calculateObligatedPercent();
+    await wrapper.vm.$nextTick();
+    const obligatedPercent = wrapper.vm.obligatedPercent;
+    const percent: number =
+      (propsData.obligated_funds / propsData.total_clin_value) * 100;
+    expect(obligatedPercent).toEqual(
+      Number.parseFloat(percent.toString()).toFixed(2)
+    );
+  });
 
-//     wrapper.setProps({
-//       obligated_funds: 15000,
-//     });
+  it("Card number renders correctly", async () => {
+    await wrapper.vm.$nextTick();
+    const card_number = wrapper.find("#card_number");
+    expect(card_number.text()).toBe(`${propsData.card_number}`);
+  });
 
-//     expect( wrapper.vm.FundFields.validate).toHaveBeenCalled();
+  it("IDIQ Clin type renders correctly", async () => {
+    await wrapper.vm.$nextTick();
+    const idiq_clin = wrapper.find("#idiq_clin");
+    expect(idiq_clin.text()).toBe(propsData.idiq_clin);
+  });
+
+  it("Total Clin Value renders correctly", async () => {
+    await wrapper.vm.$nextTick();
+    const total_clin_value = wrapper.find("#total_clin_value");
+    const formatedValue = formatter.format(propsData.total_clin_value);
+    expect(total_clin_value.text()).toBe(formatedValue);
+  });
+
+  it("Obligated funds Value renders correctly", async () => {
+    await wrapper.vm.$nextTick();
+    const obligated_funds = wrapper.find("#obligated_funds");
+    const formatedValue = formatter.format(propsData.obligated_funds);
+    expect(obligated_funds.text()).toBe(formatedValue);
+  });
+
+  it("Period of Performance Value renders correctly", async () => {
+    await wrapper.vm.$nextTick();
+    const period_of_performance = wrapper.find("#period_of_performance");
+    const formatedValue = `${formatDate(
+      propsData.pop_start_date
+    )} - ${formatDate(propsData.pop_end_date)}`;
+
+    expect(period_of_performance.text()).toBe(formatedValue);
+  });
+
+  it("Clin Number Value renders correctly", async () => {
+    await wrapper.vm.$nextTick();
+    const clin_number = wrapper.find("#clin_number");
+    expect(clin_number.text()).toBe(`CLIN ${propsData.clin_number}`);
+  });
+
+//   it("edit dialog exists", async () => {
+//     await wrapper.vm.$nextTick();
+//     const modals = wrapper.findAll("v-dialog");
+//     expect(modals.length).toBe(1);
 //   });
+
 });
