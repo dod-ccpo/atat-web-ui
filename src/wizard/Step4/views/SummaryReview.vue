05963e2cde94a1a6ff06477fea397b96360bb03c
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2021-09-29 16:36:23 -0600

    added route for portfolio summary page

---
+++ src/wizard/Step4/views/SummaryReview.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container fluid>
+    <h2>Summary Review</h2>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component({})
+export default class SummaryReview extends Vue {
+
+}
+</script>
+
+<style scoped></style>
