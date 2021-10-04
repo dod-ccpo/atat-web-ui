f19c1ef04b2085b5ba6ef693b06210cf1f2bbec0
robertmccardell-CCPO <robert.mccardell-crt@ccpo.mil>
2021-10-04 14:43:50 -0500

    clean up

--- src/api/portfolios/index.ts
+++ src/api/portfolios/index.ts
@@ -187,47 +187,4 @@ export default class PortfolioDraftsApi {
     }
     return null;
   }
-
-  // private mapPortfolio(item: any): Portfolio {
-  //   const mapTaskOrder = (taskOrderItem: any): TaskOrderDetails => {
-  //     if (taskOrderItem) {
-  //       const taskOrderFile: TaskOrderFile = {
-  //         id: taskOrderItem.id || "-1",
-  //         name: taskOrderItem.name || "",
-  //         // description: taskOrderItem.description || "",
-  //         created_at: "",
-  //         updated_at: "",
-  //         size: 20000,
-  //         status: "",
-  //       };
-
-  //       const taskOrder: TaskOrderDetails = {
-  //         task_order_number: taskOrderItem.task_order_number,
-  //         clins: taskOrderItem.clins,
-  //         task_order_file: taskOrderFile,
-  //       };
-
-  //       return taskOrder;
-  //     }
-
-  //     throw new Error("invalid item");
-  //   };
-
-  //   const portfolio: Portfolio = {
-  //     id: item.id,
-  //     description: item.portfolio_step ? item.portfolio_step.description : "",
-  //     name: item.portfolio_step ? item.portfolio_step.name : "Untitled",
-  //     dod_component: item.portfolio_step
-  //       ? item.portfolio_step.dod_components
-  //       : [],
-  //     csp_provisioning_status: item.status,
-  //     portfolio_managers: item.portfolio_step
-  //       ? item.portfolio_step.portfolio_managers
-  //       : [],
-  //     taskOrders: item.funding_step ? [mapTaskOrder(item.funding_step)] : [],
-  //     applications: [],
-  //   };
-
-  //   return portfolio;
-  // }
 }
