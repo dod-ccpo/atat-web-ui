1df545417fc1cf3c238356962c9507eb3e0d4c07
Eric Youngquist <eric.youngquist-ctr@ccpo.mil>
2021-10-06 13:25:31 -0400

    Improved 508, started componentizing modal

---
+++ src/wizard/Step4/components/AddMembers.vue
@@ -0,0 +1,368 @@
+<template>
+  <v-dialog
+    v-model="_dialog_open"
+    persistent
+    max-width="632px"
+  >
+    <v-card class="extra-padding">
+      <div id="inputWidthFaker" ref="inputWidthFaker"></div>
+      <v-card-title>
+        <h2 class="mb-2">Add team members to Tracker Application</h2>
+      </v-card-title>
+      <v-card-text
+        class="body-lg text--base-darkest"
+      >
+        <p>
+          Team members can have different levels of access to your application
+          and environments. Invite multiple people with the same permissions at once.
+        </p>
+
+        <strong id="PillboxLabel">Email Addresses</strong>
+
+        <div
+          class="error--text mb-2"
+          :class="{ 'd-flex': invalidEmailCount }"
+          v-show="invalidEmailCount"
+        >
+
+          <div class="v-messages__message mr-2 d-inline-block">
+            {{ invalidEmailCount }} error<span v-if="invalidEmailCount > 1">s</span>.
+          </div>
+
+          <v-btn
+            id="RemoveAllInvalidEntriesLink"
+            class="link-button pa-0"
+            @click="removeInvalidEmails"
+            style="height: 26px;"
+          >
+            Remove all emails with errors
+          </v-btn>
+
+        </div>
+        <div
+          id="PillboxWrapper"
+          aria-labelledby="PillboxLabel"
+          aria-describedby="PillboxInstructions"
+          class="pa-2 pillbox-wrapper mb-0"
+          tabindex="0"
+          :class="[ pillboxFocused ? 'focused' : '']"
+          @click="addEmail"
+          @focus="addEmail"
+        >
+          <v-text-field
+            v-for="member in memberList"
+            :key="member.id"
+            class="pill"
+            :class="{ 'invalid-entry': !member.isValid && member.isValid !== null }"
+            :data-member-id="member.id"
+            v-model="member.email"
+            append-icon="close"
+            @click="emailEdit"
+            @blur="emailBlurred"
+            @click:append="removeEmail"
+          />
+        </div>
+        <div class="dupe-entry-alert-wrapper">
+          <v-alert
+            v-if="duplicatedEmail"
+            class="dupe-entry-alert"
+            color="#1b1b1b"
+            dark
+            icon="error"
+            dense
+          >
+            &ldquo;{{ duplicatedEmail }}&rdquo; has already been entered.
+          </v-alert>
+
+        </div>
+        <span class="color-base" id="PillboxInstructions">
+          Must use a .mil email address. Separate multiple emails with commas.
+        </span>
+
+        <v-alert
+          v-show="invalidEmailCount"
+          outlined
+          rounded
+          color="error"
+          border="left"
+          icon="error"
+          class="text-left error_lighter black-icon mt-3"
+        >
+          <p class="black--text body-lg">
+            <span v-if="invalidEmailCount === 1">
+              The address &ldquo;{{ invalidEmail }}&rdquo; was not recognized.
+            </span>
+            <span v-if="invalidEmailCount > 1">
+              Multiple addresses were not recognized.
+            </span>
+            Please make sure that all addresses are properly formatted and .mil addresses.
+          </p>
+        </v-alert>
+
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          text
+          class="link-button"
+          @click="_dialog_open = false"
+        >
+          Cancel
+        </v-btn>
+        <v-btn
+          color="primary"
+          class="px-5"
+          @click="saveToStore"
+          :disabled="invalidEmailCount > 0 || validEmailCount === 0"
+        >
+          Add Team Members
+          <span
+            class="valid-entry-count ml-2"
+            v-if="validEmailCount"
+          >
+            {{ validEmailCount }}
+          </span>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop, PropSync } from "vue-property-decorator";
+
+@Component({})
+
+export default class AddMember extends Vue {
+
+  private pillboxFocused = false;
+
+  private duplicatedEmail: string = "";
+  private memberList: {
+    id: number,
+    email: string,
+    display_name: "",
+    access: "",
+    isValid: boolean | null,
+    isDuplicate: boolean,
+  }[] = [];
+  private validEmailList: string[] = [];
+
+  @PropSync("dialogOpen") _dialog_open!: boolean;
+
+  get inputWidthFaker() {
+    return this.$refs.inputWidthFaker as HTMLElement;
+  }
+
+  get validEmailCount(): number {
+    return this.memberList.filter((obj) => obj.isValid === true).length;
+  }
+
+  get validEmails(): string[] {
+    return this.memberList.filter(obj => obj.isValid).map(obj => obj.email);
+  }
+
+  get invalidEmailCount(): number {
+    return this.memberList.filter((obj) => obj.isValid === false).length;
+  }
+
+  get invalidEmail(): string {
+    const invalidEmails = this.memberList.filter(obj => {
+      return obj.isValid === false;
+    });
+    return invalidEmails.length ? invalidEmails[0].email : '';
+  }
+
+  get duplicateEmailCount(): number {
+    return this.memberList.filter((obj) => obj.isDuplicate === true).length;
+  }
+
+  public async mounted(): Promise<void> {
+    this.$store.dispatch("saveStepModel", [{}, 4, true]);
+  }
+
+  public addEmail(e: Event, override: boolean | null): void {
+    const targetElement = e.target;
+    const targetId = targetElement ? (targetElement as HTMLDivElement).id : "";
+    this.pillboxFocused = true;
+    let len = this.memberList.length;
+    if ((targetId === "PillboxWrapper" || override === true) &&
+      (len === 0 || this.memberList[len - 1].email !== "")) 
+    {
+      const memberId = Date.now();
+      this.memberList.push({
+        id: memberId,
+        email: "",
+        display_name: "",
+        access: "",
+        isValid: null,
+        isDuplicate: false,
+      });
+      this.$nextTick().then(() => {
+        let newInput = document.querySelector("[data-member-id='" + memberId + "']") as HTMLInputElement;
+        newInput.style.width = "40px"
+        newInput?.focus();
+        this.addInputEventListeners(this, newInput);
+      });
+    }
+  }
+
+  public removeEmail(e: Event) {
+    this.pillboxFocused = false;
+    const thisButton = e.target as HTMLButtonElement;
+    const closestElement = thisButton.closest('.v-input__slot') as HTMLElement;
+    const input = closestElement.querySelector('input[type=text]') as HTMLInputElement;
+    const i = this.validEmailList.indexOf(input.value);
+    if (i > -1) {
+      this.validEmailList.splice(i, 1);
+    }
+    const memberId = Number(input.dataset.memberId);
+    this.removeMemberFromList(memberId);
+    this.setInputWidths();
+  }
+
+  public removeInvalidEmails() {
+    this.memberList =  this.memberList.filter(obj => {
+      return obj.isValid === true;
+    });
+  }
+
+  public addInputEventListeners(vm:any, input: HTMLInputElement) {
+
+    input.addEventListener("input", () => {
+      this.inputWidthFaker.innerHTML = input.value;
+      const w = this.inputWidthFaker.offsetWidth + "px";
+      input.style.width = w;
+
+      this.duplicatedEmail = this.validEmailList.indexOf(input.value) > -1 ? input.value : "";
+    });
+
+    input.addEventListener("keydown", (e) => {
+      const keypressed:string = e.key;
+      const actionKeys:string[] = [" ", ",", ";", "Enter",];
+      if (actionKeys.indexOf(keypressed) > -1) {
+        e.preventDefault();
+        e.cancelBubble = true;
+        input.blur();
+        setTimeout(() => {
+          this.addEmail(new CustomEvent(""), true);
+        }, 0);
+      } else {
+        // for 508 compliance...
+        // if shift-tabbing out of first pill input, and have invalid emails,
+        // tab to the "Remove all emails with errors" link
+        const memberId:number = Number(input.dataset.memberId);
+        const memberListIndex = this.memberList.map(function(e) { return e.id; }).indexOf(memberId);
+        if (memberListIndex === 0 && keypressed === "Tab" && e.shiftKey && this.invalidEmailCount) {
+          const tabToLink = document.getElementById("RemoveAllInvalidEntriesLink") as HTMLButtonElement;
+          e.preventDefault();
+          e.cancelBubble = true;
+          tabToLink.focus();
+        }
+      }
+    });
+
+    input.addEventListener("paste", function(e: ClipboardEvent) {
+      e.preventDefault();
+      const {clipboardData} = e;
+      let pastedText = clipboardData ? clipboardData.getData('text/plain') : "";
+      pastedText = pastedText.replace(/['"\s]/g, "");
+      pastedText = pastedText.replace(/;/g, ",");
+
+      const pastedValuesArray:string[] = pastedText.split(",");
+      let uniqueValues = [...new Set(pastedValuesArray)]
+      const timeStamp = Date.now();
+      uniqueValues.forEach((email, i) => {
+        const validListIndex = vm.validEmailList.indexOf(email);
+        const isValid = vm.validateEmail(email);
+        if (email && isValid && validListIndex === -1) {
+          vm.validEmailList.push(email);
+          const memberId = timeStamp + i;
+          vm.memberList.push({
+            id: memberId, 
+            email: email, 
+            display_name: "", 
+            access: "",
+            isValid: isValid,
+            isDuplicate: false, // address this when comparing to existing members
+          });
+        }
+      });
+      input.blur();
+    });
+  }
+
+  public emailEdit(e: Event) {
+    e.preventDefault();
+    e.cancelBubble = true;
+    const input = e.currentTarget as HTMLInputElement;
+    const i = this.validEmailList.indexOf(input.value);
+    if (i > -1) {
+      this.validEmailList.splice(i, 1);
+    }
+    this.pillboxFocused = true;
+    this.addInputEventListeners(this, input);
+  }
+
+  public emailBlurred(e: Event) {
+    e.preventDefault();
+    e.cancelBubble = true;
+    this.pillboxFocused = false;
+    const input = e.target as HTMLInputElement;
+    const memberId:number = Number(input.dataset.memberId);
+    let emailAddressEntered = input.value;
+    emailAddressEntered = emailAddressEntered.replace(/['"]/g, "");
+
+    if (emailAddressEntered.length) {
+      const memberListIndex = this.memberList.map(function(e) { return e.id; }).indexOf(memberId);
+      const isValid = this.validateEmail(emailAddressEntered);
+      this.memberList[memberListIndex].isValid = isValid;
+
+      if (emailAddressEntered === this.duplicatedEmail) {
+        this.memberList.splice(memberListIndex, 1);
+        this.duplicatedEmail = "";
+      } else {
+        this.memberList[memberListIndex].email = emailAddressEntered;
+        if (isValid) {
+          this.validEmailList.push(emailAddressEntered);
+        }
+      }
+
+      this.inputWidthFaker.innerHTML = emailAddressEntered;
+      const w = this.inputWidthFaker.offsetWidth + "px";
+      input.style.width = w;
+
+    } else {
+      this.removeMemberFromList(memberId);
+      this.setInputWidths();
+    }
+  }
+
+  public setInputWidths() {
+    this.memberList.forEach(member => {
+      this.inputWidthFaker.innerHTML = member.email;
+      const w = this.inputWidthFaker.offsetWidth + "px";
+      const emailInput = document.querySelector("[data-member-id='" + member.id + "']")  as HTMLElement;
+      emailInput.style.width = w;
+    }, this);
+  }
+
+  public validateEmail(email: string) {
+    const isMilAddress = email.slice(-3) === "mil";
+    const emailRegex = /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/;
+    return isMilAddress && emailRegex.test(email);
+  }
+
+  public removeMemberFromList(memberId: number) {
+    this.memberList = this.memberList.filter(function( obj ) {
+      return obj.id !== memberId;
+    });
+  }
+
+  public saveToStore() {
+    console.log('save member data to store...');
+  }
+}
+
+</script>
--- src/wizard/Step4/views/Step4.vue
+++ src/wizard/Step4/views/Step4.vue
@@ -2,118 +2,6 @@
 
   <v-container fluid>
     <v-row>
-
-      <div id="inputWidthFaker" ref="inputWidthFaker"></div>
-      <v-dialog
-        v-model="dialogOpen"
-        persistent
-        max-width="632px"
-      >
-        <v-card>
-          <v-card-title>
-            <h2 class="mb-2">Add team members to Tracker Application</h2>
-          </v-card-title>
-          <v-card-text
-            class="body-lg text--base-darkest"
-          >
-            <p>
-              Team members can have different levels of access to your application
-              and environments. Invite multiple people with the same permissions at once.
-            </p>
-
-            <strong id="EmailInputLabel">Email Addresses</strong>
-            <div class="error--text" v-if="invalidEmailCount">
-              <div class="v-messages__message mr-2 d-inline-block">
-                {{ invalidEmailCount }} error<span v-if="invalidEmailCount > 1">s</span>
-              </div>
-              <a tabindex="0" @click="removeInvalidEmails">Remove all emails with errors</a>
-            </div>
-            <div
-              id="EmailInputWrapper"
-              aria-labelledby="EmailInputLabel"
-              class="pa-2 email-wrapper mb-0"
-              :class="[ emailInputFocused ? 'focused' : '']"
-              @click="addEmail"
-            >
-              <v-text-field
-                v-for="member in memberList"
-                :key="member.id"
-                class="pill"
-                :class="{ 'email-invalid': !member.isValid && member.isValid !== null }"
-                :data-member-id="member.id"
-                v-model="member.email"
-                append-icon="close"
-                @click="emailEdit"
-                @blur="emailBlurred"
-                @click:append="removeEmail"
-              />
-            </div>
-            <div class="dupe-email-alert-wrapper">
-              <v-alert
-                v-if="duplicatedEmail"
-                class="dupe-email-alert"
-                color="#1b1b1b"
-                dark
-                icon="error"
-                dense
-              >
-                &ldquo;{{ duplicatedEmail }}&rdquo; has already been entered.
-              </v-alert>
-
-            </div>
-            <span class="color-base">
-              Must use a .mil email address. Separate multiple emails with commas.
-            </span>
-
-            <v-alert
-              v-show="invalidEmailCount"
-              outlined
-              rounded
-              color="error"
-              border="left"
-              icon="error"
-              class="text-left error_lighter black-icon mt-3"
-            >
-              <p class="black--text body-lg">
-                <span v-if="invalidEmailCount === 1">
-                  The address &ldquo;{{ invalidEmail }}&rdquo; was not recognized.
-                </span>
-                <span v-if="invalidEmailCount > 1">
-                  Multiple addresses were not recognized.
-                </span>
-                Please make sure that all addresses are properly formatted and .mil addresses.
-              </p>
-            </v-alert>
-
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              text
-              class="link-button"
-              @click="dialogOpen = false"
-            >
-              Cancel
-            </v-btn>
-            <v-btn
-              color="primary"
-              class="px-5"
-              @click="dialogOpen = false"
-              :disabled="invalidEmailCount > 0 || validEmailCount === 0"
-            >
-              Add Team Members
-              <span
-                class="valid-email-count ml-2" 
-                v-if="validEmailCount"
-              >
-                {{ validEmailCount }}
-              </span>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-
-
       <v-col class="pl-0" cols="12">
         <h2 v-if="!createdApplication" class="h2">
           Invite team members to your application
@@ -346,6 +234,11 @@
         </v-row>
       </v-col>
     </v-row>
+
+    <add-members
+      :dialogOpen.sync="dialogOpen"
+    />
+
   </v-container>
 
 </template>
@@ -353,12 +246,15 @@
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+import AddMembers from "@/wizard/Step4/components/AddMembers.vue";
 
-@Component({})
+@Component({
+  components: {
+    AddMembers
+  }
+})
 export default class Step_4 extends Vue {
 
-  private dialogOpen = false;
-  private emailInputFocused = false;
   private csp =
     this.$store.state.portfolioSteps[0].model.csp ||
     "the selected Cloud Service Provider’s";
@@ -372,344 +268,14 @@ export default class Step_4 extends Vue {
     console.log("clicked");
   }
 
-  private search = ""; //sync search
-
-  private duplicatedEmail: string = "";
-  private memberList: {
-    id: number,
-    email: string,
-    display_name: "",
-    access: "",
-    isValid: boolean | null,
-    isDuplicate: boolean,
-  }[] = [];
-  private validEmailList: string[] = [];
-
-  get inputWidthFaker() {
-    return this.$refs.inputWidthFaker as HTMLElement;
-  }
-
-  get validEmailCount(): number {
-    return this.memberList.filter((obj) => obj.isValid === true).length;
-  }
-
-  get validEmails(): string[] {
-    return this.memberList.filter(obj => obj.isValid).map(obj => obj.email);
-  }
-
-  get invalidEmailCount(): number {
-    return this.memberList.filter((obj) => obj.isValid === false).length;
-  }
-
-  get invalidEmail(): string {
-    const invalidEmails = this.memberList.filter(obj => {
-      return obj.isValid === false;
-    });
-    return invalidEmails.length ? invalidEmails[0].email : '';
-  }
-
-  get duplicateEmailCount(): number {
-    return this.memberList.filter((obj) => obj.isDuplicate === true).length;
-  }
-
-  public async mounted(): Promise<void> {
-    this.$store.dispatch("saveStepModel", [{}, 4, true]);
-  }
-
-  public addEmail(e: Event, override: boolean | null): void {
-    const targetElement = e.target;
-    const targetId = targetElement ? (targetElement as HTMLDivElement).id : "";
-    debugger;
-    this.emailInputFocused = true;
-    let len = this.memberList.length;
-    if ((targetId === "EmailInputWrapper" || override === true) && 
-      (len === 0 || this.memberList[len - 1].email !== "")) 
-    {
-      const memberId = Date.now();
-      this.memberList.push({
-        id: memberId,
-        email: "",
-        display_name: "",
-        access: "",
-        isValid: null,
-        isDuplicate: false,
-      });
-      const self = this;
-      this.$forceUpdate();
-      this.$nextTick().then(function () {
-        let newInput = document.querySelector("[data-member-id='" + memberId + "']") as HTMLInputElement;
-        newInput.style.width = "40px"
-        newInput?.focus();
-        self.addInputEventListeners(self, newInput);
-      });
-    }
-  }
-
-  public removeEmail(e: Event) {
-    this.emailInputFocused = false;
-    const thisButton = e.target as HTMLButtonElement;
-    const closestElement = thisButton.closest('.v-input__slot') as HTMLElement;
-    const input = closestElement.querySelector('input[type=text]') as HTMLInputElement;
-    const i = this.validEmailList.indexOf(input.value);
-    if (i > -1) {
-      this.validEmailList.splice(i, 1);
-    }
-    const memberId = Number(input.dataset.memberId);
-    this.removeMemberFromList(memberId);
-    this.setInputWidths();
-  }
-
-  public removeInvalidEmails() {
-    this.memberList =  this.memberList.filter(obj => {
-      return obj.isValid === true;
-    });
-  }
-
-  public addInputEventListeners(vm:any, input: HTMLInputElement) {
-
-    input.addEventListener('input', () => {
-      this.inputWidthFaker.innerHTML = input.value;
-      const w = this.inputWidthFaker.offsetWidth + "px";
-      input.style.width = w;
-
-      this.duplicatedEmail = this.validEmailList.indexOf(input.value) > -1 ? input.value : "";
-    });
-
-    input.addEventListener('keydown', (e) => {
-      const keypressed:string = e.key;
-      const actionKeys:string[] = [" ", ",", ";", "Enter",];
-      if (actionKeys.indexOf(keypressed) > -1) {
-        e.preventDefault();
-        e.cancelBubble = true;
-        input.blur();
-        setTimeout(() => {
-          this.addEmail(new CustomEvent(""), true);
-        }, 0);
-      }
-    });
-
-    input.addEventListener("paste", function(e: ClipboardEvent) {
-      e.preventDefault();
-      const {clipboardData} = e;
-      let pastedText = clipboardData ? clipboardData.getData('text/plain') : '';
-      pastedText = pastedText.replace(/['"\s]/g, "");
-      pastedText = pastedText.replace(/;/g, ",");
-
-      const pastedValuesArray:string[] = pastedText.split(",");
-      let uniqueValues = [...new Set(pastedValuesArray)]
-      const timeStamp = Date.now();
-      uniqueValues.forEach((email, i) => {
-        const validListIndex = vm.validEmailList.indexOf(email);
-        const isValid = vm.validateEmail(email);
-        if (email && isValid && validListIndex === -1) {
-          vm.validEmailList.push(email);
-          const memberId = timeStamp + i;
-          vm.memberList.push({
-            id: memberId, 
-            email: email, 
-            display_name: "", 
-            access: "",
-            isValid: isValid,
-            isDuplicate: false, // address this
-          });
-        }
-      });
-      input.blur();
-    });
-  }
-
-  public emailEdit(e: Event) {
-    e.preventDefault();
-    e.cancelBubble = true;
-    const input = e.currentTarget as HTMLInputElement;
-    const i = this.validEmailList.indexOf(input.value);
-    if (i > -1) {
-      this.validEmailList.splice(i, 1);
-    }
-    this.emailInputFocused = true;
-    this.addInputEventListeners(this, input);
-  }
-
-  public emailBlurred(e: Event) {
-    e.preventDefault();
-    e.cancelBubble = true;
-    this.emailInputFocused = false;
-    const input = e.target as HTMLInputElement;
-    const memberId:number = Number(input.dataset.memberId);
-    let emailAddressEntered = input.value;
-    emailAddressEntered = emailAddressEntered.replace(/['"]/g, "");
-
-    if (emailAddressEntered.length) {
-      const memberListIndex = this.memberList.map(function(e) { return e.id; }).indexOf(memberId);
-      const isValid = this.validateEmail(emailAddressEntered);
-      this.memberList[memberListIndex].isValid = isValid;
-
-      if (emailAddressEntered === this.duplicatedEmail) {
-        this.memberList.splice(memberListIndex, 1);
-        this.duplicatedEmail = "";
-      } else {
-        this.memberList[memberListIndex].email = emailAddressEntered;
-        if (isValid) {
-          this.validEmailList.push(emailAddressEntered);
-        }
-      }
-
-      this.inputWidthFaker.innerHTML = emailAddressEntered;
-      const w = this.inputWidthFaker.offsetWidth + "px";
-      input.style.width = w;
-
-    } else {
-      this.removeMemberFromList(memberId);
-      this.setInputWidths();
-    }
-  }
+  private dialogOpen = false;
 
   public openModal() {
     this.dialogOpen = true;
-    this.setInputWidths();
-  }
-
-  public setInputWidths() {
-    const self = this;
-    this.memberList.forEach(member => {
-      self.inputWidthFaker.innerHTML = member.email;
-      const w = self.inputWidthFaker.offsetWidth + "px";
-      const emailInput = document.querySelector("[data-member-id='" + member.id + "']")  as HTMLElement;
-      emailInput.style.width = w;
+    this.$nextTick().then(() => {
+      const pillboxWrapper = document.getElementById("PillboxWrapper") as HTMLDivElement;
+      pillboxWrapper.focus();
     });
   }
-
-  public validateEmail(email: string) {
-    const isMilAddress = email.slice(-3) === "mil";
-    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return isMilAddress && emailRegex.test(email);
-  }
-
-  public removeMemberFromList(memberId: number) {
-    this.memberList = this.memberList.filter(function( obj ) {
-      return obj.id !== memberId;
-    });
-  }
-
 }
 </script>
-
-<style lang="scss">
-  .v-btn.primary.theme--light span.valid-email-count {
-    background-color: white;
-    color: #005EA2; // $primary
-    border-radius: 20px;
-    padding: 0 7px;
-    &:hover, &:focus, &:active {
-      background-color: white !important;
-    }
-  }
-  .v-card__title {
-    padding: 24px 40px 0 !important;
-    h2 {
-      font-size: 24px;
-    }
-  }
-
-  .v-card__text {
-    padding: 0 40px 24px !important;
-  }
-
-  .v-card__actions {
-    background-color: #f0f0f0; // $base_lightest
-    padding: 16px 24px !important;
-  }
-
-  .v-alert .v-alert__icon.v-icon {
-    font-size: 20px;
-    top: 0;
-  }
-
-  div#inputWidthFaker {
-    display: inline-block;
-    font-size: 16px;
-    position: absolute;
-    left: -10000px;
-    top: -10000px;
-  }
-
-  .email-wrapper {
-    border:1px solid #565C65; // $base_dark
-    height: 118px;
-    margin-bottom: 5px;
-    width: 100%;
-    max-width:550px;
-    overflow-y: auto;
-    overflow-x: hidden;
-    position: relative;
-    &.focused {
-      border-color: #005EA2; // $primary
-      outline: 2px solid #005ea2; // $primary
-    }
-  }
-
-  .dupe-email-alert-wrapper {
-    position: relative;
-    .dupe-email-alert {
-      position: absolute;
-      top: -26px;
-      left: 1px;
-      right: 2px;
-      padding: 0;
-      border-bottom-right-radius: 0 !important;
-      border-bottom-left-radius: 0 !important;
-      font-size: 14px !important;
-      z-index: 10;
-    }
-  }
-
-  .pill.v-text-field {
-    display: inline-block;
-    height: 32px;
-    border: 1px solid #D9E8F6;
-    border-radius: 15px;
-    background-color: #D9E8F6;
-    padding: 0 4px 0 12px;
-    line-height: 22px;
-    margin: 3px;
-    position: relative;
-    z-index: 2;
-    border: 1px solid transparent;
-    &.email-invalid:not(.v-input--is-focused) {
-      background-color: #F8DFE2;
-      border-color: #e21c3d;
-      padding-left: 30px;
-      &:before {
-        font-family: 'Material Icons';
-        content: "error";
-        -webkit-font-feature-settings: 'liga';
-        color: #e21c3d;
-        position: absolute;
-        top: 4px;
-        left: 6px;
-        font-size: 20px;
-      }
-    }
-
-    input {
-      height: 32px;
-      max-height: 32px;
-      line-height: 32px;
-      padding: 0;
-    }
-
-    &.v-input--is-focused {
-      background-color: transparent;
-      .v-input__append-inner {
-        opacity: 0;
-      }
-    }
-    .v-input__slot {
-      &:before, &:after {
-        display: none;
-      }
-    }
-
-  }
-
-</style>
