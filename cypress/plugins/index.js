d076ca28854f076e4b7bfe060ca91fe4bfc00a15
Prasanthi Alaparthi(Hunter Strategy) <81385743+palaparthi-ccpo@users.noreply.github.com>
2021-10-08 15:57:43 -0400

    Update cypress/plugins/index.js
    Co-authored-by: Kyle Laker (Hunter Strategy) <kyle.laker-ctr@ccpo.mil>

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,8 +14,8 @@
 
 /**
  * @type {Cypress.PluginConfig}
- /* eslint-disable @typescript-eslint/no-var-requires */
-require('dotenv').config()
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config()
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
