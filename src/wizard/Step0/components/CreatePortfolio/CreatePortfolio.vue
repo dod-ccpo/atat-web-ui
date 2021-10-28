a2485b535d6e602bb9a8db8f68ef3bc468e0d0d1
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2021-10-28 09:29:39 -0600

    fixing padding for createPortfolio Dashboard

--- src/wizard/Step0/components/CreatePortfolio/CreatePortfolio.vue
+++ src/wizard/Step0/components/CreatePortfolio/CreatePortfolio.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-container fluid class="dashboard-body body-lg wizard-step-1-view">
+  <v-container fluid class="dashboard-body body-lg wizard-step-1-view pl-14">
     <br />
     <v-row>
-      <v-col cols="6">
+      <v-col cols="8">
         <h2 class="h2 step-1-subtitle">
           Hi {{ user.given_name }}, let's create your first portfolio!
         </h2>
@@ -10,35 +10,33 @@
     </v-row>
     <br />
     <v-row>
-      <v-col cols="6">
-        <p class="p step-1">
+      <v-col cols="7" class="pa-0">
+        <p class="p step-1 pb-6 mb-0">
           To get started with provisioning your cloud resources, you will need
-          to set up a Portfolio. We will walk you through adding your contract
+          to set up a portfolio. We will walk you through adding your contract
           information, as well as establishing the workspaces and team members
           that will develop your provisioned applications within the CSP
           console.
-          <br />
-          <br />
-          You will need to have an approved Task Order to fund your project,
-          prior to completing your Portfolio submission. But don't worry, you
-          can save a draft and come back to it at any time prior to
-          provisioning. You can even invite other Portfolio Managers to help you
-          out along the way!
-          <br />
-          <br />
-          Let's get started!
-          <br />
         </p>
+        <p class="p step-1 pb-6 mb-0">
+          You will need to have an approved task order to fund your project,
+          prior to completing your portfolio submission. But don't worry, you
+          can save a draft and come back to it at any time prior to
+          provisioning. You can even invite other portfolio Managers to help you
+          out along the way!
+        </p>
+        <p class="p step-1 pb-3">Let's get started!</p>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col cols="2">
-        <hr class="hr step-1-divider border-base-lighter" />
-      </v-col>
-    </v-row>
-    <br />
-    <v-row>
-      <v-col cols="6">
+    <div class="py-10">
+      <v-row>
+        <v-col cols="3">
+          <v-divider></v-divider>
+        </v-col>
+      </v-row>
+    </div>
+    <v-row class="ma-0">
+      <v-col cols="8" class="pl-0">
         <div :icon="false" class="bg-base-lightest body-lg border px-13 py-5">
           <v-btn
             id="btn-create-new-portfolio"
