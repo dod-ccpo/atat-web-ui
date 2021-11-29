13802024e0760457538194065b5c20e5b5274212
Eric Youngquist <eric.youngquist-ctr@ccpo.mil>
2021-11-29 11:45:29 -0500

    revert accidental file change from other branch

--- src/wizard/Step0/components/CreatePortfolio/CreatePortfolio.vue
+++ src/wizard/Step0/components/CreatePortfolio/CreatePortfolio.vue
@@ -2,19 +2,19 @@
   <div class="main-content-wrapper body-lg">
     <div class="content-max-width">
       <h1 tabindex="-1">
-        {{ user.given_name }}, let’s create your first portfolio!
+        Hi {{ user.given_name }}, let’s create your first portfolio!
       </h1>
       <p>
         To get started with provisioning your cloud resources, you will need to
         set up a portfolio. We will walk you through adding your contract
         information, as well as establishing the workspaces and team members
-        that will develop your provisioned applications within the cloud service
-        provider console.
+        that will develop your provisioned applications within the CSP console.
       </p>
       <p>
         You will need to have an approved task order to fund your project, prior
-        to completing your portfolio submission. Regardless, you can start and
-        save a draft, and come back to it at any time prior to provisioning.
+        to completing your portfolio submission. But don't worry, you can save a
+        draft and come back to it at any time prior to provisioning. You can
+        even invite other portfolio managers to help you out along the way!
       </p>
       <p>Let’s get started!</p>
 
