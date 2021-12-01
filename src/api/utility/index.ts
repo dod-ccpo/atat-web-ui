60e3e583e5804bcff43717f60cbfb8ef798fa064
robertmccardell-CCPO <robert.mccardell-crt@ccpo.mil>
2021-12-01 17:49:51 -0600

    file upload fix
    fixes broken file upload after API update to require Auth token.  The file upload component was calling axios directly and didn't have the token injected into the axios instance. Added upload file utility call that makes use of the ApiClient class

---
+++ src/api/utility/index.ts
@@ -0,0 +1,20 @@
+import { TaskOrderFile } from "types/Wizard";
+import ApiClient from "../apiClient";
+
+const client = new ApiClient("");
+
+export const uploadTaskOrderFile = async (
+  file: FormData
+): Promise<TaskOrderFile> => {
+  const response = await client.post("taskOrderFiles", file, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  if (response.status !== 201) {
+    throw Error(" error uploading file");
+  }
+  const data = response.data as TaskOrderFile;
+  return data;
+};
