ec9796bd43e2f81f0c74541827537bba7abe5b84
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2021-12-15 13:05:23 -0800

    finish page outline for portfoliosummary

--- src/PortfolioSummary/Index.vue
+++ src/PortfolioSummary/Index.vue
@@ -1,9 +1,29 @@
-<template><h1>index</h1></template>
+<template>
+  <div>
+    <div class="body-lg pl-14 pt-3 bg-white">
+      <h1 class="mb-0">{{ portfolio.name }}</h1>
+      <v-tabs class="atat-tabbar width-70">
+        <v-tab class="font-weight-bold body px-0">Funding Tracker</v-tab>
+        <v-tab class="font-weight-bold body px-0">Task Orders</v-tab>
+        <v-tab class="font-weight-bold body px-0">Applications</v-tab>
+        <v-tab class="font-weight-bold body px-0">Settings</v-tab>
+      </v-tabs>
+    </div>
+    <div class="wizard-content"></div>
+  </div>
+</template>
 
-<script>
-export default {
-  name: "Index",
-};
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class App extends Vue {
+  private portfolio = "";
+  mounted(): void {
+    this.portfolio = this.$store.getters["wizard/getPortfolio"];
+    console.log(this.portfolio);
+  }
+}
 </script>
-
-<style scoped></style>
