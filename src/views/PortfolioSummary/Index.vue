fa4f35b0c6dfeb0a0d9673f2017ae631fc2e09dd
Tony Sumner <tony.sumner-ctr@ccpo.mil>
2021-12-16 16:08:48 -0500

    working to resolve missing header and added css get gray background on the page between cards AT-6650

--- src/views/PortfolioSummary/Index.vue
+++ src/views/PortfolioSummary/Index.vue
@@ -1,37 +1,49 @@
 <template>
   <div>
-    <div class="body-lg pl-14 pt-3 bg-white">
-      <h3 class="h3 mb-0">{{ portfolio.name }}</h3>
+    <div class="body-lg pt-3">
+      <h3 class="h3 mb-0 ml-14">{{ portfolioName }}</h3>
       <v-tabs class="atat-tabbar dense">
-        <v-tab class="h5 font-weight-bold body px-0 atat-tabbar-content"
+        <v-tab
+          href="#fundingTracker"
+          class="h5 font-weight-bold body px-0 atat-tabbar-content ml-14"
           ><div class="tabname pb-4">Funding Tracker</div></v-tab
         >
-        <v-tab class="h5 font-weight-bold body px-0 atat-tabbar-content"
+        <v-tab-item value="fundingTracker">
+          <funding-tracker></funding-tracker>
+        </v-tab-item>
+        <v-tab
+          class="h5 font-weight-bold body px-0 atat-tabbar-content"
+          disabled
           ><div class="tabname pb-4">Task Orders</div></v-tab
         >
-        <v-tab class="h5 font-weight-bold body px-0 atat-tabbar-content"
+        <v-tab
+          class="h5 font-weight-bold body px-0 atat-tabbar-content"
+          disabled
           ><div class="tabname pb-4">Applications</div></v-tab
         >
-        <v-tab class="h5 font-weight-bold body px-0 atat-tabbar-content"
+        <v-tab
+          class="h5 font-weight-bold body px-0 atat-tabbar-content"
+          disabled
           ><div class="tabname pb-4">Settings</div></v-tab
         >
       </v-tabs>
     </div>
-    <div class="wizard-content"></div>
   </div>
 </template>
 
 <script lang="ts">
+import { Portfolio } from "types/Portfolios";
 import { Component, Vue } from "vue-property-decorator";
-
+import FundingTracker from "../PortfolioSummary/FundingTracker/views/FundingTracker.vue";
 @Component({
-  components: {},
+  components: {
+    FundingTracker,
+  },
 })
 export default class App extends Vue {
-  private portfolio = "";
+  private portfolioName = "";
   mounted(): void {
-    this.portfolio = this.$store.getters["wizard/getPortfolio"];
-    console.log(this.portfolio);
+    this.portfolioName = this.$store.getters["wizard/getPortfolioName"]();
   }
 }
 </script>
