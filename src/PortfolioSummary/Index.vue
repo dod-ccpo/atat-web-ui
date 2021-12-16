ebe7ce79270ffb252b51b23a71835da364168425
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2021-12-15 18:08:37 -0800

    fixed font size

--- src/PortfolioSummary/Index.vue
+++ src/PortfolioSummary/Index.vue
@@ -2,17 +2,17 @@
   <div>
     <div class="body-lg pl-14 pt-3 bg-white">
       <h1 class="mb-0">{{ portfolio.name }}</h1>
-      <v-tabs class="atat-tabbar">
-        <v-tab class="font-weight-bold body px-0 atat-tabbar-content"
+      <v-tabs class="atat-tabbar dense">
+        <v-tab class="h5 font-weight-bold body px-0 atat-tabbar-content"
           >Funding Tracker</v-tab
         >
-        <v-tab class="font-weight-bold body px-0 atat-tabbar-content"
+        <v-tab class="h5 font-weight-bold body px-0 atat-tabbar-content"
           >Task Orders</v-tab
         >
-        <v-tab class="font-weight-bold body px-0 atat-tabbar-content"
+        <v-tab class="h5 font-weight-bold body px-0 atat-tabbar-content"
           >Applications</v-tab
         >
-        <v-tab class="font-weight-bold body px-0 atat-tabbar-content"
+        <v-tab class="h5 font-weight-bold body px-0 atat-tabbar-content"
           >Settings</v-tab
         >
       </v-tabs>
