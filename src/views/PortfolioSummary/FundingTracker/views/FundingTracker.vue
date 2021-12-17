c62d99a39bdbb41cea7402f766cd01bc8bd9a205
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2021-12-17 10:40:51 -0800

    pr changes

--- src/views/PortfolioSummary/FundingTracker/views/FundingTracker.vue
+++ src/views/PortfolioSummary/FundingTracker/views/FundingTracker.vue
@@ -12,15 +12,13 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col class="col-sm-8">
-        <v-card class="no-shadow v-sheet--outlined height-100">
+      <v-col class="col-md-6 col-lg-8">
+        <v-card class="no-shadow v-sheet--outlined">
           <h3 class="mb-0 pb-6">Portfolio Details</h3>
-          <v-row>
-            <v-col class="col-sm-5"
-              ><div
-                class="bg-accent-cool-lightest pl-6 pt-6 pr-3 pb-16 rounded-sm"
-              >
-                <h2 class="h1 mb-0">$147,469.04</h2>
+          <div class="d-flex flex-wrap">
+            <div style="width: 270px" class="mr-5">
+              <div class="bg-accent-cool-lightest pl-6 pt-6 pr-3 pb-16 rounded">
+                <span class="h1 mb-0">$147,469.04</span>
                 <p class="font-weight-bold body-lg mb-0 pb-5">
                   Available Funds
                 </p>
@@ -28,11 +26,11 @@
                   Your remaining portfolio balance from all of your active task
                   orders
                 </p>
-              </div></v-col
-            >
-            <v-col class="col-sm-7">
+              </div>
+            </div>
+            <div>
               <p class="text--base-darkest pt-4 mb-0">Total Portfolio Funds</p>
-              <h3 class="mb-0">$200,000.00</h3>
+              <span class="h3 mb-0">$200,000.00</span>
               <p class="text--base-dark mb-0">
                 Total value of your active task orders
               </p>
@@ -40,13 +38,13 @@
               <p class="text--base-darkest mb-0">
                 Current Period of Performance
               </p>
-              <h4 class="mb-0">Jan. 1, 2021–Dec. 31, 2021</h4>
+              <span class="mb-0">Jan. 1, 2021–Dec. 31, 2021</span>
               <p class="text--base-dark mb-0">8 months to expiration</p>
-            </v-col>
-          </v-row>
+            </div>
+          </div>
         </v-card>
       </v-col>
-      <v-col class="col-sm-4 height-100">
+      <v-col class="col-md-6 col-lg-4">
         <v-card class="no-shadow v-sheet--outlined height-100">
           <h3 class="mb-2">Funding Status</h3>
           <donut-chart
