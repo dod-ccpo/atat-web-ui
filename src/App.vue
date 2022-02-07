e19267e0c88ff222b0c098f4400f7ebeb7b90051
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-02-07 12:32:24 -0800

    fixed spacing

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="app">
-    <v-img src="./assets/logo.png" />
+    <v-img src="./assets/logo.png"/>
     <v-alert
         border="top"
         color="red lighten-2"
@@ -8,15 +8,15 @@
     >
       I'm an alert with a top border and red color
     </v-alert>
-   <!-- Users:
-    <users /> -->
+    <!-- Users:
+     <users /> -->
     <hello-world></hello-world>
 
   </v-app>
 </template>
 
 <style lang="scss">
-@import "./sass/atat.scss"; 
+@import "./sass/atat.scss";
 </style>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from "./plugins/vuetify";
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-    vuetify,
-    render: h => h(App),
+  vuetify,
+  render: h => h(App),
 }).$mount('#app')
