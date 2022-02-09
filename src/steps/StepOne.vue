5c3e80bea2f532a159bf00d8a6392678c719e8fd
Tony Sumner <tony.sumner-ctr@ccpo.mil>
2022-02-09 11:34:13 -0500

    moved scss, typescripted components
    [x] 'typescripted' all components from javascript
    [x] removed scss from atatfooter.vue into ATATFooter.scss
    
    Feature AT-7066

--- src/steps/StepOne.vue
+++ src/steps/StepOne.vue
@@ -2,8 +2,9 @@
     <div>Step One</div>
 </template>
 
-<script>
-export default {
-    name:"StepOne"
-}
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+@Component({})
+export default class StepOne extends Vue {}
 </script>
