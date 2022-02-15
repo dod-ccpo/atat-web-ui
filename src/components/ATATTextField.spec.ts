cf8a60ade006225d068d4bf07e196ec8aacf7e75
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-02-15 10:10:03 -0800

    testing texbox

---
+++ src/components/ATATTextField.spec.ts
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import { createLocalVue, mount } from "@vue/test-utils";
+import ATATTextField from "@/components/ATATTextField.vue";
+Vue.use(Vuetify);
+
+describe("Testing ATATTextField Component", () => {
+  const localVue = createLocalVue();
+  let vuetify: any;
+  let wrapper: any;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    wrapper = mount(ATATTextField, {
+      localVue,
+      vuetify,
+    });
+  });
+  it("renders successfully", async () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+  it("test input actions, is success", async () => {
+    await wrapper.vm.inputActions("hello");
+    expect(wrapper.vm.$data).toBe('hello');
+  });
+});
