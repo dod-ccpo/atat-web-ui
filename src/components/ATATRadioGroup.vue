647562d9422ab1f3e0113984dcb4fdf9edfe40ef
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-02-24 06:27:26 -0800

    removed padding and ripple

--- src/components/ATATRadioGroup.vue
+++ src/components/ATATRadioGroup.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid>
     <v-radio-group v-model="_selectedValue">
-      <v-radio :ripple="false" :class="[card? '_v_radio-button-cards': '_radio-button','pl-3','py-3']" v-for="item in items" :key="item" :label="item" :value="item"></v-radio>
+      <v-radio :ripple="false" :class="[card? '_v_radio-button-cards': '_radio-button']" v-for="item in items" :key="item" :label="item" :value="item"></v-radio>
     </v-radio-group>
   </v-container>
 </template>
