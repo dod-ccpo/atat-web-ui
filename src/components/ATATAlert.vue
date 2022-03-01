bb5faa353132a49bf58c38e822269cebafb91491
Tony Sumner <tony.sumner-ctr@ccpo.mil>
2022-03-01 11:43:05 -0500

    [WIP] Page 2 of 2
    [x] created ATATAlert component & SCSS file
    [x] crafted all of page 2 except for last textbox
    
    AT-7093

---
+++ src/components/ATATAlert.vue
@@ -0,0 +1,112 @@
+<template>
+  <div
+    v-if="show"
+    :role="role"
+    :class="[
+      getBorderWidth(),
+      type + '_alert',
+      'v-application d-block atat-alert black--text body-lg',
+    ]"
+  >
+    <div class="d-flex justify-start">
+      <div
+        v-if="showIcon"
+        class="text-center px-0 pt-1 d-flex flex-column align-start"
+      >
+        <i
+          aria-hidden="true"
+          :class="[
+            getIconSize(),
+            'v-icon notranslate material-icons theme--light text--base-darkest',
+          ]"
+        >
+          {{ getIcon() }}
+        </i>
+      </div>
+      <div class="body-lg black--text content-max-width">
+        <slot name="content"></slot>
+      </div>
+      <div
+        v-if="closeButton"
+        class="
+          text-center
+          pr-0
+          pl-4
+          pt-1
+          mx-0
+          d-flex
+          flex-column
+          align-start
+          ml-auto
+        "
+      >
+        <v-btn
+          icon
+          :ripple="false"
+          x-small
+          aria-label="Close alert"
+          @click="close"
+        >
+          <v-icon
+            aria-hidden="true"
+            class="
+              notranslate
+              material-icons
+              theme--light
+              text--base-darkest
+              icon-20
+            "
+          >
+            close
+          </v-icon>
+        </v-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component({})
+export default class ATATAlert extends Vue {
+  @Prop({ default: "presentation" }) private role?: string;
+  @Prop({ default: "" }) private icon?: string;
+  @Prop({ default: true }) private showIcon?: boolean;
+  @Prop({ default: true }) private show?: boolean;
+
+  /**
+   * type: 1) info, 2) error, 3) warning
+   */
+  @Prop({ default: "error" }) private type?: string;
+
+  /**
+   * size: 1) large or 2)small
+   * large size will render a 8px left border and 20px icon
+   * small size will render a 4px left border and 16px icon
+   */
+  @Prop({ default: "large" }) private size?: string;
+  @Prop({ default: true }) private outlined?: boolean;
+  @Prop({ default: false }) private closeButton?: boolean;
+
+  private getBorderWidth(): string {
+    return "border-left-" + (this.size === "large" ? "thick" : "slim");
+  }
+
+  private getIconSize(): string {
+    return this.size === "large" ? "icon-20" : "icon-16";
+  }
+
+  private getIcon(): string | unknown {
+    if (this.icon === "") {
+      return this.type === "success" ? "check_circle" : this.type;
+    }
+    return this.icon;
+  }
+
+  private close(): void {
+    this.show = false;
+  }
+}
+</script>
---
+++ src/steps/StepOneold.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col class="col-12">
+          <h2>Let’s start with basic info about your new acquisition</h2>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <span
+            >In this section, we will gather some overarching details about your
+            project requirements, organization, and points of contact. This
+            information will be used to complete your required acquisition forms
+            and will also help us to guide you through the JWCC procurement
+            process.</span
+          >
+          <div class="mt-10">
+            <ATATTextField
+              label="Project/Requirements Title"
+              class="input-max-width"
+              helpText="Provide a short, descriptive title of the work to be performed. This will be used to refer to this project within ATAT and across all acquisition forms."
+            />
+          </div>
+          <div class="d-flex align-start flex-column mt-10 content-max-width">
+            <span class="font-weight-bold"> What is the scope of your requirement? </span>
+            <span class="body-sm"
+              >Briefly describe the type of resources and services to be
+              acquired, and what is necessary to achieve mission specific
+              outcomes for this particular task (e.g., move DITCO’s contract
+              writing system to a cloud environment).</span
+            >
+             <ATATTextArea
+              label=""
+              class="width-100"
+              :rows=7
+            />
+          </div>
+          <div class="d-flex align-start flex-column mt-10 content-max-width">
+            <span> Is this requirement in support of an emergency declaration?</span>
+           <ATATRadioGroup
+              id="emergency-declaration-support-requirement"
+              :value.sync="radioValue"
+              :items="radioGroupItems"
+              name="another-radio-group"
+              class="mt-3"
+          >
+          </ATATRadioGroup>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts">
+import ATATTextField from "../components/ATATTextField.vue";
+import ATATTextArea from "../components/ATATTextArea.vue";
+import ATATRadioGroup from "../components/ATATRadioGroup.vue"
+
+import Vue from "vue";
+
+import { Component } from "vue-property-decorator";
+import { RadioButton } from "types/Global";
+
+@Component({
+  components: {
+    ATATTextField,
+    ATATTextArea,
+    ATATRadioGroup
+  },
+})
+export default class StepOneOld extends Vue {
+  private radioValue = '';
+  private radioGroupItems:RadioButton[] = [
+    {
+      id: "Yes",
+      label: "Yes",
+      value: "yes",
+    },
+    {
+      id: "No",
+      label: "No",
+      value: "no",
+    }
+  ] 
+}
+</script>
+
