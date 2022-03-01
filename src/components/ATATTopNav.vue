376694c6f85c898f4d9eac9ed842ad4f2859d1f2
robertmccardell-CCPO <robert.mccardell-ctr@ccpo.mil>
2022-03-01 09:42:48 -0600

    Create ATATTopNav.vue
    updates

---
+++ src/components/ATATTopNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar app class="atat-header-nav" height="48" elevation="0">
+    <div class="d-flex align-center justify-space-between pt-2 nav-header">
+      <div class="d-flex  px-4">
+        <p>Demo Package</p>
+
+    <div class="mr-auto">
+    <!-- <v-btn icon>
+      <v-icon> person_add_alt_1</v-icon>
+    </v-btn> -->
+
+    <!-- <v-btn icon>
+      <v-icon> more_horizontal</v-icon>
+    </v-btn> -->
+      </div>
+      </div>
+    </div>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+@Component({})
+export default class ATATTopNav extends Vue {}
+</script>
