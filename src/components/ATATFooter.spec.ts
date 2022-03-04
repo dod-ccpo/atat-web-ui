7e5095d579d4a1fad7ef67c0b537b1c2874279dd
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-03-04 14:27:57 -0500

    Added unit test for footer

---
+++ src/components/ATATFooter.spec.ts
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import { createLocalVue, mount } from "@vue/test-utils";
+import ATATFooter from "@/components/ATATFooter.vue";
+Vue.use(Vuetify);
+
+describe("Testing ATATTextField Component", () => {
+  const localVue = createLocalVue();
+  let vuetify: any;
+  let wrapper: any;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    wrapper = mount(ATATFooter, {
+      localVue,
+      vuetify,
+    });
+  });
+  it("renders successfully", async () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+});
