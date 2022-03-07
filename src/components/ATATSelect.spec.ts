5bbf37c0ec1126b946d828e0e7900da2df86624c
Tony Sumner <tony.sumner-ctr@ccpo.mil>
2022-03-07 17:26:29 -0500

    [WIP] code coverage 100% save for branch (50%)

--- src/components/ATATSelect.spec.ts
+++ src/components/ATATSelect.spec.ts
@@ -3,13 +3,15 @@ import Vuetify from "vuetify";
 import { createLocalVue, mount, Wrapper } from "@vue/test-utils";
 import ATATSelect from "@/components/ATATSelect.vue";
 import { SelectData } from "types/Global";
+import { DefaultProps } from "vue/types/options";
+Vue.config.productionTip = false;
 Vue.use(Vuetify);
 
 describe("Testing ATATTextField Component", () => {
   const localVue = createLocalVue();
   let vuetify: Vuetify;
-  let wrapper: Wrapper<Vue, Element>;
-
+  let wrapper: Wrapper<DefaultProps & Vue, Element>;
+  
   const items: SelectData[] = [
     {text: "Value 01", value: "value_01"},
     {text: "Value 02", value: "value_02"},
@@ -28,20 +30,49 @@ describe("Testing ATATTextField Component", () => {
         rules: [(v: string) => !!v || "is required"],
       }
     });
+
+  });
+  describe("INITIALIZATION", () => { 
+      it("renders successfully", async () => {
+        expect(wrapper.exists()).toBe(true);
+    });
   });
 
-  it("renders successfully", async () => {
-    expect(wrapper.exists()).toBe(true);
+  describe("PROPS", () => { 
+    it("'optional' prop",()=>{
+      wrapper.vm.$data.optional = true;
+      expect(wrapper.find(".optional")).toBeDefined;
+
+      wrapper.vm.$data.optional = false;
+      expect(wrapper.find(".optional")).toBeUndefined;
+    });
   });
 
-  it("Events > onchange ", async () => {
-    // await wrapper.setData({
-    //   selected: "",
+  describe("DATA", () => { 
+    it("items.length", async () => {
+      const select =  wrapper.find('.v-select');
+      const items = select.props('items');
+      expect(items.length).toBe(5);
+    }); 
+  });
+ 
+  describe("EVENTS", () => {
+    // it("onChange", async () => {
+    //   wrapper.vm.$emit("onChange", "value_04");
+    //   expect(wrapper.emitted("onChange")?.length).toBe(1);
+      
+    //   await wrapper.vm.$nextTick();
+    //   // expect(wrapper.emitted("onChange")[0][0]).toEqual("value_04");
+    //   console.log(wrapper.emitted("onChange"))
+
+    //   await wrapper.vm.$nextTick();
+    //   // expect(wrapper.vm.$data.selected).toEqual("value_04");
     // });
-    // expect(wrapper.vm.$data.selected).toBe("");
-
-    await wrapper.vm.$emit("onchange","value_04");
-    expect(wrapper.vm.$data.selected).toBe("value_04");
-  });
+    it("onChange", async () => {
+      const changedValue = "value_04";
+      await wrapper.vm.onChange(changedValue);
+      expect(wrapper.vm.$data.selected).toEqual(changedValue);
+     });
+  })
 
 });
