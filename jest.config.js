2c6b0a7fe2e887693e320b4ee115e58ced9046f1
Tony Sumner <tony.sumner-ctr@ccpo.mil>
2022-03-07 17:26:42 -0500

    [WIP] code coverage 100% save for branch (50%)

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,31 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   transformIgnorePatterns: ["/node_modules/(?!vuetify/)"],
   testMatch: ["**/src/**/*.spec.ts", "**/src/**/*.spec.js"],
-  collectCoverage: false,
+  collectCoverage: true,
   collectCoverageFrom: ["src/**/*.spec.ts", "src/**/*.vue"],
+  globals: {
+    'ts-jest': {
+      diagnostics: true
+    }
+  },
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules"
+  ],
+  moduleFileExtensions: [
+    "ts",
+    "js",
+    "json",
+    "vue"
+  ],
+  transform: {
+   
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.ts$": "ts-jest"
+  },
+ 
+  verbose: true,
+  testURL: "http://localhost/"
+ 
 }
+
+// "^.+\\.vue": "vue-jest",
