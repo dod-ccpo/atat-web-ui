faac3524baa7899dc664355e92f1c74a34ca9855
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-03-08 17:27:19 -0800

    adding Cors components to page

--- src/steps/AcquisitionPackageDetails/COR_ACOR/CorInfo.vue
+++ src/steps/AcquisitionPackageDetails/COR_ACOR/CorInfo.vue
@@ -1,19 +1,27 @@
 <template>
-  <common :isCOR=true :isACOR=false />
+  <v-container fluid class="container-max-width">
+    <h1 id="CORHeader">Let’s gather info about your Contracting Officer’s Representative (COR)</h1>
+    <p class="body pt-3 pb-0 mb-0">Your COR is an important part of your acquisition approval process. We will fill out your COR’s
+      contact
+      information on any necessary forms and will send your COR an email when documents are ready to be signed. Search
+      for your COR below or manually enter their contact information. For more guidance about CORs, visit
+      https://www.ditco.disa.mil/hq/cor/index.asp.</p>
+    <common :isCOR=true :isACOR=false />
+  </v-container>
 </template>
 <script lang="ts">
 import Vue from "vue";
 
-import { Component } from "vue-property-decorator";
+import {Component} from "vue-property-decorator";
 import Common from "./Common.vue";
 
 @Component({
   components: {
-     Common,
+    Common,
   }
 })
 export default class CorInfo extends Vue {
-  
+
 
 }
 </script>
