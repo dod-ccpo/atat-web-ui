0d2cf3b7751a67b32eb607dc8bb814ecb72abb5b
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-03-09 10:50:04 -0800

    pr changes

--- src/steps/AcquisitionPackageDetails/COR_ACOR/CorInfo.vue
+++ src/steps/AcquisitionPackageDetails/COR_ACOR/CorInfo.vue
@@ -2,10 +2,11 @@
   <v-container fluid class="container-max-width">
     <v-row>
       <v-col>
-        <h1 class="page-header" id="CORHeader">Let’s gather info about your Contracting Officer’s
-          Representative (COR)
+        <h1 class="page-header" id="CORHeader">
+          Let’s gather info about your Contracting Officer’s Representative (COR)
         </h1>
-        <p class="body pb-0 mb-0 copy-max-width">Your COR is an important part of your acquisition
+        <p class="body pb-0 mb-0 copy-max-width">
+          Your COR is an important part of your acquisition
           approval process. We will fill out your COR’s contact information on any necessary forms
           and will send your COR an email when documents are ready to be signed. Search for your COR
           below or manually enter their contact information. For more guidance about CORs, visit
