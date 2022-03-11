74a1cf8999d71b59304c68d6511265e60940a9f5
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-03-11 13:51:18 -0500

    Added ACOR header content

--- src/steps/AcquisitionPackageDetails/COR_ACOR/AcorInfo.vue
+++ src/steps/AcquisitionPackageDetails/COR_ACOR/AcorInfo.vue
@@ -2,7 +2,19 @@
   <v-container fluid class="container-max-width">
     <v-row>
       <v-col>
-        <p class="pv-10">Put ACOR header and intro here</p>
+        <h1 class="page-header">
+          Let’s gather info about your ACOR
+        </h1>
+        <p class="page-intro">
+          Your ACOR’s information will be used to fill out any forms in your 
+          acquisition package. We will send this individual an email if their 
+          signature is required on any documents. Search for your ACOR below or 
+          manually enter their contact information. For more guidance about 
+          CORs, visit 
+          <a href="https://www.ditco.disa.mil/hq/cor/index.asp" class="text-link" target="_blank">
+            https://www.ditco.disa.mil/hq/cor/index.asp<span class="external-link">.</span>
+          </a>
+        </p>
 
         <common :isACOR="true" />
 
