6d874852f4b2115777e2f9765d911fbc30d9bdaa
robertmccardell-CCPO <robert.mccardell-ctr@ccpo.mil>
2022-03-14 08:59:54 -0500

    initial changes
    initial changes

--- src/steps/AcquisitionPackageDetails/ProjectOverview.vue
+++ src/steps/AcquisitionPackageDetails/ProjectOverview.vue
@@ -3,7 +3,9 @@
     <v-container fluid class="container-max-width">
       <v-row>
         <v-col>
-          <h1 class="page-header">Let’s start with basic info about your new acquisition</h1>
+          <h1 class="page-header">
+            Let’s start with basic info about your {{ projectTitle }}
+          </h1>
           <p class="page-intro">
             In this section, we will gather some overarching details about your
             project requirements, organization, and points of contact. This
@@ -17,6 +19,8 @@
               label="Project/Requirement Title"
               class="input-max-width"
               tooltipText="Provide a short, descriptive title of the work to be performed. This will be used to refer to this project within ATAT and across all acquisition forms."
+              :value.sync="currentTitle"
+              @blur="onTitleChanged"
             />
           </div>
           <div class="d-flex align-start flex-column mt-10 textarea-max-width">
@@ -24,7 +28,7 @@
               id="ProjectScope"
               label="What is the scope of your requirement?"
               class="width-100"
-              :rows=7
+              :rows="7"
               helpText="Briefly describe the type of resources and services to be
               acquired, and what is necessary to achieve mission specific
               outcomes for this particular requirement (e.g., move DITCO’s contract
@@ -39,8 +43,8 @@
               :items="radioGroupItems"
               name="emergency-declaration-support-requirement-radio-group"
               class="mt-3"
-          >
-          </ATATRadioGroup>
+            >
+            </ATATRadioGroup>
           </div>
         </v-col>
       </v-row>
@@ -51,23 +55,25 @@
 <script lang="ts">
 import ATATTextField from "../../components/ATATTextField.vue";
 import ATATTextArea from "../../components/ATATTextArea.vue";
-import ATATRadioGroup from "../../components/ATATRadioGroup.vue"
+import ATATRadioGroup from "../../components/ATATRadioGroup.vue";
 
 import Vue from "vue";
 
 import { Component } from "vue-property-decorator";
 import { RadioButton } from "types/Global";
 
+import AcquisitionPackage from "@/store/acquisitionPackage";
+
 @Component({
   components: {
     ATATTextField,
     ATATTextArea,
-    ATATRadioGroup
+    ATATRadioGroup,
   },
 })
 export default class ProjectOverview extends Vue {
-  private radioValue = '';
-  private radioGroupItems:RadioButton[] = [
+  private radioValue = "";
+  private radioGroupItems: RadioButton[] = [
     {
       id: "Yes",
       label: "Yes",
@@ -77,8 +83,29 @@ export default class ProjectOverview extends Vue {
       id: "No",
       label: "No",
       value: "no",
+    },
+  ];
+
+  private currentTitle = "";
+
+  public get projectTitle(): string {
+    return AcquisitionPackage.projectTitle !== ""
+      ? AcquisitionPackage.projectTitle
+      : "new acquisition";
+  }
+
+  public set projectTitle(value: string) {
+    AcquisitionPackage.setProjectTitle(value);
+  }
+
+  public onTitleChanged():void {
+
+    if(this.currentTitle === "" || this.currentTitle == this.projectTitle){
+        return;
     }
-  ] 
+
+    this.projectTitle = this.currentTitle;
+      
+  }
 }
 </script>
-
--- src/store/acquisitionPackage/index.ts
+++ src/store/acquisitionPackage/index.ts
@@ -1,13 +1,19 @@
-import {  VuexModule, Module, getModule, Action, Mutation} from "vuex-module-decorators";
+import {  VuexModule, Module, getModule, Mutation} from "vuex-module-decorators";
 import rootStore from "../index";
 
 @Module({ name: 'AcquisitionPackage',  namespaced: true, dynamic: true, store: rootStore})
 export class AcquisitionPackageStore extends VuexModule  {
+   projectTitle = "";
    hasAlternativeContactRep: boolean | null = null;
 
    @Mutation
    public setHasAlternateCOR(value: boolean): void{
       this.hasAlternativeContactRep = value;
+   }
+
+   @Mutation
+   public setProjectTitle(value: string) : void {
+      this.projectTitle = value;
    }
 
 }
