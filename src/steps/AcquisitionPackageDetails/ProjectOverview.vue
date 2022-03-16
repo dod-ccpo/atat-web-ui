dae7893b6a6a01c4e0408ec2db687c907569d78a
Tony Sumner <tony.sumner-ctr@ccpo.mil>
2022-03-16 15:02:04 -0400

    reinstated feature/AT-7127-project-overview -validation after git issues

--- src/steps/AcquisitionPackageDetails/ProjectOverview.vue
+++ src/steps/AcquisitionPackageDetails/ProjectOverview.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
+   <v-form ref="form" lazy-validation>
     <v-container fluid class="container-max-width">
       <v-row>
         <v-col>
-          <h1 class="page-header">
-            Let’s start with basic info about your new acquisition
-          </h1>
+          <h1 class="page-header">Let’s start with basic info about your new acquisition</h1>
           <p class="page-intro">
             In this section, we will gather some overarching details about your
             project requirements, organization, and points of contact. This
@@ -17,10 +15,13 @@
             <ATATTextField
               id="ProjectTitle"
               label="Project/Requirement Title"
+              :rules="[
+                $validators.required('Please enter your project title'), 
+                $validators.maxLength(60, 'Title cannot exceed 60 characters')
+              ]"
+              :value="projectTitle"
               class="input-max-width"
               tooltipText="Provide a short, descriptive title of the work to be performed. This will be used to refer to this project within ATAT and across all acquisition forms."
-              :value.sync="currentTitle"
-              @blur="onTitleChanged"
             />
           </div>
           <div class="d-flex align-start flex-column mt-10 textarea-max-width">
@@ -28,7 +29,11 @@
               id="ProjectScope"
               label="What is the scope of your requirement?"
               class="width-100"
-              :rows="7"
+              :rows=7
+              :rules="[
+                $validators.required('Please describe the scope of your requirement'), 
+                $validators.maxLength(300, 'Please limit your description to 300 characters or less')
+              ]"
               helpText="Briefly describe the type of resources and services to be
               acquired, and what is necessary to achieve mission specific
               outcomes for this particular requirement (e.g., move DITCO’s contract
@@ -36,44 +41,46 @@
             />
           </div>
           <div class="d-flex align-start flex-column mt-10">
-            <ATATRadioGroup
+           <ATATRadioGroup
               id="emergency-declaration-support-requirement"
               legend="Is this requirement in support of an emergency declaration?"
               :value.sync="radioValue"
               :items="radioGroupItems"
               name="emergency-declaration-support-requirement-radio-group"
               class="mt-3"
-            >
-            </ATATRadioGroup>
+              :rules="[
+                $validators.required('Please select an option'), 
+              ]"
+          >
+          </ATATRadioGroup>
           </div>
         </v-col>
       </v-row>
     </v-container>
-  </div>
+  </v-form>
 </template>
 
 <script lang="ts">
 import ATATTextField from "../../components/ATATTextField.vue";
 import ATATTextArea from "../../components/ATATTextArea.vue";
-import ATATRadioGroup from "../../components/ATATRadioGroup.vue";
+import ATATRadioGroup from "../../components/ATATRadioGroup.vue"
 
 import Vue from "vue";
 
-import {Component} from "vue-property-decorator";
-import {RadioButton} from "types/Global";
-
-import AcquisitionPackage from "@/store/acquisitionPackage";
+import { Component } from "vue-property-decorator";
+import { RadioButton } from "types/Global";
 
 @Component({
   components: {
     ATATTextField,
     ATATTextArea,
-    ATATRadioGroup,
+    ATATRadioGroup
   },
 })
 export default class ProjectOverview extends Vue {
-  private radioValue = "";
-  private radioGroupItems: RadioButton[] = [
+  private radioValue = '';
+  private projectTitle = '';
+  private radioGroupItems:RadioButton[] = [
     {
       id: "Yes",
       label: "Yes",
@@ -83,21 +90,24 @@ export default class ProjectOverview extends Vue {
       id: "No",
       label: "No",
       value: "no",
-    },
-  ];
+    }
+  ] 
 
-  private currentTitle = "";
+  get Form(): Vue & { validate: () => boolean } {
+    return this.$refs.form as Vue & { validate: () => boolean };
+  }
+  
+  public async validateForm(): Promise<boolean> {
+    let valid = false;
 
-  public get projectTitle(): string {
-    return AcquisitionPackage.getTitle()
+    await this.$nextTick(() => {
+      valid = this.Form.validate();
+    });
+    return valid;
   }
 
-  public set projectTitle(value: string) {
-    AcquisitionPackage.setProjectTitle(value);
-  }
-
-  public onTitleChanged(): void {
-    this.projectTitle = this.currentTitle;
+  public async mounted(): Promise<void> {
+    // await this.validateForm();
   }
 }
 </script>
