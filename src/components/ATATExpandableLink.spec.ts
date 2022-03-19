42c74878d92465cad6ade3a879c10ed6e1b63ff3
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-03-19 13:08:21 -0400

    Added jest test for PageHead, ExpandableLink, and CheckboxGroup components

---
+++ src/components/ATATExpandableLink.spec.ts
@@ -0,0 +1,68 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import { createLocalVue, mount, Wrapper } from "@vue/test-utils";
+import ATATExpandableLink from "@/components/ATATExpandableLink.vue";
+import { DefaultProps } from "vue/types/options";
+Vue.config.productionTip = false;
+Vue.use(Vuetify);
+
+describe("Testing ATATSelect Component", () => {
+  const localVue = createLocalVue();
+  let vuetify: Vuetify;
+  let wrapper: Wrapper<DefaultProps & Vue, Element>;
+  const ariaId = "MyId";
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    wrapper = mount(ATATExpandableLink, {
+      localVue,
+      vuetify,
+      propsData: {
+        ariaId,
+      }
+    });
+
+  });
+  describe("INITIALIZATION", () => { 
+      it("renders successfully", async () => {
+        expect(wrapper.exists()).toBe(true);
+    });
+  });
+
+  describe("PROPS", () => { 
+    it("ariaId defined", async()=>{
+      expect(wrapper.find("#Button_" + ariaId)).toBeDefined;
+
+      await wrapper.setProps({ ariaId: null })
+      expect(wrapper.vm.ariaId).toBe(null)
+
+    });
+  });
+
+  describe("EVENTS", () => {
+    it("expands and collapses", async () => {
+      const expandLink = wrapper.find(".expandable-content-opener");
+      expect(expandLink.classes("closed")).toBe(true);
+
+      const content = wrapper.find("#Content_" + ariaId);
+      expect(content.isVisible()).toBe(false);
+      // expect(content.attributes("aira-hidden")).toBe("true");
+      
+      expandLink.trigger("click");
+
+      wrapper.vm.$nextTick(async () => {
+        expect(content.isVisible()).toBe(true);
+        expect(expandLink.classes()).toContain("open");
+        // expect(content.attributes("aira-hidden")).toBe("false");
+        expandLink.trigger("click");
+        wrapper.vm.$nextTick(() => {
+          expect(expandLink.classes()).toContain("closed");
+          expect(content.isVisible()).toBe(false);
+        });
+  
+      });
+
+     });
+  })
+
+});
--- src/components/ATATPageHead.vue
+++ src/components/ATATPageHead.vue
@@ -1,25 +1,25 @@
 <template>
-<v-app-bar app flat>
-
-  <div class="d-flex justify-space-between width-100">
-    <div class="h3">{{ headline }}</div>
-    <div class="d-flex justify-end">
-      <v-btn icon class="mr-5 icon-24">
-        <v-icon class="text-base-dark">person_add_alt_1</v-icon>
-      </v-btn>
-      <v-btn icon class="icon-24">
-        <v-icon class="text-base-dark">more_horiz</v-icon>
-      </v-btn>
+  <v-app-bar app flat>
+    <div class="d-flex justify-space-between width-100">
+      <div id="PackageNameHeader" class="h3">{{ headline }}</div>
+      <div class="d-flex justify-end">
+        <v-btn icon class="mr-5 icon-24" id="Person_Button">
+          <v-icon class="text-base-dark">person_add_alt_1</v-icon>
+        </v-btn>
+        <v-btn icon class="icon-24" id="More_Button">
+          <v-icon class="text-base-dark">more_horiz</v-icon>
+        </v-btn>
+      </div>
     </div>
-  </div>
-</v-app-bar>
+  </v-app-bar>
 </template>
-
 
 <script lang="ts">
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
+
 @Component({})
+
 export default class ATATPageHead extends Vue {
   @Prop({ default: "Headline" }) private headline!: string;
 }
