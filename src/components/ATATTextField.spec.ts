83d22d706defa564b371ff4cef5c7914933c4334
Tony Sumner <tony.sumner-ctr@ccpo.mil>
2022-03-21 09:44:37 -0400

    [WIP] ATATTextField unit test

--- src/components/ATATTextField.spec.ts
+++ src/components/ATATTextField.spec.ts
@@ -9,19 +9,50 @@ describe("Testing ATATTextField Component", () => {
   const localVue = createLocalVue();
   let vuetify: Vuetify;
   let wrapper: Wrapper<DefaultProps & Vue, Element>;
-
+  let textField: Wrapper<Vue,Element>;
+  // let textField: HTMLInputElement;
+  const propsId = "testId";
+  const id = "#" + propsId + "_text_field";
+  
   beforeEach(() => {
     vuetify = new Vuetify();
     wrapper = mount(ATATTextField, {
       localVue,
       vuetify,
+      propsData: {
+        id: propsId,
+      },
+    });
+    textField = wrapper.find(id);
+  });
+
+  describe("INITIALIZATION", () => { 
+    it("renders successfully", async () => {
+      expect(wrapper.exists()).toBe(true);
     });
   });
-  it("renders successfully", async () => {
-    expect(wrapper.exists()).toBe(true);
-  });
-  it("test input actions, is success", async () => {
-    await wrapper.vm.inputActions("hello");
-    expect(wrapper.exists()).toBe(true);
+
+  describe("EVENTS", () => { 
+    it("onInput", async () => {
+      // const spy = jest.spyOn(wrapper.vm, "onInput");
+      // await wrapper.vm.onInput("newInputValue")
+      // expect(spy).toHaveBeenCalled();
+      textField.trigger("input");
+      await wrapper.vm.$nextTick(()=>{
+        console.log(wrapper.emitted());
+      })
+    });
+
+    // it("onBlur", async () => {
+    //   // const spy = jest.spyOn(wrapper.vm, "onBlur");
+    //   // await wrapper.vm.onBlur("")
+    //   // expect(spy).toHaveBeenCalled();
+      
+    //   textField.trigger("blur");
+    //   await wrapper.vm.$nextTick(()=>{
+    //     console.log(wrapper.emitted().blur);
+    //   })
+      
+    // });
   });
 });
