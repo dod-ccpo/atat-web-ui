fa57019e9c6d5497896646564e293f0a45cde713
Prasanthi Alaparthi <prasanthi.alaparthi-ctr@ccpo.mil>
2022-03-21 11:02:08 -0400

    Fixed the operator sign  [x]AT-7170

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -137,7 +137,7 @@ Cypress.Commands.add("serviceOrAgency", (inputText) => {
         .then(($option) => {
             const selectedOption = $option.val();
             cy.log(selectedOption);
-            if (selectedOption == "Defense Information Systems Agency (DISA) **") {
+            if (selectedOption === "Defense Information Systems Agency (DISA) **") {
                 cy.iframe("#atat-app").find("#DisaOrg_AutoComplete_Wrapper")
                     .should('exist')
                     .and("be.visible")
