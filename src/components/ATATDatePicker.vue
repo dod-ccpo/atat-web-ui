6aa36742cefbb0b41169c0756f7aa3eed1a976c0
Tony Sumner <tony.sumner-ctr@ccpo.mil>
2022-03-22 13:12:10 -0400

    [WIP] datepicker initialization

---
+++ src/components/ATATDatePicker.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-menu
+      v-model="menu2"
+      :close-on-content-click="false"
+      :nudge-right="40"
+      transition="scale-transition"
+      offset-y
+      min-width="auto"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-text-field
+          v-model="date"
+          label="Picker without buttons"
+          prepend-icon="mdi-calendar"
+          readonly
+          v-bind="attrs"
+          v-on="on"
+        ></v-text-field>
+      </template>
+      <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
+    </v-menu>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component } from "vue-property-decorator";
+import Vue from "vue";
+
+@Component({})
+export default class ATATDatePicker extends Vue {}
+</script>
