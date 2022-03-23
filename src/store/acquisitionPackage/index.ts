806c5014fc37f3ceea2b4c923b8875b11b49e928
robertmccardell-CCPO <robert.mccardell-ctr@ccpo.mil>
2022-03-23 12:44:57 -0500

    updating api scaffolding and creating baseTableApi

--- src/store/acquisitionPackage/index.ts
+++ src/store/acquisitionPackage/index.ts
@@ -1,5 +1,9 @@
-import {getModule, Module, Mutation, VuexModule} from "vuex-module-decorators";
+import {Action, getModule, Module, Mutation, VuexModule} from "vuex-module-decorators";
 import rootStore from "../index";
+import api from "@/api";
+import { AcquisitionPackageDTO } from "@/models/AcquisitionPackageDTO";
+
+const ATAT_ACQUISTION_PACKAGE_KEY="ATAT_ACQUISTION_PACKAGE_KEY";
 
 @Module({
   name: 'AcquisitionPackage',
@@ -10,6 +14,7 @@ import rootStore from "../index";
 
 export class AcquisitionPackageStore extends VuexModule {
   projectTitle = "";
+  acquisitionPackage: AcquisitionPackageDTO  | null = null;
   hasAlternativeContactRep: boolean | null = null;
 
   public getTitle(): string {
@@ -22,10 +27,40 @@ export class AcquisitionPackageStore extends VuexModule {
   }
 
   @Mutation
+  public setAcquisitionPackage(value: AcquisitionPackageDTO): void {
+    this.acquisitionPackage = value;
+  }
+
+  @Mutation
   public setProjectTitle(value: string): void {
     this.projectTitle = value;
   }
 
+  @Action({ rawError: true })
+  public async initialize(): Promise<void> {
+     
+       const storedAcquisitionPackageData = sessionStorage.getItem(ATAT_ACQUISTION_PACKAGE_KEY) as string;
+       
+      if(storedAcquisitionPackageData && storedAcquisitionPackageData.length > 0){
+           const parsedData = JSON.parse(storedAcquisitionPackageData) as AcquisitionPackageDTO;
+           this.setAcquisitionPackage(parsedData);
+      }
+      else{
+
+           try {
+            const acquisitionPackage = await api.acquisitionPackages.create();
+             if(acquisitionPackage){
+               this.setAcquisitionPackage(acquisitionPackage);
+               sessionStorage.setItem(ATAT_ACQUISTION_PACKAGE_KEY, JSON.stringify(acquisitionPackage));
+             }
+             
+           } catch (error) {
+             
+              console.log(`error creating acquisition package ${error}`);
+           }
+
+      }
+  }
 }
 
 const AcquisitionPackage = getModule(AcquisitionPackageStore);
