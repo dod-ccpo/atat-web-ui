a3c29ef343fd5d23d8e7287fd590e8050d05496b
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-03-22 23:02:48 -0700

    ATATDialog Test

---
+++ src/components/ATATDialog.spec.ts
@@ -0,0 +1,61 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import {createLocalVue, mount, Wrapper} from "@vue/test-utils";
+import ATATDialog from "@/components/ATATDialog.vue";
+import {DefaultProps} from "vue/types/options";
+
+
+Vue.use(Vuetify);
+
+describe("Testing ATATDialog Component", () => {
+  const localVue = createLocalVue();
+  let vuetify: Vuetify;
+  let wrapper: Wrapper<DefaultProps & Vue, Element>;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    jest.useFakeTimers();
+    wrapper = mount(ATATDialog, {
+      localVue,
+      vuetify,
+      propsData: {
+        title: '',
+      }
+    });
+  });
+  it("renders successfully", async () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  describe("PROPS", () => {
+    it("optional", async()=>{
+      await wrapper.setProps({title: `Bacon ipsum dolor amet ball tip minim shoulder
+         buffalo t-bone eu.Strip steak nostrud pancetta ullamco
+         tongue irure`});
+      expect(wrapper.find(".modalDialogTitle")).toBeDefined;
+    });
+  });
+
+  describe("CancelBtn", () => {
+    it("fires off cancel event", async() => {
+      await wrapper.setProps({showDialog: true});
+      jest.advanceTimersByTime(100000)
+      expect(wrapper.find('#dialog_cancel')).toBeDefined()
+
+      const cancelBtn = wrapper.find('#dialog_cancel')
+      cancelBtn.trigger('click')
+      expect(wrapper.vm.cancelItem).toBeDefined()
+    })
+  })
+
+  describe("okBtn", () => {
+    it("fires off onOk event", async() => {
+      await wrapper.setProps({showDialog: true});
+      expect(wrapper.find('#dialog_ok')).toBeDefined()
+
+      const okBtn = wrapper.find('#dialog_ok')
+      okBtn.trigger('click')
+      expect(wrapper.vm.onOK).toBeDefined()
+    })
+  })
+});
