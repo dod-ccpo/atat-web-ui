6f65232404635c4d05e7e4286677655fd2a2af58
Tony Sumner <tony.sumner-ctr@ccpo.mil>
2022-03-28 09:50:43 -0400

    Vuetify datepicker typescripted

--- src/components/ATATDatePicker.vue
+++ src/components/ATATDatePicker.vue
@@ -1,82 +1,45 @@
-<template>
-  <div :id="id + 'DatePickerContainer'" class="atat-date-picker">
-    <v-menu
-      ref="date-picker-menu"
-      v-model="menu"
-      :close-on-content-click="false"
-      transition="scale-transition"
-      min-width="auto"
-      nudge-bottom="getMenuTop"
-      :attach="'#' + id + 'DatePickerContainer'"
-      absolute
-      :nudge-top="0"
-      :nudge-left="0"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <div class="d-flex align-center" v-if="label">
-          <label
-            :id="id + 'DatePickerLabel'"
-            class="form-field-label mb-2 mr-2"
-            :for="id + 'DatePickerTextField'"
-          >
-            {{ label }}
-            <span v-if="optional" class="optional"> Optional </span>
-          </label>
-          <ATATTooltip
-            :tooltipText="tooltipText"
-            :tooltipTitle="tooltipTitle"
-            :id="id"
-            :label="label"
-          />
-        </div>
-        <v-text-field
-          ref="atat-date-picker"
-          :id="id + 'DatePickerTextField'"
-          :height="42"
-          :placeholder="placeHolder"
-          class="text-primary input-max-width d-flex align-center"
-          :hide-details="true"
-          v-model="getSelectedDate"
-          outlined
-          :style="'width: ' + width + 'px'"
-          dense
-          v-bind="attrs"
-          v-on="on"
-          @input="$emit('update:date', $event.target.value)"
-          @mousedown="toggleMenu"
-        >
-          <template slot="append-outer">
-            <v-btn
-              icon
-              :id="id + 'DatePickerButton'"
-              aria-label="Open calendar to select date"
-              @click="toggleMenu"
-              class="pa-0 icon-28 ml-2"
-            >
-              <v-icon 
-                :id="id + 'DatePickerButtonIcon'" 
-                class="icon-28 text-base-darkest">
-                calendar_today
-              </v-icon>
-            </v-btn>
-          </template>
-        </v-text-field>
-      </template>
-      <v-date-picker
-        :id="id + 'DatePicker'"
-        v-model="selectedDate"
-        @click:date="updateDateProperty"
-        :show-adjacent-months="showAdjacentMonths"
-        no-title
-        scrollable
-      >
-      </v-date-picker>
-    </v-menu>
-  </div>
-</template>
 
+<template>
+   <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+        lg="6"
+      >
+        <v-menu
+          ref="menu1"
+          v-model="menu1"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          max-width="290px"
+          min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="dateFormatted"
+              label="Date"
+              hint="MM/DD/YYYY format"
+              persistent-hint
+              prepend-icon="mdi-calendar"
+              v-bind="attrs"
+              @blur="date = parseDate(dateFormatted)"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="date"
+            no-title
+            @input="menu1 = false"
+          ></v-date-picker>
+        </v-menu>
+        <p>Date in ISO format: <strong>{{ date }}</strong></p>
+      </v-col>
+    </v-row>
+   </v-container>
+</template>
 <script lang="ts">
-import { Component, Prop, PropSync } from "vue-property-decorator";
+import { Component, Prop, PropSync, Watch } from "vue-property-decorator";
 import Vue from "vue";
 import Inputmask from "inputmask";
 import { format, isValid } from "date-fns";
@@ -88,86 +51,35 @@ import ATATTooltip from "@/components/ATATTooltip.vue";
   },
 })
 export default class ATATDatePicker extends Vue {
-  //props
-  @Prop({ default: "" }) private label!: string;
-  @Prop({ default: "" }) private id!: string;
-  @Prop({ default: false }) private optional!: boolean;
-  @PropSync("date", {
-    default: format(
-      new Date(new Date().valueOf() + new Date().getTimezoneOffset() * 60000),
-      "yyyy-MM-dd"
-    ),
-  })
-  private _date!: string;
-  @Prop({ default: "" }) private placeHolder!: string;
-  @Prop({ default: true }) private showAdjacentMonths!: boolean;
-  @Prop({ default: "" }) private width!: string;
+  private date = (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10);
+  private dateFormatted = this.formatDate((new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10));
+  private menu1 = false;
 
-  // data
-  private menu = false;
-  private selectedDate = "";
-  private dateFormat = "MM/dd/yyyy";
-  private menuTop = "0";
-
-  //getters
-  set getSelectedDate(value: string) {
-    this.selectedDate = isValid(value)
-      ? format(new Date(value), this.dateFormat)
-      : "";
+  get computedDateFormatted (): string {
+    return this.formatDate(this.date)
   }
 
-  get getSelectedDate(): string {
-    let selectedDate = new Date(this.selectedDate);
-    const dt = isValid(selectedDate)
-      ? new Date(
-          selectedDate.valueOf() + selectedDate.getTimezoneOffset() * 60000
-        )
-      : new Date();
-    return this.formatDate(dt);
+    // watch: {
+  @Watch("date")
+  protected formatDateWatcher(newVal: string): void {
+    this.dateFormatted = this.formatDate(this.date)
+  }
+    
+
+  
+  private formatDate (date: string): string {
+    if (!date) return ''
+
+    const [year, month, day] = date.split('-')
+    return `${month}/${day}/${year}`
   }
 
-  get getMenutop(): string{
-    return this.label !== "" ? "80" : "40";
-  }
+  private parseDate (date: string): string {
+    if (!date) return "";
 
-  //events
-  private updateDateProperty(): void {
-    this.$emit("update:date", this.formatDate(new Date(this.selectedDate)));
+    const [month, day, year] = date.split('/')
+    return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
   }
-
-  //functions
-  /**
-   * toggle menus based on value of this.menu
-   */
-  private toggleMenu(): void {
-    this.menu = !this.menu;
+    
   }
-
-  private formatDate(date: Date): string {
-    return format(
-      new Date(date.valueOf() + date.getTimezoneOffset() * 60000),
-      this.dateFormat
-    );
-  }
-
-  /**
-   * mask input date text boxes with MM/DD/YYYY
-   */
-  private addMasks(): void {
-    [this.id + "DatePickerTextField"].forEach((tbId) => {
-      Inputmask({
-        alias: "datetime",
-        inputFormat: "mm/dd/yyyy",
-        placeholder: "MM/DD/YYYY",
-        outputFormat: "MM/DD/YYYY",
-        nullable: true,
-      }).mask(document.getElementById(tbId) as HTMLElement);
-    });
-  }
-
-  //lifecycle hooks
-  private mounted(): void {
-    this.addMasks();
-  }
-}
 </script>
