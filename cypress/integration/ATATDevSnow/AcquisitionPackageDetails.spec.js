e5727e56aa4724b30e741d71bc1a458ca9e7944f
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-04-05 09:04:30 -0400

    Removed commented out test

--- cypress/integration/ATATDevSnow/AcquisitionPackageDetails.spec.js
+++ cypress/integration/ATATDevSnow/AcquisitionPackageDetails.spec.js
@@ -98,37 +98,6 @@ describe("Test suite: Acquisition Package ", () => {
         cy.btnExists(projectOverview.cancelBtn, " Cancel ");
     });
 
-    // it("TC4: Surge Capabilities-Asserts and Validations Tell us more about the scope of your project", () => {
-    //
-    // SURGE CAPABILITIES MOVED TO STEP 10
-    //
-    //     cy.fillNewAcquisition(projectDetails.projectTitle + "001", projectDetails.scope)
-    //     // Navigates to "Tell us more about the scope of your project"
-    //     cy.textExists(common.header, "Tell us more about the scope of your project");
-        
-    //     //Label of the view
-    //     cy.textExists(financialDetails.surgeCapabilitiesTxt, "Surge Capabilities");
-        
-    //     // ContractPricePercentage text
-    //     cy.textExists(financialDetails.contractPriceTxt," If surge capabilities are required, what percentage of the contractor’s total proposed price will not be exceeded? ");
-        
-    //     //Enter the aplha numeric value to validate the error message 
-    //     cy.fillSurgeCapabilities(projectDetails.invalidTextContractPercentage);   
-        
-    //     //Enter the value more than 50  to validate the error message
-    //     cy.fillSurgeCapabilities(projectDetails.invalidTextContractPercentage);    
-        
-    //     //Enter the value more than 50  to validate the error message
-    //     cy.fillSurgeCapabilities(projectDetails.invalidLessNumericContractPercentage);
-
-    //     //buttons that exists on the view
-    //     cy.btnExists(common.continueBtn, " Continue ");
-    //     cy.btnExists(common.backBtn, "Back");
-        
-    //     //Enter the Valid Percentage
-    //     cy.fillSurgeCapabilities(projectDetails.validContractPercentage,"continue");
-    // });
-
     it("TC4: Organization: Next,we'll gather information about your organization & Address Type is Foreign", () => {
     
         cy.fillNewAcquisition(projectDetails.projectTitle1, projectDetails.scope1);
