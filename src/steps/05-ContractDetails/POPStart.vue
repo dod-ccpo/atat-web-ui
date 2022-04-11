16a311cd3388aba2c0280f6a03f6784c5d121974
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-04-11 11:39:39 -0700

    page ready for PR

--- src/steps/05-ContractDetails/POPStart.vue
+++ src/steps/05-ContractDetails/POPStart.vue
@@ -16,15 +16,35 @@
               class="copy-max-width max-width-740"
               id="PoPStartDate"
               :card="true"
-              :items="startDateOptions"
-              :value.sync="selectedStartDateOption"
+              :items="startPoPDateOptions"
+              :value.sync="selectedPoPStartDateOption"
             />
           </div>
-          <div>
+          <div v-if="selectedPoPStartDateOption ==='YesStartDate'">
             <hr class="my-9"/>
-            <p class="mb-10">
+            <p class="mb-2">
               Requested start date
             </p>
+            <div class="copy-max-width d-flex mb-9">
+              <ATATSelect
+                id="RequestDateOption"
+                class="mr-7"
+                label=""
+                :items="requestDateOptions"
+                :selectedValue.sync="selectedRequestDateOption"
+                style="max-width: 196px"
+              />
+              <ATATDatePicker id="RequestDatePicker" class="mt-2"/>
+            </div>
+            <ATATAlert v-if="selectedRequestDateOption === 'Not later than' " type="warning">
+              <template slot="content">
+                <p>All efforts will be made to accommodate your requested period of performance
+                  start date. However, there is no guarantee that the award will be made by said
+                  date. Normal contracting lead times and/or complexity of requirements may prevent
+                  meeting the requested date.
+                </p>
+              </template>
+            </ATATAlert>
           </div>
         </v-col>
       </v-row>
@@ -35,20 +55,24 @@
 <script lang="ts">
 import Vue from "vue";
 import {Component} from "vue-property-decorator";
-
-import ATATRadioGroup from "@/components/ATATRadioGroup.vue"
-
-import {RadioButton} from "../../../types/Global";
+import ATATAlert from "@/components/ATATAlert.vue";
+import ATATDatePicker from "@/components/ATATDatePicker.vue";
+import ATATRadioGroup from "@/components/ATATRadioGroup.vue";
+import ATATSelect from "@/components/ATATSelect.vue";
+import {RadioButton, SelectData} from "../../../types/Global";
 
 @Component({
   components: {
+    ATATAlert,
+    ATATDatePicker,
     ATATRadioGroup,
+    ATATSelect,
   },
 })
 
 export default class POPStart extends Vue {
-  private selectedStartDateOption = "";
-  private startDateOptions: RadioButton[] = [
+  private selectedPoPStartDateOption = "";
+  private startPoPDateOptions: RadioButton[] = [
     {
       id: "YesStartDate",
       label: "Yes.",
@@ -59,6 +83,17 @@ export default class POPStart extends Vue {
       label: "No. The PoP should start upon execution of the task order.",
       value: "NoStartDate",
     },
+  ];
+  private selectedRequestDateOption = "";
+  private requestDateOptions: SelectData[] = [
+    {
+      text: 'No sooner than',
+      value: "No sooner than"
+    },
+    {
+      text: 'Not later than',
+      value: "Not later than"
+    }
   ];
 }
 </script>
