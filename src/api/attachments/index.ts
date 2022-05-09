2f99cc768a9e7537bf0b36d5c30aa18113b147b2
robertmccardell-CCPO <robert.mccardell-ctr@ccpo.mil>
2022-05-09 15:33:49 -0500

    updates to create a generic api base

---
+++ src/api/attachments/index.ts
@@ -0,0 +1,7 @@
+import { ApiBase } from "../apiBase";
+import baseApi from "../base";
+export class AttachmentApi extends ApiBase {
+  constructor(endpointName: string) {
+    super(endpointName);
+  }
+}
