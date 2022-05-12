4aa52026344947fd7e63b5b6568f184e3c2a4982
Prasanthi Alaparthi <prasanthi.alaparthi-ctr@ccpo.mil>
2022-05-12 12:37:19 -0400

    GFE-Property details validations [x] Test case to verify the validation for property requirements screen. AT-7387

--- cypress/integration/ATATDevSnow/GovernmentFurnishedEquipment.spec.js
+++ cypress/integration/ATATDevSnow/GovernmentFurnishedEquipment.spec.js
@@ -77,4 +77,16 @@ describe("Test suite: Government Furnished Equipment", () => {
       .and('have.css', 'color', colors.primary);
       
   });
+
+  it("TC4: Validations: Will this be a future recurring requirement?", () => {
+    cy.clickSideStepper(common.stepGovFurEquipLink, " Government Furnished Equipment ");
+    cy.textExists(
+      common.header,
+      " Will government equipment be furnished, provided or acquired under this acquisition? ");
+    //No radio button option is selected
+    cy.radioBtn(govFurEquip.yesRadioOption, "true").focus().tab().blur().then(() => {
+      cy.checkErrorMessage(govFurEquip.govEquipRadioError, "Please select an option");
+    });
+      
+  });
 });
