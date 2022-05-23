0cfe864032d96e44d3f27d668b95d4736263dbe5
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-05-23 06:58:57 -0700

    WIP form has all components

--- src/steps/05-PerformanceRequirements/DOW/Form.vue
+++ src/steps/05-PerformanceRequirements/DOW/Form.vue
@@ -1,62 +1,43 @@
-
 <template>
   <div class="mb-7">
     <v-container fluid class="container-max-width">
       <v-row>
         <v-col class="col-12">
-          <h1 class="page-header">
-            Let’s work on your performance requirements
-          </h1>
           <div class="copy-max-width">
-            <p class="mb-10">
-              Through JWCC, you have the ability to procure many offerings for
-              Anything as a Service (XaaS) and Cloud Support Packages. Specify
-              any categories that may apply to your acquisition below, and we’ll
-              walk through each selection to get more details next.
-              <a
-                role="button"
-                @click="openSlideoutPanel"
-                @keydown.enter="openSlideoutPanel"
-                @keydown.space="openSlideoutPanel"
-                tabindex="0"
-              >
-                Learn more about categories.
-              </a>
-            </p>
+            <div v-for="(instance, index) in instances" :key="instance.classification.name">
+              <p v-if="instanceLength > 1" id="RequirementHeading">
+                <span>{{index + 1}}</span>
+                Tell us about the<span>{{instance.classification.name}}</span>instance
+              </p>
 
-            <p id="XaaSLabel" class="_checkbox-group-label">
-              What type of XaaS resources, tools and services do you need?
-            </p>
-
-            <ATATCheckboxGroup
-              id="XaaSCheckboxes"
-              aria-describedby="XaaSLabel"
-              :value.sync="xaasSelectedOptions"
-              :items="xaasCheckboxItems"
-              :card="false"
-              class="copy-max-width"
-              :rules="[
-                $validators.required('Please select at least one option.')
-              ]"
-            />
-
-            <hr />
-
-            <p id="CloudSupportLabel" class="_checkbox-group-label">
-              What type(s) of cloud support packages do you need?
-            </p>
-            <ATATCheckboxGroup
-              id="CloudSupportCheckboxes"
-              aria-describedby="CloudSupportLabel"
-              :value.sync="cloudSupportSelectedOptions"
-              :items="cloudSupportCheckboxItems"
-              :card="false"
-              class="copy-max-width"
-              :rules="[
-                $validators.required('Please select at least one option.')
-              ]"
-            />
-
+              <ATATTextArea
+                id="OperationToBePerformed"
+                label="Describe the anticipated need and usage of this requirement"
+                class="width-100"
+                :rows="7"
+                :value.sync="instance.description"
+                maxChars="500"
+              />
+              <ATATRadioGroup
+                class="copy-max-width mb-10"
+                id="RequirementRadioOptions"
+                legend="Is this requirement for the entire duration of your task order?"
+                :items="requirementOptions"
+                :value.sync="instance.neededForEntireDuration"
+              />
+              <p id="CloudSupportLabel" class="_checkbox-group-label">
+                Which base and/or option periods do you need this requirement?
+              </p>
+              <ATATCheckboxGroup
+                id="CloudSupportCheckboxes"
+                aria-describedby="CloudSupportLabel"
+                :value.sync="instance.periods"
+                :items="checkboxItems"
+                :card="false"
+                class="copy-max-width"
+              />
+              <hr />
+            </div>
           </div>
         </v-col>
       </v-row>
@@ -70,6 +51,7 @@ import { Component, Prop } from "vue-property-decorator";
 import ATATCheckboxGroup from "@/components/ATATCheckboxGroup.vue";
 import ATATRadioGroup from "@/components/ATATRadioGroup.vue";
 import ATATTextArea from "@/components/ATATTextArea.vue";
+import { Checkbox, RadioButton, stringObj } from "../../../../types/Global";
 
 
 @Component({
@@ -82,7 +64,68 @@ import ATATTextArea from "@/components/ATATTextArea.vue";
 
 export default class Form extends Vue {
   // props
-  @Prop({ default: true }) private dense!: boolean;
+  @Prop({default: () => []}) private data!: stringObj;
+  public instances = [
+    {
+      classification: {
+        name: "Unclassified / Impact Level 2 (IL2)",
+        value: "IL2",
+      },
+      description: "",
+      neededForEntireDuration: null,
+      periods: []
+    },
+    {
+      classification: {
+        name: "Unclassified / Impact Level 2 (IL2)",
+        value: "IL2",
+      },
+      description: "",
+      neededForEntireDuration: null,
+      periods: []
+    }
+  ]
+  private requirementOptions: RadioButton[] = [
+    {
+      id: "Yes",
+      label: `Yes`,
+      value: "YES",
+    },
+    {
+      id: "No",
+      label: `No`,
+      value: "NO",
+    },
+  ];
+  private checkboxItems: Checkbox[] = [
+    {
+      id: "BasePeriod",
+      label: "Base period",
+      value: "BasePeriod",
+    },
+    {
+      id: "OptionPeriod1",
+      label: "Option Period 1",
+      value: "OptionPeriod1",
+    },
+    {
+      id: "OptionPeriod2",
+      label: "Option Period 2",
+      value: "OptionPeriod2",
+    },
+    {
+      id: "OptionPeriod3",
+      label: "Option Period 3",
+      value: "OptionPeriod3",
+    },
+    {
+      id: "OptionPeriod4",
+      label: "Option Period 4",
+      value: "OptionPeriod4",
+    },
+  ];
+
+  private instanceLength = this.instances.length
 
 }
 </script>
