8a952e5e11ac1e6f018fe5cee77d4d320cbdf0e7
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-06-17 15:12:19 -0400

    Fixed NaN if tabbing out of amount with field blank

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -66,6 +66,7 @@ export const sanitizeOfferingName = (offeringName: string): string => {
 
 // formats a number to currency string with commas and 2 decimal places
 export const toCurrencyString = (num: number): string => {
+  num = isNaN(num) ? 0 : num;
   return num.toLocaleString(
     undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }
   );
