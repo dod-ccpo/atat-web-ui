bf1cc396681d41630ead3c7db82c6a1401dbcb32
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-06-22 16:51:23 -0700

    init push

---
+++ src/components/ATATContactForm.vue
@@ -0,0 +1,223 @@
+
+<template>
+  <section class="mt-10">
+    <hr />
+    <h2 id="ContactInfoHeader" class="form-section-heading">
+      Your {{ corOrAcor }}’s Contact Information
+    </h2>
+
+    <ATATRadioGroup
+      id="ContactAffiliation"
+      :legend="
+        'What role best describes your ' +
+        corOrAcor +
+        '’s affiliation with the DoD?'
+      "
+      :items="contactRoles"
+      :value.sync="_selectedRole"
+      :rules="[
+        $validators.required('Please enter your ' + corOrAcor + '’s role.'),
+      ]"
+      class="mb-10"
+      @radioButtonSelected="contactTypeChange"
+    />
+
+    <v-form ref="CORACORContactForm">
+      <ATATSelect
+        id="Branch"
+        v-show="_selectedRole === 'MILITARY'"
+        v-model="_selectedBranch"
+        class="_input-max-width mb-10"
+        label="Service Branch"
+        placeholder=""
+        :items="branchData"
+        :selectedValue.sync="_selectedBranch"
+        :showAccessRadioButtons.sync="_showAccessRadioButtons"
+        :returnObject="true"
+        :rules="[
+          $validators.required(
+            'Please select your ' + corOrAcor + '’s service branch.'
+          ),
+        ]"
+      />
+
+      <div v-show="(_selectedBranch && _selectedBranch.value) || _selectedRole === 'CIVILIAN'">
+        <ATATAutoComplete
+          id="Rank"
+          v-show="_selectedRole === 'MILITARY'"
+          label="Rank"
+          titleKey="name"
+          :items="selectedBranchRanksData"
+          :searchFields="['name', 'grade']"
+          :selectedItem.sync="_selectedRank"
+          :rules="[
+            $validators.required(
+              'Please select your ' + corOrAcor + '’s rank.'
+            ),
+          ]"
+          class="_input-max-width mb-7"
+          icon="arrow_drop_down"
+        />
+
+        <ATATSelect
+          id="Salutation"
+          v-show="_selectedRole === 'CIVILIAN'"
+          class="_input-max-width mb-7"
+          label="Salutation"
+          :optional="true"
+          placeholder=""
+          :items="salutationData"
+          :selectedValue.sync="_selectedSalutation"
+        />
+
+        <v-row class="form-section mb-7">
+          <v-col class="col-12 col-lg-3">
+            <ATATTextField
+              label="First name"
+              id="FirstName"
+              class="_input-max-width"
+              :value.sync="_firstName"
+              :rules="[
+                $validators.required(
+                  'Please enter your first name.'
+                ),
+              ]"
+            />
+          </v-col>
+          <v-col class="col-12 col-lg-3">
+            <ATATTextField
+              label="Middle name"
+              id="MiddleName"
+              :optional="true"
+              class="_input-max-width"
+              :value.sync="_middleName"
+            />
+          </v-col>
+          <v-col class="col-12 col-lg-3">
+            <ATATTextField
+              label="Last name"
+              id="LastName"
+              class="_input-max-width"
+              :value.sync="_lastName"
+              :rules="[
+                $validators.required(
+                  'Please enter your last name.'
+                ),
+              ]"
+            />
+          </v-col>
+          <v-col class="col-12 col-lg-3">
+            <ATATTextField
+              label="Suffix"
+              id="Suffix"
+              :optional="true"
+              width="80"
+              :value.sync="_suffix"
+            />
+          </v-col>
+        </v-row>
+
+        <ATATPhoneInput
+          id="PhoneNumber"
+          label="Phone number"
+          class="width-100 mb-10"
+          :value.sync="_phone"
+          :country.sync="_selectedPhoneCountry"
+          :extensionValue.sync="_phoneExt"
+          :rules="[
+            $validators.required(
+              'Please enter your phone number'
+            ),
+            $validators.isPhoneNumberValid(this._selectedPhoneCountry),
+          ]"
+        />
+
+        <ATATTextField
+          id="EmailAddress"
+          label="Email address"
+          class="_input-max-width mb-10"
+          helpText="Enter a .mil or .gov email address."
+          :value.sync="_email"
+          :rules="[
+            $validators.required(
+              'Please enter your email address.'
+            ),
+            $validators.isEmail(),
+          ]"
+        />
+      </div>
+    </v-form>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop, PropSync } from "vue-property-decorator";
+import ATATAutoComplete from "@/components/ATATAutoComplete.vue";
+import ATATPhoneInput from "@/components/ATATPhoneInput.vue";
+import ATATRadioGroup from "@/components/ATATRadioGroup.vue";
+import ATATSelect from "@/components/ATATSelect.vue";
+import ATATTextField from "@/components/ATATTextField.vue";
+
+import { RadioButton, SelectData, RankData } from "../../../../types/Global";
+
+@Component({
+  components: {
+    ATATAutoComplete,
+    ATATPhoneInput,
+    ATATRadioGroup,
+    ATATSelect,
+    ATATTextField,
+  },
+})
+export default class ATATContactForm extends Vue {
+  $refs!: {
+    CORACORContactForm: Vue & {
+      resetValidation: () => void;
+      reset: () => void;
+    };
+  };
+
+  //props
+
+  @Prop() private branchData!: SelectData[];
+  @Prop() private selectedBranchRanksData!: SelectData[];
+
+  @PropSync("showAccessRadioButtons") private _showAccessRadioButtons!: boolean;
+  @PropSync("selectedPhoneCountry") private _selectedPhoneCountry?: string;
+
+  @PropSync("selectedRole") private _selectedRole?: string;
+  @PropSync("selectedRank") private _selectedRank?: RankData;
+  @PropSync("selectedBranch") private _selectedBranch?: SelectData;
+  @PropSync("selectedSalutation") private _selectedSalutation?: SelectData;
+  @PropSync("firstName") private _firstName?: string;
+  @PropSync("middleName") private _middleName?: string;
+  @PropSync("lastName") private _lastName?: string;
+  @PropSync("suffix") private _suffix?: string;
+  @PropSync("email") private _email?: string;
+  @PropSync("phone") private _phone?: string;
+  @PropSync("phoneExt") private _phoneExt?: string;
+
+  // data
+
+  private salutationData: SelectData[] = [
+    { text: "Mr.", value: "MR" },
+    { text: "Mrs.", value: "MRS" },
+    { text: "Miss", value: "MISS" },
+    { text: "Ms.", value: "MS" },
+    { text: "Dr.", value: "DR" },
+  ];
+  private contactRoles: RadioButton[] = [
+    {
+      id: "Military",
+      label: "Military",
+      value: "MILITARY",
+    },
+    {
+      id: "Civilian",
+      label: "Civilian",
+      value: "CIVILIAN",
+    },
+  ];
+}
+</script>
