9bf194035d72c2abe571d88faa50eb38f3887235
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-06-23 15:39:29 -0400

    Added search button and error alert

--- src/components/ATATSearch.vue
+++ src/components/ATATSearch.vue
@@ -16,41 +16,71 @@
         :label="label"
       />
     </div>
-
-    <v-text-field
-      :id="id + '_SearchInput'"
-      class="_search-input"
-      clearable
-      @input="onInput"
-      outlined
-      dense
-      :height="42"
-      :value.sync="_value"
-      :placeholder="placeHolder"
+    <div 
+      class="d-flex"
       :style="'width: ' + width + 'px'"
+    >
+      <v-text-field
+        :id="id + '_SearchInput'"
+        class="_search-input"
+        clearable
+        @input="onInput"
+        outlined
+        dense
+        :height="40"
+        :value.sync="_value"
+        :placeholder="placeHolder"
 
-      autocomplete="off"
+        autocomplete="off"
 
-    />
-    <v-btn class="primary">
-      <ATATSVGIcon 
-        name="search"
-        color="white"
-        width="18"
-        height="18"
       />
-    </v-btn>
+      <v-btn
+        :id="id + '_SearchButton'" 
+        class="primary _search-button"
+      >
+        <ATATSVGIcon 
+          name="search"
+          color="white"
+          width="18"
+          height="18"
+        />
+      </v-btn>
+    </div>
+
+    <ATATAlert
+      :id="id + '_SearchAlert'"
+      type="error"
+      v-show="error"
+      maxWidth="660"
+    >
+      <template v-slot:content>
+        <p>
+          We could not find your order within G-Invoicing. Please enter a valid 
+          order number and search again. 
+        </p>
+        <p class="mb-0">
+          If you confirmed your order number within G-Invoicing and continue to 
+          receive this message, please reach out to our User Engagement Team for 
+          support.          
+        </p>
+      </template>
+    </ATATAlert>
+
+
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { Component, Prop, PropSync } from "vue-property-decorator";
+import ATATAlert from "@/components/ATATAlert.vue";
 import ATATTooltip from "@/components/ATATTooltip.vue"
 import ATATSVGIcon from "@/components/icons/ATATSVGIcon.vue";
 
+
 @Component({
   components: {
+    ATATAlert,
     ATATSVGIcon,
     ATATTooltip,
   },
@@ -59,13 +89,14 @@ import ATATSVGIcon from "@/components/icons/ATATSVGIcon.vue";
 export default class ATATSearch extends Vue {
   @Prop({ default: "Search" }) private id!: string;
   @Prop({ default: "" }) private placeHolder!: string;
-  @Prop({ default: "272" }) private width!: string;
+  @Prop({ default: "320" }) private width!: string;
   @Prop({ default: "" }) private label!: string;
   @Prop({ default: "" }) private tooltipTitle!: string;
   @Prop({ default: "" }) private tooltipText!: string;
 
   @PropSync("value", { default: "" }) private _value!: string;
 
+  private error = true;
   private errorMessages: string[] = [];
   private onInput(v: string) {
     this._value = v;
