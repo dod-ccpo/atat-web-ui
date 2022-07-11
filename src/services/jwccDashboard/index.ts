aac5e621964a79a4b20d2cdde79e8dc9d75b37cd
robertmccardell-CCPO <robert.mccardell-ctr@ccpo.mil>
2022-07-11 14:30:01 -0500

    Updates

---
+++ src/services/jwccDashboard/index.ts
@@ -0,0 +1,11 @@
+import api from "@/api";
+import { ClinDTO, CostsDTO, TaskOrderDTO } from "@/api/models";
+import { AxiosRequestConfig } from "axios";
+
+export class JWCCDashboardService {
+
+  public async getFundSpentToDate():Promise<void>{
+
+    throw new Error('not implemented');
+  }
+}
