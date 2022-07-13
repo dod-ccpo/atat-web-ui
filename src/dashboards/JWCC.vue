1df657d0ad1fe8b40f7a88fa6b94a07e5074a707
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-07-13 12:42:37 -0400

    Added bar chart component

--- src/dashboards/JWCC.vue
+++ src/dashboards/JWCC.vue
@@ -33,6 +33,17 @@
                     class="_no-shadow v-sheet--outlined height-100 pa-8"
                   >
                     <h3 class="mb-6">Monthly Spend Summary</h3>
+                    <p>
+                      Compare total funds spent last month on all JWCC portfolios 
+                      against the end-of-month forecast for this month.
+                    </p>
+                    <BarChart 
+                      chartId="MonthlySpendBarChart"
+                      :chartData="barChartMonthlySpendData"
+                      :chartOptions="barChartMonthlySpendOptions"
+                      :useChartDataLabels="true"
+                    />
+
                   </v-card>
                 </v-col>
                 <v-col cols="5">
@@ -43,6 +54,18 @@
                     <h3 class="mb-6">Funds Spent by Cloud Service Provider</h3>
                   </v-card>
                 </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col>
+                  <v-card
+                    id="FundsSpentByOrgCard" 
+                    class="_no-shadow v-sheet--outlined height-100 pa-8"
+                  >
+                    <h3 class="mb-6">Funds Spent by Organization</h3>
+                  </v-card>
+                </v-col>
+
               </v-row>
 
             </div>
@@ -61,14 +84,97 @@ import { Component } from "vue-property-decorator";
 
 import ATATFooter from "../components/ATATFooter.vue";
 import ATATPageHead from "../components/ATATPageHead.vue";
+import BarChart from "../components/charts/BarChart.vue";
+
+import ATATCharts from "@/store/charts";
 
 @Component({
   components: {
     ATATFooter,
-    ATATPageHead
+    ATATPageHead,
+    BarChart,
   }
 })
 
-export default class JWCCDashboard extends Vue {}
+export default class JWCCDashboard extends Vue {
+  public chartDataColors = ATATCharts.chartDataColors;
+  public chartDataColorSequence = ATATCharts.chartDataColorSequence;
+  public chartDataColorsTranslucent = this.chartDataColorSequence.map((color) => {
+    return color + "33";
+  });
+  public chartAuxColors = ATATCharts.chartAuxColors;
+
+  public barChartMonthlySpendData = {
+    labels: [["Last Month","(April 2022)"], ["End-of-month Forecast", "(May 2022)"]],
+    datasets: [
+      {
+        barPercentage: 0.5,
+        barThickness: 100,
+        maxBarThickness: 100,
+        minBarLength: 2,
+        data: [83123, 95987],
+        backgroundColor: [this.chartDataColorSequence[0], this.chartDataColorSequence[0]],
+        datalabels: {
+          color: "#161b1e",
+          anchor: "end",
+          align: "top",
+          offset: "6",
+          font: {
+            family: "Roboto",
+            weight: "500",
+            size: "14"
+          }
+        }
+      }
+    ]
+  };
+
+  public barChartMonthlySpendOptions = {
+    plugins: {
+      legend: { display: false },
+      datalabels: {
+        formatter: function(value: number): string {
+          if (!isNaN(value)) {
+            return "$" + value.toLocaleString(
+              undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 }
+            );
+          }
+          return "";
+        }
+      }
+    },
+    animation: { duration: 0 },
+    title: {
+      display: true,
+      text: 'Foo and Bar'
+    },
+    scales: {
+      x: {
+        grid: {
+          borderWidth: 2,
+          borderColor: this.chartAuxColors["lineChart-axis"],
+          tickWidth: 0,
+          color: "transparent",
+
+        }
+      },
+      y: {
+        min: 0,
+        max: 125000,
+        grid: {
+          borderColor: "transparent",
+          tickWidth: 0,
+        },
+        ticks: {
+          stepSize: 25000,
+          callback: function (value: number): string {
+            return "$" + Math.round(value / 1000) + "k";
+          },
+        },
+      },
+    }
+  }
+
+}
 
 </script>
