b3481e4c3a5d15215f781e5655da461c60413dd8
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-07-13 09:57:57 -0400

    Completed stubbed data

--- src/dashboards/JWCC.vue
+++ src/dashboards/JWCC.vue
@@ -27,7 +27,64 @@
                             Across all organizations, task orders, and portfolios
                           </p>
                         </div>
-                        Active task orders
+
+                        <div class="d-flex justify-space-between mb-5">
+                          <div class="d-flex align-center">
+                            Active task orders
+                          </div>
+                          <div class="font-weight-700">
+                            500
+                          </div>
+                        </div>
+
+                        <div class="d-flex justify-space-between mb-5">
+                          <div class="d-flex align-flex-start">
+                            <span class="d-inline-block">
+                              Total obligated funds
+                            </span>
+                            <ATATTooltip
+                              :tooltipText="totalObligatedFundsTooltipText"
+                              buttonStyle="margin-top: 2px"
+                              buttonClass="ml-1"
+                            />
+                          </div>
+                          <div class="font-weight-700">
+                            $500,000,000
+                          </div>
+                        </div>
+
+                        <div class="d-flex justify-space-between mb-5">
+                          <div class="d-flex align-flex-start">
+                            <span class="d-inline-block">
+                              Total value of JWCC<br />
+                              task orders
+                            </span>
+                            <ATATTooltip
+                              :tooltipText="totalValueJWCCTooltipText"
+                              buttonStyle="margin-top: 2px"
+                              buttonClass="ml-1"
+                            />
+                          </div>
+                          <div class="font-weight-700">
+                            $7,000,000,000
+                          </div>
+                        </div>
+
+                        <div class="d-flex justify-space-between">
+                          <div class="d-flex align-flex-start">
+                            <span class="d-inline-block">
+                              Average monthly spend
+                            </span>
+                            <ATATTooltip
+                              :tooltipText="avgMonthlySpendTooltipText"
+                              buttonStyle="margin-top: 2px"
+                              buttonClass="ml-1"
+                            />
+                          </div>
+                          <div class="font-weight-700">
+                            $1,234,567
+                          </div>
+                        </div>
 
 
                       </v-col>
@@ -82,14 +139,27 @@ import { Component } from "vue-property-decorator";
 
 import ATATFooter from "../components/ATATFooter.vue";
 import ATATPageHead from "../components/ATATPageHead.vue";
+import ATATTooltip from "@/components/ATATTooltip.vue"
 
 @Component({
   components: {
     ATATFooter,
-    ATATPageHead
+    ATATPageHead,
+    ATATTooltip,
   }
 })
 
-export default class JWCCDashboard extends Vue {}
+export default class JWCCDashboard extends Vue {
+  public totalObligatedFundsTooltipText = `This is the legal amount allocated by the 
+    government to fund all task orders awarded under JWCC. This is a portion of the 
+    total value of JWCC task orders.`;
+  public totalValueJWCCTooltipText = `This is the total amount of money requested for 
+    all task orders awarded under JWCC to date. It includes obligated funds that 
+    have been allocated to portfolios, as well as non-obligated funds that could 
+    potentially be exercised in option periods.`;
+  public avgMonthlySpendTooltipText = `Average amount that is spent and invoiced 
+    each month on all JWCC task orders`;
+
+}
 
 </script>
