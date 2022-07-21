cc1d49f2b05d051ae1a531b72c376ce183aed620
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-07-21 13:21:48 -0700

    added description to Classifications

--- src/steps/03-Background/CurrentEnvironment/ClassificationLevelsPage.vue
+++ src/steps/03-Background/CurrentEnvironment/ClassificationLevelsPage.vue
@@ -1,16 +1,69 @@
-
 <template>
   <div>
-    Future Classification Levels page
+    <v-container class="container-max-width" fluid>
+      <v-row>
+        <v-col class="col-12">
+          <h1 class="page-header mb-3">
+            What classification level(s) are your instances deployed in?
+          </h1>
+          <div class="copy-max-width">
+            <p id="IntroP" class="mb-10">
+              If you have instances within two or more classification levels, we will gather details
+              about each instance later.
+            </p>
+            <p id="SelectMessage" class="mb-4">
+              Select all that apply to your contracting effort.
+            </p>
+          </div>
+          <ATATCheckboxGroup
+            id="ClassificationLevelCheckboxes"
+            :card="false"
+            :hasOtherValue="true"
+            :items="checkboxItems"
+            :rules="[
+              $validators.required('Please select at least one classification level.')
+            ]"
+            :value.sync="selectedOptions"
+            class="copy-max-width"
+            name="checkboxes"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
 
 import { Component } from "vue-property-decorator";
+import ATATCheckboxGroup from "@/components/ATATCheckboxGroup.vue";
+import { Checkbox } from "../../../../types/Global";
+import { ClassificationLevelDTO } from "@/api/models";
+import classificationRequirements from "@/store/classificationRequirements";
+import { buildClassificationCheckboxList } from "@/helpers";
+
 @Component({
+  components: {
+    ATATCheckboxGroup,
+  }
 })
-export default class ClassificationLevels extends Vue {
+export default class ClassificationLevelsPage extends Vue {
+  private checkboxItems: Checkbox[] = []
+  public selectedOptions: string[] = [];
+  public classifications: ClassificationLevelDTO[] = []
+
+  private createCheckboxItems(data: ClassificationLevelDTO[]) {
+    return buildClassificationCheckboxList(data, "");
+  }
+
+  public async loadOnEnter(): Promise<void> {
+    this.classifications = await classificationRequirements.getAllClassificationLevels();
+    this.checkboxItems =this.createCheckboxItems(this.classifications)
+  }
+
+  public async mounted(): Promise<void> {
+    await this.loadOnEnter();
+  }
 }
 </script>
 
