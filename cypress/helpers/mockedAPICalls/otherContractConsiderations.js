b399af9a310a309c1830358d022082bcb7d9814c
Tony Sumner <tony.sumner-ctr@ccpo.mil>
2022-07-25 16:58:43 -0400

    [x] refactored trainingCourse test case AT-7763

--- cypress/helpers/mockedAPICalls/otherContractConsiderations.js
+++ cypress/helpers/mockedAPICalls/otherContractConsiderations.js
@@ -213,22 +213,21 @@ const otherContractConsiderations = (fixtureFolder) => {
     ],
     trainingcourse: [
       {
-        'fixture': fixtureFolder + '/taskOrder_POST',
+        'fixture': fixtureFolder + '/TC1_taskOrder_POST_1',
         'apiURL': 'x_g_dis_atat_task_order',
         'action': 'POST',
-        'statusCode': 200,
         'times': 1,
         'testCase' : 1
       },
       {
-        'fixture': fixtureFolder + '/taskOrder_GET',
-        'apiURL': 'x_g_dis_atat_task_order',
-        'action': 'GET',
+        'fixture': fixtureFolder + '/TC1_contractConsiderations_POST_1',
+        'apiURL': 'x_g_dis_atat_contract_considerations',
+        'action': 'POST',
         'times': 1,
         'testCase' : 1
       },
       {
-        'fixture': fixtureFolder + '/contractConsiderations_GET_1',
+        'fixture': fixtureFolder + '/TC1_contractConsiderations_GET_1',
         'apiURL': 'x_g_dis_atat_contract_considerations/**',
         'action': 'GET',
         'statusCode': 200,
@@ -236,35 +235,79 @@ const otherContractConsiderations = (fixtureFolder) => {
         'testCase' : 1
       },
       {
-        'fixture': fixtureFolder + '/contractConsiderations_GET_2',
-        'apiURL': 'x_g_dis_atat_contract_considerations/**',
-        'action': 'GET',
-        'statusCode': 200,
-        'times': 1,
-        'testCase' : 1
-      },
-      {
-        'fixture': fixtureFolder + '/contractConsiderations_POST_1',
-        'apiURL': 'x_g_dis_atat_contract_considerations',
-        'times': 1,
-        'action': 'POST',
-        'testCase' : 1
-      },
-      {
-        'fixture': fixtureFolder + '/contractConsiderations_POST_2',
-        'apiURL': 'x_g_dis_atat_contract_considerations',
-        'times': 1,
-        'action': 'POST',
-        'testCase' : 1
-      },
-      {
-        'fixture': fixtureFolder + '/contractConsiderations_PATCH_1',
+        'fixture': fixtureFolder + '/TC1_contractConsiderations_PATCH_1',
         'apiURL': 'x_g_dis_atat_contract_considerations/**',
         'action': 'PATCH',
         'statusCode': 200,
         'times': 1,
         'testCase' : 1
-      }
+      },
+      {
+        'fixture': fixtureFolder + '/TC2_taskOrder_POST_1',
+        'apiURL': 'x_g_dis_atat_task_order',
+        'action': 'POST',
+        'times': 1,
+        'testCase' : 2
+      },
+      {
+        'fixture': fixtureFolder + '/TC2_contractConsiderations_POST_1',
+        'apiURL': 'x_g_dis_atat_contract_considerations',
+        'action': 'POST',
+        'times': 1,
+        'testCase' : 2
+      },
+      {
+        'fixture': fixtureFolder + '/TC2_contractConsiderations_GET_1',
+        'apiURL': 'x_g_dis_atat_contract_considerations/**',
+        'action': 'GET',
+        'statusCode': 200,
+        'times': 1,
+        'testCase' : 2
+      },
+      {
+        'fixture': fixtureFolder + '/TC3_taskOrder_POST_1',
+        'apiURL': 'x_g_dis_atat_task_order',
+        'action': 'POST',
+        'times': 1,
+        'testCase' : 3
+      },
+      {
+        'fixture': fixtureFolder + '/TC3_contractConsiderations_POST_1',
+        'apiURL': 'x_g_dis_atat_contract_considerations',
+        'action': 'POST',
+        'times': 1,
+        'testCase' : 3
+      },
+      {
+        'fixture': fixtureFolder + '/TC3_contractConsiderations_GET_1',
+        'apiURL': 'x_g_dis_atat_contract_considerations/**',
+        'action': 'GET',
+        'statusCode': 200,
+        'times': 1,
+        'testCase' : 3
+      },
+      {
+        'fixture': fixtureFolder + '/TC4_taskOrder_POST_1',
+        'apiURL': 'x_g_dis_atat_task_order',
+        'action': 'POST',
+        'times': 1,
+        'testCase' : 4
+      },
+      {
+        'fixture': fixtureFolder + '/TC4_contractConsiderations_POST_1',
+        'apiURL': 'x_g_dis_atat_contract_considerations',
+        'action': 'POST',
+        'times': 1,
+        'testCase' : 4
+      },
+      {
+        'fixture': fixtureFolder + '/TC4_contractConsiderations_GET_1',
+        'apiURL': 'x_g_dis_atat_contract_considerations/**',
+        'action': 'GET',
+        'statusCode': 200,
+        'times': 1,
+        'testCase' : 4
+      },
     ]
   }
 }
