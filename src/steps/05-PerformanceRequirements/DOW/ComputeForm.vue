0945335fbb67bdf4b434331c542ff92fb89553c6
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-07-26 18:31:58 -0400

    removed commented lines

--- src/steps/05-PerformanceRequirements/DOW/ComputeForm.vue
+++ src/steps/05-PerformanceRequirements/DOW/ComputeForm.vue
@@ -398,12 +398,7 @@ export default class ComputeForm extends Vue {
 
   public isPeriodsDataMissing = false;
   public isClassificationDataMissing = false;
-  // public selectedPeriods: string[] = [];
-  // public operatingSystemAndLicensing = "";
-  // public numberOfVCPUs = "";
-  // public memory = "";
-  // public storageAmount = "";
-  // public selectedStorageType = "";
+
   public storageTypes: SelectData[] = [
     { text: "General Purpose SSD", value: "General Purpose SSD" },
     { text: "Provisioned IOPS SSD", value: "Provisioned IOPS SSD" },
@@ -533,9 +528,6 @@ export default class ComputeForm extends Vue {
         this.modalSelectedOptions.push(val.sys_id || "")
       });
       this.checkSingleClassification();
-      // if (this.avlClassificationLevelObjects.length === 1) {
-      //   this.setSingleClassification();
-      // }
     }
 
     // set available classification levels for radio buttons if > 1 level selected
