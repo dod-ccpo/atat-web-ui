03ca2b27500ee6a5a67b792250749e9ea56b4b08
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-07-27 15:51:11 -0400

    added v data table

--- src/steps/05-PerformanceRequirements/DOW/ComputeRequirements.vue
+++ src/steps/05-PerformanceRequirements/DOW/ComputeRequirements.vue
@@ -15,14 +15,43 @@
           </p>
 
           <v-data-table
-            :headers="headers"
-            :items="desserts"
+            :headers="tableHeaders"
+            :items="tableData"
             :items-per-page="5"
             class="elevation-1"
-          ></v-data-table>          
+            :hide-default-footer="true"
+          >
+            <template v-slot:item.actions="{ item }">
+              <v-icon
+                small
+                class="mr-2"
+                @click="editItem(item)"
+              >
+              mdi-pencil
+              </v-icon>
+              <v-icon
+                small
+                @click="deleteItem(item)"
+              >
+                mdi-delete
+              </v-icon>
+            </template>
+            <template v-slot:no-data>
+              <v-btn
+                color="primary"
+                @click="initialize"
+              >
+                Reset
+              </v-btn>
+            </template>
+
+          </v-data-table>          
         </v-col>
       </v-row>
     </v-container>
+
+
+    
   </div>
 </template>
 
@@ -41,100 +70,34 @@ export default class ComputeRequirements extends Vue {
   public computeInstances: ComputeData[] = [];
 
 
-  public headers = [
+  public tableHeaders = [
     {
-      text: 'Dessert (100g serving)',
+      text: '#',
       align: 'start',
-      sortable: false,
-      value: 'name',
+      value: 'instanceNumber',
     },
-    { text: 'Calories', value: 'calories' },
-    { text: 'Fat (g)', value: 'fat' },
-    { text: 'Carbs (g)', value: 'carbs' },
-    { text: 'Protein (g)', value: 'protein' },
-    { text: 'Iron (%)', value: 'iron' },
+    { text: 'Location', value: 'location' },
+    { text: 'Quantity', value: 'qty' },
+    { text: 'vCPU', value: 'vCPU' },
+    { text: 'Memory', value: 'memory' },
+    { text: 'Storage', value: 'storage' },
+    { text: 'Performance', value: 'performance' },
+    { text: 'e x', value: 'actions' },
+    // { text: 'Actions', value: 'actions', sortable: false },
+
   ];
-  public desserts = [
+  public tableData = [
     {
-      name: 'Frozen Yogurt',
-      calories: 159,
-      fat: 6.0,
-      carbs: 24,
-      protein: 4.0,
-      iron: '1%',
+      instanceNumber: '1',
+      location: "CONUS East, CONUS West",
+      qty: 1,
+      vCPU: 2,
+      memory: 3,
+      storage: 4,
+      performance: "Medium performance (Standard)",
+      actions: ""
     },
-    {
-      name: 'Ice cream sandwich',
-      calories: 237,
-      fat: 9.0,
-      carbs: 37,
-      protein: 4.3,
-      iron: '1%',
-    },
-    {
-      name: 'Eclair',
-      calories: 262,
-      fat: 16.0,
-      carbs: 23,
-      protein: 6.0,
-      iron: '7%',
-    },
-    {
-      name: 'Cupcake',
-      calories: 305,
-      fat: 3.7,
-      carbs: 67,
-      protein: 4.3,
-      iron: '8%',
-    },
-    {
-      name: 'Gingerbread',
-      calories: 356,
-      fat: 16.0,
-      carbs: 49,
-      protein: 3.9,
-      iron: '16%',
-    },
-    {
-      name: 'Jelly bean',
-      calories: 375,
-      fat: 0.0,
-      carbs: 94,
-      protein: 0.0,
-      iron: '0%',
-    },
-    {
-      name: 'Lollipop',
-      calories: 392,
-      fat: 0.2,
-      carbs: 98,
-      protein: 0,
-      iron: '2%',
-    },
-    {
-      name: 'Honeycomb',
-      calories: 408,
-      fat: 3.2,
-      carbs: 87,
-      protein: 6.5,
-      iron: '45%',
-    },
-    {
-      name: 'Donut',
-      calories: 452,
-      fat: 25.0,
-      carbs: 51,
-      protein: 4.9,
-      iron: '22%',
-    },
-    {
-      name: 'KitKat',
-      calories: 518,
-      fat: 26.0,
-      carbs: 65,
-      protein: 7,
-      iron: '6%',
-    },
+
   ];
 
 
