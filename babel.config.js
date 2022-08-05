4822dcebc803b62b2d988ac8348e24d8892d78cc
Kyle Laker <kyle.laker-ctr@ccpo.mil>
2022-08-05 13:51:30 -0400

    Fix coverage reporting for Vue with Istanbul
    It seems that Istanbul, by default, is not configured to collect
    coverage for `.vue` files. This adds that configuration. It also only
    adds the plugin when `NODE_ENV` is `test` (should happen by default with
    `npm run test`).
    
    A custom name is required because it seems that `@vue/app` already
    includes the istanbul plugin but misconfigured.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 // instrument .js and .vue files
+const plugins = [];
+if (process.env.NODE_ENV === "test") {
+  plugins.push(["babel-plugin-istanbul", { extension: [".js", ".ts", ".vue"]}, "istanbul-coverage"]);
+}
 module.exports = {
   presets: [
     '@vue/app'
   ],
+  plugins,
 }
