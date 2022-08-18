d50a42dfad10900556d45938ba5a6daf0887d34e
Tom Arnold (Hunter Strategy) <77513554+tomarnold-ccpo@users.noreply.github.com>
2022-08-18 12:11:51 -0500

    fixing filepath issue in lcov.info

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ const plugins = [];
 if (process.env.NODE_ENV === "test") {
   plugins.push(["babel-plugin-istanbul", {
     extension: [".vue"],
+    "useInlineSourceMaps": false
   }, "istanbul-coverage"]);
 }
 module.exports = {
