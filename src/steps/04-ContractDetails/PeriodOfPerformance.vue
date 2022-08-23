3de4be7dd17974d3181dc570daffc6627ce250e3
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-08-23 09:24:22 -0700

    make 3 lines into 1

--- src/steps/04-ContractDetails/PeriodOfPerformance.vue
+++ src/steps/04-ContractDetails/PeriodOfPerformance.vue
@@ -57,10 +57,7 @@
                       <ATATTextField
                         :id="getIdText(getOptionPeriodLabel(index)) + 'Duration'"
                         class="mr-4"
-                        :class="[
-                          { 'error--text': index === 0 && basePeriodMissing },
-                          {'error--text': oneYearCheck(optionPeriods[index])}
-                        ]"
+                        :class="[{ 'error--text': index === 0 && basePeriodMissing },{'error--text': oneYearCheck(optionPeriods[index])}]"
                         width="178"
                         :value.sync="optionPeriods[index].duration"
                         type="number"
