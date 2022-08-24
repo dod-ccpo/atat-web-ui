74ad072632db956dc6a2a1d793b833356c7ba2f4
Jason Burkert (Hunter Strategy) <jason.burkert-ctr@ccpo.mil>
2022-08-24 06:41:14 -0400

    Update ServiceNow configuration to accomodate the scoped app name, REST API name and its resources `api/x_g_dis_atat/vue_app_container/[js|img|other_assets]`
    Ticket: AT-7859

--- servicenow.config.js
+++ servicenow.config.js
@@ -1,34 +1,34 @@
-const servicenowConfig = {
-  /**
-   * This is a default prefix for all ServiceNow APIs
-   * should not be changed
-   */
-  REST_API_PATH: '/api',
+/**
+ * This is the prefix for this ServiceNow API.
+ *   api/ - included by default for ServiceNow scripted REST APIs
+ *   x_g_dis_atat/ - the scope of the ATAT application
+ *   vue_app_container - the api_id of the Vue App Container scripted REST API
+ */
+const REST_API_PATH = `api/x_g_dis_atat/vue_app_container`;
 
+const servicenowConfig = {
   /**
    * ServiceNow path to GET resource which serves javascript files
    * Current configuration does not produce CSS files
    * CSS code will be embedded into javascript files
    */
-  JS_API_PATH: 'api/x_744337_vue_app/container/js/',
+  JS_API_PATH: `${REST_API_PATH}/js/`,
   /**
    * ServiceNow path to GET resource which serves
    * Image files (png, jpg, gif)
    * SVG files will be embedded into javascript files
    */
-  IMG_API_PATH: 'api/x_744337_vue_app/container/img/',
+  IMG_API_PATH: `${REST_API_PATH}/img/`,
   /**
    * ServiceNow path to GET resource which serves
    * other files, like fonts etc.
    */
-  ASSETS_API_PATH: 'api/x_744337_vue_app/container/other_assets/',
+  ASSETS_API_PATH: `${REST_API_PATH}/other_assets/`,
   /**
    * fonts and images below this size will be put inside
    * JS chunks, instead of being saved as separate files
    */
   ASSET_SIZE_LIMIT: 10000,
+};
 
-
-}
-
-module.exports = servicenowConfig
+module.exports = servicenowConfig;
