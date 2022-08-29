c1ad077aee7f5ae37b7a2c985ba9bf8b64d23087
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-08-29 18:10:46 -0400

    Began stubbing in member invite modal

--- src/portfolio/components/AddMembersModal.vue
+++ src/portfolio/components/AddMembersModal.vue
@@ -1,16 +1,59 @@
 
 <template>
-  <div>
-    Future Add Members Modal page
-  </div>
+
+  <ATATDialog
+    :showDialog.sync="showModal"
+    :title="'Invite people to “' + projectTitle + '”'"
+    no-click-animation
+    okText="Invite"
+    width="632"
+    :OKDisabled="true"
+  >
+    <template #content>
+      <p class="body">
+        Use “.mil” or “.gov” email addresses to ensure people can authenticate with 
+        a CAC to access your portfolio. 
+        <a
+          id="LearnMoreLink" 
+          role="button"
+        >
+          Learn more about portfolio roles
+        </a>
+      </p>
+      <ATATTextArea
+        id="EmailAddresses"
+        label="Email addresses"
+        rows="7"
+        class="pb-16"
+      />
+    </template>
+  </ATATDialog>
+
 </template>
 <script lang="ts">
 import Vue from "vue";
 
-import { Component } from "vue-property-decorator";
+import { Component, Prop } from "vue-property-decorator";
+import AcquisitionPackage from "@/store/acquisitionPackage";
+
+import ATATDialog from "@/components/ATATDialog.vue";
+import ATATTextArea from "@/components/ATATTextArea.vue";
+
 @Component({
+  components: {
+    ATATDialog,
+    ATATTextArea,
+  }
 })
+
 export default class AddMembersModal extends Vue {
+  @Prop({ default: false }) public showModal?: boolean;
+
+  public get projectTitle(): string {
+    return AcquisitionPackage.projectTitle !== ""
+      ? AcquisitionPackage.projectTitle
+      : "New Acquisition";
+  }
+
 }
 </script>
-
