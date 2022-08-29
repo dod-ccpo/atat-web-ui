649f655c0dcb4ebadfcf28448825c2d25bc935c4
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-08-29 08:56:20 -0700

    folder and file structure for portfolio

---
+++ src/portfolio/Index.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div>
+    Index file for portfolio page
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+import { Component } from "vue-property-decorator";
+@Component({
+})
+export default class Portfolio extends Vue {
+}
+</script>
+
---
+++ src/portfolio/TaskOrder.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div>
+    Future Task Order page
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+import { Component } from "vue-property-decorator";
+@Component({
+})
+export default class TaskOrder extends Vue {
+}
+</script>
+
