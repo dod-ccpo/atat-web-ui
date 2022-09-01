68c18a97e955dc0dd5120203192a8f961b692c79
robertmccardell-CCPO <robert.mccardell-ctr@ccpo.mil>
2022-09-01 15:15:16 -0500

    clean up

--- src/plugins/sanitize.ts
+++ src/plugins/sanitize.ts
@@ -1,13 +1,6 @@
 import Vue from "vue";
 import sanitizeHtml from 'sanitize-html';
 
-// export class SanitizePlugin {
-
-//   public sanitize(content: string ): string{
-//     return sanitizeHtml(content);
-//   }
-// }
-
 export const sanitize = (content: string ): string => {
   return sanitizeHtml(content);
 }
