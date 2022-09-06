ca4cdd878a73ee6cf5916022ae418d1810aa9339
robertmccardell-CCPO <robert.mccardell-ctr@ccpo.mil>
2022-09-06 13:00:43 -0500

    updates to main test

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,21 +1,11 @@
 
 test('Can mount app', async () => {
-  try {
-
-    //mock console log to prevent errors
-    console.log = jest.fn();
-
-    document.body.innerHTML =
+  console.log = jest.fn();
+  document.body.innerHTML =
     '<div id="app">' +
     '</div>';
-
-    // Executes main file
-    require('./main');
-
-    const pElement = document.getElementById('app');
-    expect(pElement).not.toBeUndefined();
-    
-  } catch (error) {
-    // do nothing here
-  }
+  // Executes main file
+  require('./main');
+  const pElement = document.getElementById('app');
+  expect(pElement).not.toBeUndefined();
 });
