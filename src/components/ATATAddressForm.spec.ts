e36c6af887628072486520cec11bb7115f9e9c97
Tony Sumner (Hunter Strategy) <84856468+tonysumner-ccpo@users.noreply.github.com>
2022-09-07 15:17:32 -0400

    WIP

--- src/components/ATATAddressForm.spec.ts
+++ src/components/ATATAddressForm.spec.ts
@@ -10,14 +10,31 @@ describe("Testing ATATStepperNavigation", () => {
   const localVue = createLocalVue();
   let vuetify: Vuetify;
   let wrapper: Wrapper<DefaultProps & Vue, Element>;
+  let addressTypeRadioButtons: Wrapper<DefaultProps & Vue, Element>;
 
   const addressTypeOptions = [
     {
-      id: "1",
-      label: "radio option 1",
-      value: "radioOption1",
+      "id": "USAddress",
+      "label": "U.S. address",
+      "value": "US"
+    },
+    {
+      "id": "MilitaryAddress",
+      "label": "Military/Diplomatic (APO, FPO, or DPO)",
+      "value": "MILITARY"
+    },
+    {
+      "id": "ForeignAddress",
+      "label": "Foreign address",
+      "value": "FOREIGN"
     }
-  ] 
+  ]
+
+  const addressTypes = {
+    "USA": "US",
+    "MIL": "MILITARY",
+    "FOR": "FOREIGN"
+  }
 
   beforeEach(() => {
     vuetify = new Vuetify();
@@ -25,15 +42,37 @@ describe("Testing ATATStepperNavigation", () => {
       vuetify,
       localVue,
       propsData:({
-        addressTypeOptions
+        addressTypeOptions,
+        addressTypes
       })
     });
+    addressTypeRadioButtons = wrapper.find({ref: "addressType"});
   });
 
   describe("INITIALIZATION", () => {
     it("renders successfully", async () => {
       expect(wrapper.exists()).toBe(true);
     });
+
+    it("addressTypeChange() - supplies addressType to set $props.selectedCountry ", 
+      async ()=> {
+        await wrapper.setProps({
+          selectedAddressType: "US",
+        });
+        await wrapper.vm.addressTypeChange("US");
+        
+        // const radioButton = await addressTypeRadioButtons.find("input#Radio_USAddress");
+        // await radioButton.trigger("radioButtonSelected");
+        // await addressTypeRadioButtons.trigger("radioButtonSelected");
+        console.log(await wrapper.vm.$props.selectedCountry.text);
+        Vue.nextTick(()=>{
+          // 
+          // console.log(radioButton.html())
+          // console.log(wrapper.vm.$props.selectedCountry);
+        })
+        
+      })
+
   });
 
 
