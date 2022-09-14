4dcf0e180611d4566358e97f030ec401eae564ab
Tony Sumner (Hunter Strategy) <84856468+tonysumner-ccpo@users.noreply.github.com>
2022-09-14 17:18:03 -0400

    completed styling preview page

--- src/documentReview/Form.vue
+++ src/documentReview/Form.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>
-    <PageHead></PageHead>
-    <div id="FormDiv">
+  <div id="FormDiv">
+    <PageHead :docTitle="docTitle"></PageHead>
+    <div>
       <v-form id="reviewForm">
         <h2>Part I. Requirements Owner Information</h2>
+        <EmergencyDeclarationSupport
+          legend="Is this requirement in support of an emergency declaration?"
+          topMarginClass="mt-2"
+          :emergencyDeclaration.sync="emergencyDeclaration"
+        ></EmergencyDeclarationSupport>
+        <ProjectTitle
+          class="mt-9"
+          label="Project/Requirements Title"
+          :currentTitle.sync = "currentTitle"
+        ></ProjectTitle>
+        <ProjectScope
+          class="mt-10"
+          label="What is the scope of your requirement?"
+          :projectScope.sync="projectScope"
+        ></ProjectScope>
       </v-form>
     </div>
   </div>
@@ -12,12 +27,51 @@
 import Vue from "vue";
 
 import PageHead from "./components/DocReviewHead.vue"
-import { Component } from "vue-property-decorator";
+import { Component, Prop } from "vue-property-decorator";
+import EmergencyDeclarationSupport 
+  from "@/steps/01-AcquisitionPackageDetails/components/EmergencyDeclarationSupport.vue";
+import ProjectTitle
+  from "@/steps/01-AcquisitionPackageDetails/components/ProjectTitle.vue";
+import ProjectScope
+  from "@/steps/01-AcquisitionPackageDetails/components/ProjectScope.vue";
+import AcquisitionPackage, { StoreProperties } from "@/store/acquisitionPackage";
+import { ProjectOverviewDTO } from "@/api/models";
 
 @Component({
   components: {
-    PageHead
+    PageHead,
+    EmergencyDeclarationSupport,
+    ProjectTitle,
+    ProjectScope
   },
 })
-export default class DocumentReviewForm extends Vue {}
+export default class DocumentReviewForm extends Vue {
+  @Prop({ default: "" }) private docTitle!: string;
+
+  private currentTitle = "";
+  private projectScope = "";
+  private emergencyDeclaration = "";
+
+  public async mounted(): Promise<void> {
+    await this.loadOnEnter();
+  }
+
+  public async loadOnEnter(): Promise<void> {
+    const storeData = await AcquisitionPackage.loadData<ProjectOverviewDTO>({
+      storeProperty: StoreProperties.ProjectOverview,
+    });
+
+    if (storeData) {
+      this.currentTitle = storeData.title;
+      this.projectScope = storeData.scope;
+      if (
+        storeData.emergency_declaration &&
+        storeData.emergency_declaration.length > 0
+      ) {
+        this.emergencyDeclaration =
+          storeData.emergency_declaration === "true" ? "yes" : "no";
+      }
+    }
+  }
+}
 </script>
