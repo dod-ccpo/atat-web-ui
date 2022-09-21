77b4e5eb066fa300130c04f51ae5343cd97e14a6
robertmccardell-CCPO <robert.mccardell-ctr@ccpo.mil>
2022-09-21 18:29:30 -0500

    adding base component

---
+++ src/steps/01-AcquisitionPackageDetails/components/RequestingServiceAgency.vue
@@ -0,0 +1,33 @@
+<template>
+  <ATATAutoComplete
+          id="ServiceOrAgency"
+          class="_input-max-width mb-2"
+          label="What service or agency do you work for?"
+          :label-sr-only="false"
+          titleKey="text"
+          :searchFields="['text']"
+          :items="serviceOrAgencyData"
+          :selectedItem.sync="selectedServiceOrAgency"
+          :rules="[$validators.required('Please select your service or agency.')]"
+          placeholder="Find your service/agency"
+          icon="arrow_drop_down"
+        />
+</template>
+
+<script lang="ts">
+/* eslint-disable camelcase */
+import { Component} from "vue-property-decorator";
+import Vue from "vue";
+import ATATAutoComplete from "@/components/ATATAutoComplete.vue";
+
+@Component({
+  components: {
+    ATATAutoComplete
+  },
+})
+
+export default class RequestingServiceAgency extends Vue {
+
+
+}
+</script>
