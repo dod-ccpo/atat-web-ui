6ff0cc80b8e13ff7be4597feb566f395a4840043
robertmccardell-CCPO <robert.mccardell-ctr@ccpo.mil>
2022-09-22 09:10:11 -0500

    component updates

--- src/steps/01-AcquisitionPackageDetails/components/RequestingServiceAgency.vue
+++ src/steps/01-AcquisitionPackageDetails/components/RequestingServiceAgency.vue
@@ -1,33 +1,54 @@
 <template>
   <ATATAutoComplete
-          id="ServiceOrAgency"
-          class="_input-max-width mb-2"
-          label="What service or agency do you work for?"
-          :label-sr-only="false"
-          titleKey="text"
-          :searchFields="['text']"
-          :items="serviceOrAgencyData"
-          :selectedItem.sync="selectedServiceOrAgency"
-          :rules="[$validators.required('Please select your service or agency.')]"
-          placeholder="Find your service/agency"
-          icon="arrow_drop_down"
-        />
+    :id="id"
+    :label="label"
+    :rules="rules"
+    class="_input-max-width mb-2"
+    :label-sr-only="false"
+    titleKey="text"
+    :searchFields="['text']"
+    :items="serviceOrAgencyData"
+    :selectedItem.sync="_selectedServiceOrAgency"
+    placeholder="Find your service/agency"
+    icon="arrow_drop_down"
+  />
 </template>
 
 <script lang="ts">
 /* eslint-disable camelcase */
-import { Component} from "vue-property-decorator";
+import { Component, Prop, PropSync } from "vue-property-decorator";
 import Vue from "vue";
 import ATATAutoComplete from "@/components/ATATAutoComplete.vue";
+import { SelectData } from "types/Global";
+import { convertSystemChoiceToSelect } from "@/helpers";
+import OrganizationData from "@/store/organizationData";
+
+const emptySelectData: SelectData = { text: "", value: "" };
 
 @Component({
   components: {
-    ATATAutoComplete
+    ATATAutoComplete,
   },
 })
-
 export default class RequestingServiceAgency extends Vue {
+  @Prop({ default: "ServiceOrAgency" }) private id!: string;
+  @Prop({ default: "What service or agency do you work for?" })
+  private label!: string;
+  @Prop({ default: "title goes here" }) private helpText!: string;
+  @Prop({ default: true }) private isForm!: boolean;
+  @PropSync("rules") private _rules!: "";
+  @PropSync("selectedServiceOrAgency", {default: emptySelectData}) 
+    private _selectedServiceOrAgency!: SelectData;
+
+  private emptySelectData: SelectData = { text: "", value: "" };
+  private DisaOrgName = "DEFENSE_INFORMATION_SYSTEMS_AGENCY";
+  private serviceOrAgencyData: SelectData[] = [];
 
 
+  public async loadOnEnter(): Promise<void> {
+    this.serviceOrAgencyData = convertSystemChoiceToSelect(
+      OrganizationData.service_agency_data
+    );
+  }
 }
 </script>
