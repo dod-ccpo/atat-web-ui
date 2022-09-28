1d72e91c4d703f8a568c0ff44c7ac5cbee5c1057
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-09-28 10:15:33 -0700

    fixed failed test

--- src/steps/10-FinancialDetails/IGCE/SurgeCapabilities.spec.ts
+++ src/steps/10-FinancialDetails/IGCE/SurgeCapabilities.spec.ts
@@ -3,10 +3,12 @@ import Vuetify from "vuetify";
 import { createLocalVue, mount, Wrapper } from "@vue/test-utils";
 import { DefaultProps } from "vue/types/options";
 import SurgeCapabilities from "@/steps/10-FinancialDetails/IGCE/SurgeCapabilities.vue";
+import validators from "@/plugins/validation"
 Vue.use(Vuetify);
 
 describe("Testing SurgeCapabilities Component", () => {
   const localVue = createLocalVue();
+  localVue.use(validators);
   let vuetify: Vuetify;
   let wrapper: Wrapper<DefaultProps & Vue, Element>;
 
