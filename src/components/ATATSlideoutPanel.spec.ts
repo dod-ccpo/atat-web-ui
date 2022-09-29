f722e7297cf64b6d7994835e7e77ff443d6a3c32
Tony Sumner (Hunter Strategy) <84856468+tonysumner-ccpo@users.noreply.github.com>
2022-09-29 14:30:52 -0400

    completed unit tests.  All 100%

--- src/components/ATATSlideoutPanel.spec.ts
+++ src/components/ATATSlideoutPanel.spec.ts
@@ -1,11 +1,10 @@
 import {createLocalVue, mount, Wrapper} from "@vue/test-utils";
 import Vuetify from "vuetify";
-import {Component, DefaultProps} from "vue/types/options";
+import {DefaultProps} from "vue/types/options";
 import Vue from "vue";
 import validators from "@/plugins/validation";
 import ATATSlideoutPanel from "@/components/ATATSlideoutPanel.vue";
 import SlideoutPanel from "@/store/slideoutPanel/index";
-import {VBtn} from "vuetify/lib/components";
 
 describe("ATATSlideoutPanel Component", () => {
   const localVue = createLocalVue();
@@ -13,60 +12,26 @@ describe("ATATSlideoutPanel Component", () => {
   Vue.use(Vuetify);
   let wrapper: Wrapper<DefaultProps & Vue>;
   localVue.use(validators);
-  jest.useFakeTimers()
+  jest.useFakeTimers();
 
   beforeEach(() => {
     vuetify = new Vuetify();
     wrapper = mount(ATATSlideoutPanel, {
-      /**
-       * mounting 'data()' and 'computed' are a way to mock and alter the computed properties of
-       * a component in the test cases
-       */
-      data() {
-        return {
-          computedTitle: null,
-          computedIsSlideoutPanelOpen: false
-        };
-      },
       localVue,
       vuetify,
-      provide: {
-        SlideoutPanel,
-        document
-      }
     });
   });
   afterEach(() => {
     jest.clearAllMocks();
+    jest.clearAllTimers
   })
 
   it("renders successfully", async () => {
     expect(wrapper.exists()).toBe(true);
   });
 
-  it("should not attach the title tag in the DOM if not set", async () => {
-    expect(wrapper.find('#PanelTitle').element).toBeUndefined();
-  });
-
-  it("should display the title in the DOM when set", async () => {
-    await SlideoutPanel.setSlideoutPanelComponent({
-      title: "TEST TITLE 2",
-      component: {}
-    });
-    const titleTag = await wrapper.find('#PanelTitle');
-    expect(titleTag.text()).toEqual('TEST TITLE 2'); // computed title must be set during the mount
-  });
-
-  it("should return the panel title that is set'", async () => {
-    await SlideoutPanel.setSlideoutPanelComponent({
-      title: "TEST TITLE 3",
-      component: {}
-    });
-    await wrapper.vm.$nextTick(); // without this line the updated panel title will not render
-    expect(wrapper.vm.panelTitle).toEqual('TEST TITLE 3');
-  });
-
-  it("should open slide out panel if class list contains 'v-navigation-drawer--open'", () => {
+  it("transitionEnded() should open slide out panel if class list "  +
+    "contains 'v-navigation-drawer--open'", () => {
     const eventMock = {
       currentTarget: {
         classList: {
@@ -81,23 +46,23 @@ describe("ATATSlideoutPanel Component", () => {
     expect(SlideoutPanel.openSlideoutPanel).toHaveBeenCalled();
   });
 
-  it("should close slide out panel if class list not contains 'v-navigation-drawer--open'",
-    () => {
-      const eventMock = {
-        currentTarget: {
-          classList: {
-            contains: () => {
-              return false;
-            }
+  it("transitionEnded() should close slide out panel if class list not " +
+    "contains 'v-navigation-drawer--open'", () => {
+    const eventMock = {
+      currentTarget: {
+        classList: {
+          contains: () => {
+            return false;
           }
         }
       }
-      jest.spyOn(SlideoutPanel, 'closeSlideoutPanel');
-      wrapper.vm.transitionEnded(eventMock);
-      expect(SlideoutPanel.closeSlideoutPanel).toHaveBeenCalled();
-    });
+    }
+    jest.spyOn(SlideoutPanel, 'closeSlideoutPanel');
+    wrapper.vm.transitionEnded(eventMock);
+    expect(SlideoutPanel.closeSlideoutPanel).toHaveBeenCalled();
+  });
 
-  it("should close slide out panel on close button click'", async () => {
+  it("closeSlideoutPanel() test if clicking close button closes panel", async () => {
     await SlideoutPanel.setSlideoutPanelComponent({
       title: "dummy panel Title",
       component: {}
@@ -109,7 +74,8 @@ describe("ATATSlideoutPanel Component", () => {
     expect(await SlideoutPanel.slideoutPanelIsOpen).toBe(false);
   });
 
-  it("should set the boolean value of 'isSlideoutPanelOpen'", async () => {
+  it("set isSlideoutPanelOpen(true) is to set the boolean value of " +
+  "$data.isOpen to be true", async () => {
     await wrapper.setProps({
       alwaysOpen:  true
     });
@@ -117,14 +83,31 @@ describe("ATATSlideoutPanel Component", () => {
     expect(await wrapper.vm.$data.isOpen).toBe(true);
   })
 
-  it("should set the boolean value of 'isSlideoutPanelOpen'", async () => {
+  it("set isSlideoutPanelOpen(false) is to set the boolean value of " +
+  "$data.isOpen to be false", async () => {
     wrapper.vm.isSlideoutPanelOpen = false;
     expect(wrapper.vm.$data.isOpen).toBe(false);
   })
 
-  it("slideoutPanelToggle(true) - should focus on panel title after toggling " +
+  it("get isSlideoutPanelOpen() " +
+  "$data.isOpen to be false", async () => {
+
+    //mock div
+    const div = document.createElement("input");
+    div.setAttribute("class", "v-overlay--active");
+    document.body.appendChild(div);
+
+    SlideoutPanel.doOpenSlideoutPanel("dummyId");
+    wrapper.vm.$vuetify.breakpoint.sm = true;
+    await wrapper.vm.isSlideoutPanelOpen;
+    jest.advanceTimersByTime(2000);
+    expect(SlideoutPanel.slideoutPanelIsOpen).toBe(true);
+  })
+
+  it("slideoutPanelToggle(true) should focus on panel title after toggling " +
     "the slide out panel", async () => {
-    //create div
+    
+    //mock div
     const div = document.createElement("input");
     div.setAttribute("id", "PanelWrap");
     document.body.appendChild(div);
@@ -132,24 +115,9 @@ describe("ATATSlideoutPanel Component", () => {
     //call slideoutPanelToggle()
     await wrapper.vm.slideoutPanelToggle(true);
     Vue.nextTick();
-    const panelWrap = await wrapper.find("#PanelWrap");
+    const panelWrap = wrapper.find("#PanelWrap");
     expect(panelWrap.element.scrollTop).toBe(0)
   });
-
-
-  // {
-  //   focus() {
-  //     return null;
-  //   },
-  //   scrollTop: -1
-  // })
-  /*it("should focus on slide out panels opener id after panel toggles to closed state", async () => {
-    jest.spyOn(document, 'getElementById');
-    wrapper.vm.slideoutPanelToggle(false);
-    wrapper.vm.isSlideoutPanelOpen = false;
-    await wrapper.vm.$nextTick();
-    expect(document.getElementById).not.toHaveBeenCalledWith('PanelTitle');
-  });*/
 
 });
 
