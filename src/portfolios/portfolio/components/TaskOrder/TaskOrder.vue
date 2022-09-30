5231a692667a090ac22e46e09c50928252ec6b29
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-09-30 09:00:41 -0700

    initial push

--- src/portfolios/portfolio/components/TaskOrder/TaskOrder.vue
+++ src/portfolios/portfolio/components/TaskOrder/TaskOrder.vue
@@ -1,7 +1,21 @@
-
 <template>
   <div>
-    Future Task Order page
+    <div>
+      <h2 class="pb-3">All task orders</h2>
+      <div class="d-flex justify-space-between ">
+        <p class="mr-10">View details about the task orders associated with your portfolio below.
+          You can request to modify an active task order to exercise an option or to change CLIN
+          funding and period of performance details. You can also add a new task order to continue
+          funding your cloud resources and support for this portfolio.
+        </p>
+        <v-btn
+         outlined
+         class="ml-10 secondary"
+        >
+          Add a new task order
+        </v-btn>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
