3f0a62f4cc93200283079e5761e84510105b6873
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-10-10 11:43:12 -0400

    Completed logic for form

--- src/steps/10-FinancialDetails/IGCE/EstimatesDeveloped.vue
+++ src/steps/10-FinancialDetails/IGCE/EstimatesDeveloped.vue
@@ -16,16 +16,66 @@
 
           <ATATCheckboxGroup
             id="UsedForEstimatingCheckboxes"
-            :value.sync="selectedOptions"
+            class="copy-max-width mb-10"
+            :value.sync="selectedTools"
             :hasOtherValue="true"
-            :items="checkboxItems"
+            otherValue="Other"
+            :otherValueEntered.sync="otherValueEntered"
+            otherValueRequiredMessage="Please enter your other type of information or tool"
+            :items="toolsOptions"
             name="checkboxes"
             :card="false"
-            class="copy-max-width"
             :rules="[
               $validators.required(`Please select at least one type of information
                 or tool used to estimate your costs.`)
             ]"
+            otherEntryType="textfield"
+
+          />
+
+          <ATATTextArea
+            id="HowEstimateMade"
+            label="How was your cost estimate made?"
+            class="max-width-740 mb-7"
+            rows="7"
+            :rules="[
+              $validators.required('Please describe how your cost estimate was made'),
+              $validators.maxLength(1000, 
+                'Please limit your description to 1,000 characters or less')
+            ]"
+            helpText="Briefly explain the rationale used to formulate your projected 
+              prices, and identify any and all assumptions used (e.g., schedule or 
+              budget constraints, technology expectations, inflation, travel costs, 
+              etc.)."
+            :value.sync="howEstimateMade"
+            maxChars="1000"
+          />
+
+          <ATATRadioGroup
+            id="PriceComparison"
+            class="mb-10"
+            legend="Thinking of your previous contract for this requirement, how 
+              did the cost estimate compare with the prices that you actually paid 
+              for services and/or support?" 
+            :value.sync="selectedPriceComparison"
+            :items="priceComparisonOptions"
+            name="price-comparison"
+            :rules="[
+              $validators.required(`Please select the option that best describes
+                your previous cost estimates.`)
+            ]"
+          />
+          
+          <ATATTextField
+            v-if="showPercentage"
+            ref="percentOverUnder"
+            :label="percentageLabel"
+            id="PricePercentage"
+            suffix="%"
+            width="150"
+            :value.sync="percentOverUnder"
+            :rules="percentageRules"
+            type="number"
           />
 
         </div>
@@ -36,21 +86,117 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
+import { Component, Watch } from "vue-property-decorator";
 import ATATRadioGroup from "@/components/ATATRadioGroup.vue";
 import ATATCheckboxGroup from "@/components/ATATCheckboxGroup.vue";
+import ATATTextArea from "@/components/ATATTextArea.vue";
+import ATATTextField from "@/components/ATATTextField.vue";
 
-import { RadioButton } from "../../../../types/Global";
+import { RadioButton, Checkbox } from "../../../../types/Global";
 
 @Component({
   components: {
+    ATATCheckboxGroup,
     ATATRadioGroup,
-    ATATCheckboxGroup
+    ATATTextArea,
+    ATATTextField,
   }
 })
 
 export default class EstimatesDeveloped extends Vue {
+  $refs!: {
+    percentOverUnder: Vue & {
+      resetValidation(): void
+    };
+  };
+
+
+  public selectedPriceComparison = "";
+  public howEstimateMade = "";
+  public otherValueEntered = "";
+  public percentOverUnder = "";
+  
+  public selectedTools: string[] = [];
+  private toolsOptions: Checkbox[] = [
+    {
+      id: "AWSCalc",
+      label: "AWS pricing calculator",
+      value: "AWSCalc",
+    },
+    {
+      id: "GoogleCalc",
+      label: "Google Cloud pricing calculator",
+      value: "GoogleCalc",
+    },
+    {
+      id: "AzureCalc",
+      label: "Microsoft Azure pricing calculator",
+      value: "AzureCalc",
+    },
+    {
+      id: "OracleCalc",
+      label: "Oracle Cloud pricing calculator",
+      value: "OracleCalc",
+    },
+    {
+      id: "VendorInvoices",
+      label: "Vendor invoices from a previous requirement",
+      value: "VendorInvoices",
+    },
+    {
+      id: "Other",
+      label: "Other",
+      value: "Other",
+    },
+  ];
+
+  public priceComparisonOptions: RadioButton[] = [
+    {
+      id: "More",
+      label: "The previous cost estimate was <strong>more than</strong> the actual prices paid.",
+      value: "More",
+    },
+    {
+      id: "Less",
+      label: "The previous cost estimate was <strong>less than</strong> the actual prices paid.",
+      value: "Less",
+    },
+    {
+      id: "Same",
+      label: `The previous cost estimate was approximately the <strong>same</strong> 
+        as the actual prices paid.`,
+      value: "Same",
+    },
+  ];
+
+  public get percentageLabel(): string {
+    const label = "By approximately what percentage was your previous IGCE total price <strong>" + 
+      this.overUnderStr + "</strong>?";
+    return label;
+  } 
+
+  public get showPercentage(): boolean {
+    return this.selectedPriceComparison === "More" || this.selectedPriceComparison === "Less";
+  }
+
+  public get percentRequiredMessage(): string {
+    return  "Please enter the approximate percentage that your previous total was " 
+      +  this.overUnderStr + ".";
+  } 
+
+  public get overUnderStr(): string {
+    return this.selectedPriceComparison === "More" ? "overestimated" : "underestimated";
+  }
+
+  public percentageRules = [this.$validators.required(this.percentRequiredMessage)]
+
+  @Watch("selectedPriceComparison")
+  public async selectedPriceComparisonChanged(): Promise<void> {
+    this.$nextTick(()=> {
+      this.$refs.percentOverUnder?.resetValidation();
+      this.percentageRules = [this.$validators.required(this.percentRequiredMessage)]
+    })
+  }
 
 }
 </script>
-
