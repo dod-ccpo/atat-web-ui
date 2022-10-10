cc6199dc7ffd1dbcb175239c18a7bfeb16953733
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-10-10 16:24:47 -0700

    initial push

---
+++ src/packages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Future page of Packages
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+import { Component } from "vue-property-decorator";
+@Component({
+})
+export default class Packages extends Vue {
+}
+</script>
+
