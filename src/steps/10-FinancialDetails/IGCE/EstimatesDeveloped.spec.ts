aa5914db74d504b6d82bb777317f68924993c709
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-10-10 12:09:59 -0400

    Fixed unit test

--- src/steps/10-FinancialDetails/IGCE/EstimatesDeveloped.spec.ts
+++ src/steps/10-FinancialDetails/IGCE/EstimatesDeveloped.spec.ts
@@ -3,10 +3,12 @@ import Vuetify from "vuetify";
 import { createLocalVue, mount, Wrapper } from "@vue/test-utils";
 import { DefaultProps } from "vue/types/options";
 import EstimatesDeveloped from "@/steps/10-FinancialDetails/IGCE/EstimatesDeveloped.vue";
+import validators from "@/plugins/validation";
 Vue.use(Vuetify);
 
 describe("Testing EstimatesDeveloped Component", () => {
   const localVue = createLocalVue();
+  localVue.use(validators);
   let vuetify: Vuetify;
   let wrapper: Wrapper<DefaultProps & Vue, Element>;
 
