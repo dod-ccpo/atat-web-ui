47ba8feadd0d05bba3c1d6843df229779a03aa9f
Ajay Nalamala <ajay.nalamala@hunterstrategy.net>
2022-10-14 14:48:28 -0400

    AT-8071-8098: Remove unused import that is causing build failure

--- src/api/acquisitionPackages/index.ts
+++ src/api/acquisitionPackages/index.ts
@@ -1,4 +1,4 @@
-import {AcquisitionPackageDTO, AcquisitionPackageSummaryDisplay, AcquisitionPackageSummaryDTO} from "../models";
+import {AcquisitionPackageDTO, AcquisitionPackageSummaryDisplay} from "../models";
 import { TableApiBase } from "../tableApiBase";
 const TABLENAME = "x_g_dis_atat_acquisition_package";
 export class AcquisitionPackagesApi extends TableApiBase<AcquisitionPackageDTO> {
