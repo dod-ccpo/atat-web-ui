bdd224e74c38797ace3adc750aa7f0dc94132c51
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-10-21 12:58:24 -0400

    updated unit test

--- src/home/ExistingUser.spec.ts
+++ src/home/ExistingUser.spec.ts
@@ -4,9 +4,11 @@ import {createLocalVue, mount, Wrapper} from "@vue/test-utils";
 import {DefaultProps} from "vue/types/options";
 import ExistingUser from "./ExistingUser.vue";
 import VueRouter from "vue-router";
-import { PackageSummaryDTO } from "@/api/models";
-import PackageSummary from "@/store/packageSummary";
-import { resolve } from "path";
+import { 
+  AcquisitionPackageSummaryDTO,
+  AcquisitionPackageSummarySearchDTO, 
+} from "@/api/models";
+import AcquisitionPackageSummary from "@/store/acquisitionPackageSummary";
 
 Vue.use(Vuetify);
 
@@ -17,13 +19,19 @@ describe("Existing User Component", () => {
   let vuetify: Vuetify;
   let wrapper: Wrapper<DefaultProps & Vue, Element>;
 
-  const packages: PackageSummaryDTO[] = [
+  const packages: AcquisitionPackageSummaryDTO[] = [
     {
       // eslint-disable-next-line camelcase
-      sys_id: "123456"
+      sys_id: "123456",
     }
-  ]
+  ];
 
+  const packageData = {
+    acquisitionPackageSummaryList: packages,
+    // eslint-disable-next-line camelcase
+    total_count: 1
+  }
+    
   const routes = [
     {
       name: "Project_Overview",
@@ -46,9 +54,10 @@ describe("Existing User Component", () => {
 
   describe("testing Existing User", () => {
     it("renders successfully", async () => {
-      jest.spyOn(PackageSummary, "getPackageData").mockImplementation(
-        async(): Promise<PackageSummaryDTO[]> => packages
-      )
+      jest.spyOn(AcquisitionPackageSummary, "searchAcquisitionPackageSummaryList")
+        .mockReturnValue(
+          new Promise((resolve) => resolve(packageData))
+        );
       expect(wrapper.exists()).toBe(true);
     });
 
--- src/home/ExistingUser.vue
+++ src/home/ExistingUser.vue
@@ -209,13 +209,18 @@ export default class ExistingUser extends Vue {
   };
 
   private async loadOnEnter(){
-    const packageData = await AcquisitionPackageSummary
-      .searchAcquisitionPackageSummaryList(this.searchDTO);
-    this.packageData = packageData.acquisitionPackageSummaryList;
+    try {
+      const packageData = await AcquisitionPackageSummary
+        .searchAcquisitionPackageSummaryList(this.searchDTO);
+      this.packageData = packageData.acquisitionPackageSummaryList;
 
-    this.packageCount = this.packageData.length;
-    const draftPackages = this.packageData.filter(obj => obj.package_status?.value === "DRAFT");
-    this.draftPackageCount = draftPackages?.length || 0;
+      this.packageCount = this.packageData.length;
+      const draftPackages = this.packageData.filter(obj => obj.package_status?.value === "DRAFT");
+      this.draftPackageCount = draftPackages?.length || 0;
+    }
+    catch {
+      console.log("Error loading acquisition package data");
+    }
   }
 
   public mounted():void{
