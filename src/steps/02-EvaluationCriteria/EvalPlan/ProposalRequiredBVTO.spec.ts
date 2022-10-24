d051ec24f92ab5b26978df685a6ccb79a549d730
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-10-24 19:03:47 -0400

    updated unit test

--- src/steps/02-EvaluationCriteria/EvalPlan/ProposalRequiredBVTO.spec.ts
+++ src/steps/02-EvaluationCriteria/EvalPlan/ProposalRequiredBVTO.spec.ts
@@ -2,24 +2,24 @@ import Vue from "vue";
 import Vuetify from "vuetify";
 import {createLocalVue, mount, Wrapper} from "@vue/test-utils";
 import {DefaultProps} from "vue/types/options";
-import ProposalRequiredBVTODifferentiators
-  from "@/steps/02-EvaluationCriteria/EvalPlan/ProposalRequiredBVTODifferentiators.vue";
+import ProposalRequiredBVTO
+  from "@/steps/02-EvaluationCriteria/EvalPlan/ProposalRequiredBVTO.vue";
 Vue.use(Vuetify);
 
-describe("Testing ProposalRequiredBVTODifferentiators Component", () => {
+describe("Testing ProposalRequiredBVTO Component", () => {
   const localVue = createLocalVue();
   let vuetify: Vuetify;
   let wrapper: Wrapper<DefaultProps & Vue, Element>;
 
   beforeEach(() => {
     vuetify = new Vuetify();
-    wrapper = mount(ProposalRequiredBVTODifferentiators, {
+    wrapper = mount(ProposalRequiredBVTO, {
       vuetify,
       localVue
     });
   });
 
-  describe("testing ProposalRequiredBVTODifferentiators render", () => {
+  describe("testing ProposalRequiredBVTO render", () => {
     it("renders successfully", async () => {
       expect(wrapper.exists()).toBe(true);
     });
