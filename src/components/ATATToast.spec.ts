09bc0cc90c4d42be847ab0898ddeae06d12daf76
Tony Sumner (Hunter Strategy) <84856468+tonysumner-ccpo@users.noreply.github.com>
2022-10-25 17:17:00 -0400

    completed ATATTooltip unit test AT-7275

--- src/components/ATATToast.spec.ts
+++ src/components/ATATToast.spec.ts
@@ -19,26 +19,26 @@ describe("Testing ATATToast.vue Component", () => {
     });
   });
 
-  describe("ATA", () => { 
+  describe("functions", () => { 
     it("renders successfully", async () => {
       expect(wrapper.exists()).toBe(true);
     });
 
-    it("setToastOpen() ", async()=>{
+    it("isToastOpen(true) to ensure $data.isOpen===true", async()=>{
       await wrapper.setData({
         isToastOpen: true
       });
       expect(wrapper.vm.$data.isOpen).toBe(true);
     })
 
-    it("setToast(true)", async () => {
+    it("setToast(true) to ensure $data.isOpen===true", async () => {
       const isOpen = true;
       wrapper.vm.setToast(isOpen);
       await Vue.nextTick();
       expect(wrapper.vm.$data.isOpen).toBe(isOpen)
     })
 
-    it("setToast(true)", async () => {
+    it("setToast(true) to ensure store property Toast.toast.isOpen===false", async () => {
       jest.useFakeTimers();
       await wrapper.setData({
         toast:{
@@ -54,7 +54,7 @@ describe("Testing ATATToast.vue Component", () => {
       expect(Toast.toast.isOpen).toBe(false);
     })
 
-    it("getTimeout() with $data.hasUndo && reasonable length message returns 3 seconds", 
+    it("getTimeout() with $data.hasUndo && reasonable length message returns default 3 seconds", 
       async () => {
         await wrapper.setData({
           toast:{
@@ -62,21 +62,21 @@ describe("Testing ATATToast.vue Component", () => {
             hasUndo: false,
           }
         })
-        expect(await wrapper.vm.getTimeout).toBe(3000)
+        expect(await wrapper.vm.getTimeout).toBe(3000);
       })
 
-    it("getTimeout() with $data.hasUndo adds 2 seconds to returned value of 3 seconds", 
-      async () => {
-        await wrapper.setData({
-          toast:{
-            message: "this is a toast message",
-            hasUndo: true,
-          }
-        })
-        expect(await wrapper.vm.getTimeout).toBe(5000)
+    it("getTimeout() with $data.hasUndo && reasonable length message adds 2 seconds to " +
+        "returned value of 3 seconds", async () => {
+      await wrapper.setData({
+        toast:{
+          message: "this is a toast message",
+          hasUndo: true,
+        }
       })
+      expect(await wrapper.vm.getTimeout).toBe(5000)
+    })
 
-    it("onUndo() ensure right value is emitted", async () => {
+    it("onUndo() ensure `toast-undo` value is emitted", async () => {
       wrapper.vm.onUndo(); 
       expect(wrapper.emitted('toast-undo')).toBeTruthy();
     })
