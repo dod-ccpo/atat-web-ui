7a82726cc8da5df1b2dcc054e1a5a55a6e19697b
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2022-10-27 11:37:31 -0400

    Added unit tests

--- src/store/acquisitionPackage/index.spec.ts
+++ src/store/acquisitionPackage/index.spec.ts
@@ -16,46 +16,6 @@ localVue.use(Vuex);
 describe("AcquisitionPackage Store", () => {
   let acqPackageStore: AcquisitionPackageStore;
 
-  const mockPackage = {
-    "fair_opportunity": "",
-    "sensitive_information": "",
-    "classification_level": "",
-    "docusign_envelope_id": "",
-    "sys_updated_on": "2022-10-27 14:25:52",
-    "contract_award": "",
-    "number": "ACQ0027452",
-    "period_of_performance": "",
-    "sys_id": "1234567890",
-    "edms_folder_created": "false",
-    "sys_updated_by": "eric.youngquist-ctr@ccpo.mil",
-    "ditco_ko": "",
-    "current_contract_and_recurring_information": "",
-    "sys_created_on": "2022-10-27 14:25:52",
-    "primary_reviewer": "",
-    "pending_operators": "",
-    "selected_service_offerings": "",
-    "sys_created_by": "eric.youngquist-ctr@ccpo.mil",
-    "funding_plans": "",
-    "project_overview": "",
-    "gfe_overview": "",
-    "package_status": "DRAFT",
-    "contract_considerations": "",
-    "sys_mod_count": "0",
-    "css_pre_award_id": "",
-    "contract_modifications": "",
-    "sys_tags": "",
-    "mission_owners": "",
-    "contract_type": "",
-    "css_tracking_number": "",
-    "idiq_clins": "",
-    "requirements_cost_estimate": "",
-    "funding_request": "",
-    "secondary_reviewers": "",
-    "organization": "",
-    "contributors": "",
-    "current_environment": ""
-  }
-
   const initialEvalPlan: EvaluationPlanDTO = {
     source_selection: "",
     method: "",
