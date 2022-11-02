0fa09645b6ddce94b86f78d8d571ffefb3d7f00a
Ajay Nalamala <ajay.nalamala@hunterstrategy.net>
2022-11-02 09:01:16 -0400

    AT-8031: Add UT coverage to financial details store

--- src/store/financialDetails/index.spec.ts
+++ src/store/financialDetails/index.spec.ts
@@ -3,7 +3,11 @@ import {createLocalVue} from "@vue/test-utils";
 import Vuex, {Store} from "vuex";
 import {getModule} from "vuex-module-decorators";
 import {FinancialDetailsStore} from "@/store/financialDetails/index";
-import {RequirementsCostEstimateDTO} from "@/api/models";
+import {
+  FundingRequestFSFormDTO,
+  FundingRequestMIPRFormDTO,
+  RequirementsCostEstimateDTO
+} from "@/api/models";
 import Vue from "vue";
 import {api} from "@/api";
 jest.mock('@/store/helpers')
@@ -15,6 +19,8 @@ describe("FinancialDetails Store",
   () => {
     let financialDetailsStore: FinancialDetailsStore;
     let requirementsCostEstimateDTO: RequirementsCostEstimateDTO;
+    let fundingRequestFSFormDTO: FundingRequestFSFormDTO
+    let fundingRequestMIPRFormDTO: FundingRequestMIPRFormDTO
 
     beforeEach(() => {
       const createStore = (storeOptions: any = {}):
@@ -26,6 +32,20 @@ describe("FinancialDetails Store",
         feePercentage: "",
         feeCharged: "",
         surge_capacity: ""
+      }
+      fundingRequestFSFormDTO = {
+        fs_form_7600a_filename: "Test 7600",
+        fs_form_7600a_attachment: "123",
+        fs_form_7600b_attachment: "",
+        fs_form_7600b_filename: "",
+        use_g_invoicing: "",
+        order_number: "",
+        gt_c_number: ""
+      }
+      fundingRequestMIPRFormDTO = {
+        mipr_number: "A12",
+        mipr_filename: "Test MIPR",
+        mipr_attachment: ""
       }
     })
     afterEach(() => {
@@ -90,6 +110,64 @@ describe("FinancialDetails Store",
         await financialDetailsStore.loadRequirementsCostEstimate();
       } catch {
         await expect(financialDetailsStore.setRequirementsCostEstimate).not.toHaveBeenCalled();
+      }
+    })
+    
+    it('Test loadFundingRequestFSForm()- should not load requirements' +
+      'cost estimate from the api if not already created', async () => {
+      jest.spyOn(api.fundingRequestFSFormTable, "retrieve");
+      await financialDetailsStore.loadFundingRequestFSForm();
+      expect(api.fundingRequestFSFormTable.retrieve).not.toHaveBeenCalled();
+    })
+
+    it('Test loadFundingRequestFSForm()- should load req cost estimate from api', async () => {
+      jest.spyOn(api.fundingRequestFSFormTable, "retrieve").mockReturnValue(
+        Promise.resolve(fundingRequestFSFormDTO)
+      )
+      financialDetailsStore.setFundingRequestFSForm(fundingRequestFSFormDTO);
+      await financialDetailsStore.loadFundingRequestFSForm();
+      expect(api.fundingRequestFSFormTable.retrieve).toHaveBeenCalled();
+    })
+
+    it('Test loadFundingRequestFSForm()- should catch the error', async () => {
+      financialDetailsStore.setFundingRequestFSForm(fundingRequestFSFormDTO);
+      jest.spyOn(api.fundingRequestFSFormTable, "retrieve").mockImplementation(() => {
+        throw Error;
+      })
+      jest.spyOn(financialDetailsStore, "setFundingRequestFSForm");
+      try {
+        await financialDetailsStore.loadFundingRequestFSForm();
+      } catch {
+        await expect(financialDetailsStore.setFundingRequestFSForm).not.toHaveBeenCalled();
+      }
+    })
+
+    it('Test loadFundingRequestMIPRForm()- should not load requirements' +
+      'cost estimate from the api if not already created', async () => {
+      jest.spyOn(api.fundingRequestMIPRFormTable, "retrieve");
+      await financialDetailsStore.loadFundingRequestMIPRForm();
+      expect(api.fundingRequestMIPRFormTable.retrieve).not.toHaveBeenCalled();
+    })
+
+    it('Test loadFundingRequestMIPRForm()- should load req cost estimate from api', async () => {
+      jest.spyOn(api.fundingRequestMIPRFormTable, "retrieve").mockReturnValue(
+        Promise.resolve(fundingRequestMIPRFormDTO)
+      )
+      financialDetailsStore.setFundingRequestMIPRForm(fundingRequestMIPRFormDTO);
+      await financialDetailsStore.loadFundingRequestMIPRForm();
+      expect(api.fundingRequestMIPRFormTable.retrieve).toHaveBeenCalled();
+    })
+
+    it('Test loadFundingRequestMIPRForm()- should catch the error', async () => {
+      financialDetailsStore.setFundingRequestMIPRForm(fundingRequestMIPRFormDTO);
+      jest.spyOn(api.fundingRequestMIPRFormTable, "retrieve").mockImplementation(() => {
+        throw Error;
+      })
+      jest.spyOn(financialDetailsStore, "setFundingRequestMIPRForm");
+      try {
+        await financialDetailsStore.loadFundingRequestMIPRForm();
+      } catch {
+        await expect(financialDetailsStore.setFundingRequestMIPRForm).not.toHaveBeenCalled();
       }
     })
 
