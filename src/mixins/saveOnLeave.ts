5510186f61817a500cb72ab93ef4a5ab0fe7d7c4
Nate Kingsley (Hunter Strategy) <nate.kingsley-ctr@ccpo.mil>
2022-11-15 10:15:38 -0500

    Adding checks in saveOnLeave to validate form and allow movement to next if valid

--- src/mixins/saveOnLeave.ts
+++ src/mixins/saveOnLeave.ts
@@ -1,11 +1,18 @@
 import Vue from "vue";
 import { Route } from "vue-router";
 import { Component } from "vue-property-decorator";
+import AcquisitionPackage from "@/store/acquisitionPackage";
 
 // Register the router hooks with their names
 Component.registerHooks(["beforeRouteLeave"]);
 @Component({})
 export default class SaveOnLeave extends Vue {
+
+  $refs!: {
+    form: Vue & {
+      validate: () => boolean;
+    };
+  };
 
   /**
    * Method that get's called before route leave
@@ -23,9 +30,18 @@ export default class SaveOnLeave extends Vue {
     next: (n: void) => void
   ): Promise<void> {
     const goNext = await this.saveOnLeave();
+
+    const formToValidate = this.$refs.form;
+    let isValid = true;
+
+    if(formToValidate){
+      AcquisitionPackage.setValidateNow(true);
+      isValid = this.$refs.form.validate();
+    }
     
     this.$nextTick(()=> {
-      if (goNext) {
+      AcquisitionPackage.setValidateNow(false);
+      if (goNext && isValid) { 
         next();
       }
     })
