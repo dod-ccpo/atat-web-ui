b85abe842c0ca845466b27e5a0b562e191aabdca
Prasanthi Alaparthi <prasanthi.alaparthi-ctr@ccpo.mil>
2022-11-17 13:26:15 -0500

    Tests for Create Evaluation Plan step [x] Option 1 No Tech Proposal [x] Option 4 Set Equal Lump Sum
    AT-8320
    AT-8327

--- cypress/support/evalCriteria.js
+++ cypress/support/evalCriteria.js
@@ -66,3 +66,100 @@ Cypress.Commands.add("selectEvaluationPlanOption", (radioSelector, value) => {
       }
     })
 })
+
+Cypress.Commands.add("selectMethodSelectionSectionOption", (radioSelector, value) => {
+  cy.radioBtn(radioSelector, value).click({ force: true });
+  cy.findElement(ep.methodSecActiveBtn)
+    .then(($radioBtn) => {
+      const unformattedText = $radioBtn.text()
+      const selectedOption = cleanText(unformattedText);
+      cy.log(selectedOption);
+      cy.textExists(common.continueBtn, "Continue").click();
+      const lptaOption = "radio_button_checkedLowest Price" +
+        " Technically Acceptable" +
+        " (LPTA)Award will be made to the lowest priced offeror meeting the compliance standards.";
+      const bvtoOption = "radio_button_checkedBest Value Trade-Off" +
+        " (BVTO)Award will be made to the CSP providing the best value.";
+      const bestUse = "radio_button_checked“Best use” solutionAward" +
+        " will be made to the CSP offering the “best use.”"
+      const lowestRisk = "radio_button_checked“Lowest risk” solutionAward" +
+        " will be made to the CSP providing the lowest risk."
+      if (selectedOption === lptaOption || selectedOption === bvtoOption) {
+        cy.wait(1000);
+        cy.verifyPageHeader(
+          "Now let’s review compliance standards when technical proposals are required"
+        );
+        cy.textExists(fo.evalPlanAlertHeader, "Compliance Standards");
+        if (selectedOption === lptaOption) {
+          cy.verifyTextMatches(
+            ep.techPropAlertSubHeader,
+            "Award will be made to the lowest priced offeror meeting the following" +
+            " compliance standards:"
+          );
+        } else {
+          cy.verifyTextMatches(
+            ep.techPropAlertSubHeader,
+            "Award will be made to the CSP providing the best value and meets the following" +
+            " compliance standards:"
+          );
+        }
+      } else if (selectedOption === bestUse || selectedOption === lowestRisk) {
+        cy.wait(1000);
+        cy.verifyPageHeader("Now let’s review assessment criteria required for white papers");
+        cy.textExists(fo.evalPlanAlertHeader, "Assessment Areas");
+        if (selectedOption === bestUse) {
+          cy.verifyTextMatches(
+            ep.techPropAlertSubHeader,
+            "Award will be made to the CSP whose white paper offers the “best use” solution" +
+            " and meets the following assessment areas:"
+          );
+        } else {
+          cy.verifyTextMatches(
+            ep.techPropAlertSubHeader,
+            "Award will be made to the CSP whose white paper offers the “lowest risk” solution" +
+            " and meets the following assessment areas:"
+          );
+        }
+      }
+        
+    });
+});
+
+Cypress.Commands.add("selectCustomStandardsRadioOption", (radioSelector, value) => {
+  cy.radioBtn(radioSelector, value).click({ force: true });
+  cy.findElement(ep.customRadioActiveBtn)
+    .then(($radioBtn) => {
+      const unformattedText = $radioBtn.text()
+      const selectedOption = cleanText(unformattedText);
+      cy.log(selectedOption);
+      const yesOption = "radio_button_checkedYes," +
+        " I want to write my own custom compliance standard(s)."
+      if (selectedOption === yesOption) {
+        cy.findElement(ep.customSpecSection).should("exist");
+        cy.findElement(ep.custom0SpecTextbox).should("be.visible").and("be.empty");
+      } else {
+        cy.findElement(ep.customSpecSection).should("not.be.visible");
+      }
+    });
+});
+
+Cypress.Commands.add("customSpecExists", () => {
+  cy.findElement(common. wrap)
+    .then((main) => {
+      if (main.find(ep.customSpec0Control).length > 0) {
+        cy.log("CustomSpec1 FOUND");
+        cy.findElement(ep.custom0DeleteBtn)
+          .should("exist")
+          .and("not.be.disabled");
+        cy.findElement(ep.custom1DeleteBtn)
+          .should("exist")
+          .and("not.be.disabled");                                
+      } else {
+        cy.log("CustomSpec1 NOT FOUND!");
+        cy.findElement(ep.custom0DeleteBtn)
+          .should("exist")
+          .and("be.disabled")
+      }
+    });
+
+});
