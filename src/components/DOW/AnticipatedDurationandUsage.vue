e3738515d32a4ef18321f05924fab78a2da84252
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2022-11-22 09:51:20 -0600

    init

---
+++ src/components/DOW/AnticipatedDurationandUsage.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop, PropSync } from "vue-property-decorator";
+
+import ATATTextArea from "@/components/ATATTextArea.vue";
+
+@Component({
+  components: {
+    ATATTextArea,
+  }
+})
+
+export default class AnticipatedDurationandUsage extends Vue {
+  @Prop("anticipatedNeedUsage") public _anticipatedNeedUsage!: string;
+  @Prop() public index!: number;
+  @Prop({ default: "requirement" }) public requirementOrInstance?: string;
+}
+
+</script>
--- src/steps/04-ContractDetails/CrossDomain.vue
+++ src/steps/04-ContractDetails/CrossDomain.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
-    Future Cross Domain page
+    <v-container fluid class="container-max-width">
+      <v-row>
+        <v-col class="col-12">
+          <h1 class="page-header mb-3">
+            Do you require a cross-domain solution (CDS)?
+          </h1>
+          <div class="copy-max-width">
+            <p class="mb-10" id="IntroP">
+              In the next section, we will dive into the types of resources, tools, and services
+              that you need for this acquisition. The classification level(s) that you select below
+              will be applied to any performance requirements that you specify. If you need more
+              than one level, we will walk you through what is required within each level later.
+            </p>
+            <p id="SelectMessage">
+              Select all that apply to your project.
+            </p>
+          </div>
+          <ATATCheckboxGroup
+            id="ClassificationLevelCheckboxes"
+            :value.sync="selectedOptions"
+            :items="checkboxItems"
+            name="checkboxes"
+            :card="false"
+            class="copy-max-width"
+            :rules="[
+              $validators.required('Please select at least one classification level.')
+            ]"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 <script lang="ts">
