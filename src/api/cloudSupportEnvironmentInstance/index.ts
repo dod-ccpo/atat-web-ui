25e2a07c2d7673381588fca1c00d60ff5518e993
Nate Kingsley (Hunter Strategy) <nate.kingsley-ctr@ccpo.mil>
2022-12-12 16:50:20 -0500

    Fixing lint line max item for api naming being long

--- src/api/cloudSupportEnvironmentInstance/index.ts
+++ src/api/cloudSupportEnvironmentInstance/index.ts
@@ -1,7 +1,8 @@
 import { CloudSupportEnvironmentInstanceDTO } from "../models";
 import { TableApiBase } from "../tableApiBase";
 export const TABLENAME = "x_g_dis_atat_cloud_support_environment_instance";
-export class CloudSupportEnvironmentInstanceAPI extends TableApiBase<CloudSupportEnvironmentInstanceDTO> {
+export class CloudSupportEnvironmentInstanceAPI 
+  extends TableApiBase<CloudSupportEnvironmentInstanceDTO> {
   constructor() {
     super(TABLENAME);
   }
