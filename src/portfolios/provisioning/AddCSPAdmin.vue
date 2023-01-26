10ca4eb273a8467c2ca73b290ad194bbe2f57ff0
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2023-01-26 10:11:10 -0500

    Added basic layout and copy

--- src/portfolios/provisioning/AddCSPAdmin.vue
+++ src/portfolios/provisioning/AddCSPAdmin.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>
-  Add CSP Admin
-  </div>
+  <v-container fluid class="container-max-width">
+    <v-row>
+      <v-col>
+        <h1 class="page-header mb-0">
+          Now, let’s add administrators to manage your CSP portal
+        </h1>
+        <p>
+          Before we can start provisioning, you need to add at least one Cloud 
+          Service Provider (CSP) administrator. These individuals will be granted 
+          full access to your cloud resources within the !!!!![Azure portal]!!!!!!, enabling 
+          them to manage user accounts and configure workspace settings. 
+          <a
+            role="button"
+            id="CSPAdminLearnMore"
+            class="_text-link"
+            tabindex="0"
+            @click="openSlideoutPanel"
+            @keydown.enter="openSlideoutPanel"
+            @keydown.space="openSlideoutPanel">
+            Learn more about administrators
+          </a>
+
+
+          
+        </p>
+
+      
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
