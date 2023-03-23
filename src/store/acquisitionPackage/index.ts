419644426a1d2a7f1bdf24ea7ca88fb65da0eb64
Tony Sumner (Hunter Strategy) <84856468+tonysumner-ccpo@users.noreply.github.com>
2023-03-23 18:50:55 -0400

    [x] resolved sensitive info bug AT-8641

--- src/store/acquisitionPackage/index.ts
+++ src/store/acquisitionPackage/index.ts
@@ -678,7 +678,7 @@ export class AcquisitionPackageStore extends VuexModule {
   public sampleAdditionalButtonActionInStore(actionArgs: string[]): void {
     console.log("in store: actionArgs", actionArgs);
   }
-  @Action
+  @Action ({rawError: true})
   public async getDocGenStatus(packageId: string): Promise<string> {
     if(this.acquisitionPackage){
       this.acquisitionPackage.docgen_job_status = 
@@ -687,12 +687,12 @@ export class AcquisitionPackageStore extends VuexModule {
     return this.acquisitionPackage?.docgen_job_status || "";
   }
 
-  @Action
+  @Action ({rawError: true})
   public async saveDocGenStatus(newDocGenStatus: string): Promise<void> {
-    if(this.acquisitionPackage && this.acquisitionPackage.sys_id){
+    if(this.acquisitionPackage && AcquisitionPackage.packageId){
       this.acquisitionPackage.docgen_job_status = newDocGenStatus;
       await api.acquisitionPackageTable.update(
-        this.acquisitionPackage.sys_id,
+        AcquisitionPackage.packageId,
         this.acquisitionPackage
       );
     }
@@ -1375,7 +1375,7 @@ export class AcquisitionPackageStore extends VuexModule {
   public async saveAcquisitionPackage(): Promise<void>{
     if(this.acquisitionPackage && this.acquisitionPackage.sys_id){
       await api.acquisitionPackageTable.update(
-        this.acquisitionPackage.sys_id,
+        this.packageId,
         this.acquisitionPackage
       );
     }
@@ -1421,10 +1421,12 @@ export class AcquisitionPackageStore extends VuexModule {
           })
           : await api.sensitiveInformationTable.create(data);
       this.setSensitiveInformation(savedSensitiveInformation);
+      debugger;
       this.setAcquisitionPackage({
         ...this.sensitiveInformation,
-        sensitive_information: {value: sys_id}
+        sensitive_information: sys_id
       } as AcquisitionPackageDTO);
+      debugger;
     } catch (error) {
       throw new Error(`error occurred saving sensitive info data ${error}`);
     }
@@ -1517,7 +1519,7 @@ export class AcquisitionPackageStore extends VuexModule {
     };
     const sysID = (isDocumentTypeSigned
       ? await api.packageDocumentsSignedTable.getQuery(getdocumentsSysIDQuery)
-      : await api.packageDocumentsUnsignedTable.getQuery(getdocumentsSysIDQuery))[0].sys_id;
+      : await api.packageDocumentsUnsignedTable.getQuery(getdocumentsSysIDQuery))[0]?.sys_id || "";
     return this.getDomain + '/download_all_attachments.do?sysparm_sys_id=' + sysID;
   }
 
