40a893c1dbc44397b2689c86b1b85400e2ac1b81
Prasanthi Alaparthi <prasanthi.alaparthi-ctr@ccpo.mil>
2023-05-09 09:31:01 -0400

    Test sceanrios for security requirments

--- cypress/integration/ATATDevSnow/ContractDetails/Workflow.spec.js
+++ cypress/integration/ATATDevSnow/ContractDetails/Workflow.spec.js
@@ -1,34 +1,50 @@
-import { bootstrapMockApis,randomAlphaNumeric,randomString}from "../../../helpers";
+/* eslint-disable cypress/no-unnecessary-waiting */
+import {
+  randomAlphaNumeric,
+  randomNumber,
+  randomString,
+} from "../../../helpers";
 import common from "../../../selectors/common.sel";
 import co from "../../../selectors/contractOffice.sel";
 import contractDetails from "../../../selectors/contractDetails.sel";
 
-describe("Test suite: Contract Details: E2E work flow",  () => {
-
+describe("Test suite: Contract Details: E2E work flow", () => {
   let pt = "TC-Step-3-ContractDetails-E2E-" + randomAlphaNumeric(5);
   let scope = "Project Scope-" + randomString(5);
+  let classInput = randomNumber(2);
+  let securityReqDetails;
+  let alertMessage =
+    "You do not need to complete a DD Form 254," +
+    " DoD Contract Security Classification Specification, for this task order." +
+    " JWCC provides a DD Form 254 at the contract level that covers access to all" +
+    " classification levels for the task orders ordered within it.";
+  let cdsLabelTxt = "What type of cross-domain solution do you need?";
 
   beforeEach(() => {
-    bootstrapMockApis();
+    cy.fixture("securityRequirement").then((sr) => {
+      securityReqDetails = sr;
+    });
     cy.launchATAT(true);
     cy.homePageClickAcquisitionPackBtn();
     cy.selectDitcoOption(co.radioDITCO, "DITCO");
     cy.textExists(common.stepAcquisitionText, " Acquisition Package Details ");
     //Verify the Substeps are  visible
-    cy.textExists(common.subStepProjectOverviewTxt, " Project Overview ");    
+    cy.textExists(common.subStepProjectOverviewTxt, " Project Overview ");
     cy.fillNewAcquisition(pt, scope);
     cy.clickDevToggleBtn();
     cy.clickSideStepper(common.stepContractDetailsLink, " Contract Details ");
     cy.activeStep(common.stepContractDetailsText);
-    cy.verifyPageHeader("Let’s gather details about the duration of your task order");
-    cy.findElement(contractDetails.addOptionLink).click();    
+    cy.verifyPageHeader(
+      "Let’s gather details about the duration of your task order"
+    );
+    cy.findElement(contractDetails.addOptionLink).click();
     cy.btnExists(common.continueBtn, " Continue ").not("[disabled]").click();
     cy.waitUntilElementIsGone(contractDetails.baseInputTxtBox);
-    cy.verifyPageHeader(
-      " Do you want to request a PoP start date? "
-    );
-    cy.radioBtn(contractDetails.popStartDateYesRadioOption, "YES").click({ force: true });
-    cy.findElement(contractDetails.requestedStartDate).should("exist"); 
+    cy.verifyPageHeader(" Do you want to request a PoP start date? ");
+    cy.radioBtn(contractDetails.popStartDateYesRadioOption, "YES").click({
+      force: true,
+    });
+    cy.findElement(contractDetails.requestedStartDate).should("exist");
     cy.selectDatefromDatePicker(
       contractDetails.calendarIcon,
       contractDetails.navigateNextMonth,
@@ -38,24 +54,31 @@ describe("Test suite: Contract Details: E2E work flow",  () => {
     );
     cy.btnExists(common.continueBtn, " Continue ").not("[disabled]").click();
     cy.waitUntilElementIsGone(contractDetails.popStartDateYesRadioOption);
-    cy.verifyPageHeader(
-      "Will this be a recurring requirement?"
-    );
-    cy.radioBtn(contractDetails.yesRadioOption, "YES").not("[disabled]").click({ force: true });
+    cy.verifyPageHeader("Will this be a recurring requirement?");
+    cy.radioBtn(contractDetails.yesRadioOption, "YES")
+      .not("[disabled]")
+      .click({ force: true });
     cy.btnExists(common.continueBtn, " Continue ").not("[disabled]").click();
     cy.waitUntilElementIsGone(contractDetails.yesRadioOption);
-    cy.verifyPageHeader("Which contract type(s) apply to this acquisition?"); 
-  });   
-  
-  it("TC1: If unclassified Class Level selected ", () => {  
-    cy.findCheckBox(contractDetails.ffpCheckBox, "FFP").should("not.be.checked")
+    cy.verifyPageHeader("Which contract type(s) apply to this acquisition?");
+  });
+
+  it("TC1: If unclassified Class Level selected ", () => {
+    cy.findCheckBox(contractDetails.ffpCheckBox, "FFP")
+      .should("not.be.checked")
       .check({ force: true });
-    cy.findCheckBox(contractDetails.tmCheckBox, "T&M").should("not.be.checked")
-      .check({ force: true }).then(() => {
+    cy.findCheckBox(contractDetails.tmCheckBox, "T&M")
+      .should("not.be.checked")
+      .check({ force: true })
+      .then(() => {
         cy.findElement(contractDetails.tmTextFieldLabel).should("exist");
-        cy.textExists(contractDetails.tmTextFieldLabel,
-          "Please provide justification for your T&M contract type.");
-        cy.textExists(contractDetails.tmLearnMoreLink, "Learn more").should("exist");
+        cy.textExists(
+          contractDetails.tmTextFieldLabel,
+          "Please provide justification for your T&M contract type."
+        );
+        cy.textExists(contractDetails.tmLearnMoreLink, "Learn more").should(
+          "exist"
+        );
         const inputText = randomAlphaNumeric(8);
         cy.enterTextInTextField(contractDetails.tmTextFieldInputBox, inputText);
       });
@@ -64,18 +87,17 @@ describe("Test suite: Contract Details: E2E work flow",  () => {
     cy.verifyPageHeader(
       " What classification level(s) will be required for your cloud resources and/or services? "
     );
-    cy.selectCheckBoxes([contractDetails.level2])
-    cy.btnClick(common.continueBtn, " Continue ");    
-    // Cross domain page& security req page  is skipped 
+    cy.selectCheckBoxes([contractDetails.level2]);
+    cy.btnClick(common.continueBtn, " Continue ");
+    // Cross domain page& security req page  is skipped
     cy.waitUntilElementIsGone(contractDetails.level2);
-    cy.verifyPageHeader(
-      "Do you have a current contract for this effort?"
-    );
-  }); 
+    cy.verifyPageHeader("Do you have a current contract for this effort?");
+  });
 
-  it("TC2: If both unclassified & Secret Class Level selected ", () => {  
-    cy.findCheckBox(contractDetails.ffpCheckBox, "FFP").should("not.be.checked")
-      .check({ force: true });    
+  it("TC2: If both unclassified & Secret Class Level selected ", () => {
+    cy.findCheckBox(contractDetails.ffpCheckBox, "FFP")
+      .should("not.be.checked")
+      .check({ force: true });
     cy.btnClick(common.continueBtn, " Continue ");
     cy.waitUntilElementIsGone(contractDetails.ffpCheckBox, "FFP");
     cy.verifyPageHeader(
@@ -84,15 +106,229 @@ describe("Test suite: Contract Details: E2E work flow",  () => {
     cy.selectCheckBoxes([contractDetails.level5, contractDetails.level6]);
     const expectedLabels = [
       "Unclassified / Impact Level 5 (IL5)" +
-      " Accommodates DoD CUI and National Security Systems",
-      "Secret / Impact Level 6 (IL6)"
-    ]
-    cy.verifyCheckBoxLabels(contractDetails.checkedClassCheckBoxes,expectedLabels);    
-    cy.btnClick(common.continueBtn, " Continue ");    
-    cy.waitUntilElementIsGone(contractDetails.level4);
-    cy.verifyPageHeader(     
-      "Let’s find out more about your security requirements"
+        " Accommodates DoD CUI and National Security Systems",
+      "Secret / Impact Level 6 (IL6)",
+    ];
+    cy.verifyCheckBoxLabels(
+      contractDetails.checkedClassCheckBoxes,
+      expectedLabels
     );
-  }); 
+    cy.wait(2000);
+    cy.btnClick(common.continueBtn, " Continue ");
+    cy.waitUntilElementIsGone(contractDetails.level4);
+    cy.verifyPageHeader("Let’s find out more about your security requirements");
+    cy.verifyCheckBoxLabels(
+      contractDetails.secretCheckbox,
+      securityReqDetails.secret.checkboxes
+    );
+    cy.selectCheckBoxes(["#Checkbox_4", "#Checkbox_6"]);
+    cy.btnClick(common.continueBtn, " Continue ");
+    cy.waitUntilElementIsGone("#Checkbox_4");
+    cy.verifyPageHeader("Do you require a cross-domain solution (CDS)?");
+    cy.findElement(contractDetails.cdsYesOption)
+      .click({ force: true })
+      .then(() => {
+        cy.findElement(contractDetails.cds).should("be.visible");
+        cy.textExists(contractDetails.cdsLabel, cdsLabelTxt);
+        cy.verifyCheckBoxLabels(
+          contractDetails.cdsCheckbox,
+          securityReqDetails.unClassSecretCDS.checkboxes
+        );
+        cy.selectCheckBoxes([contractDetails.unclastoSecrCB]);
+        cy.enterTextInTextField(contractDetails.cdsUtoSTxtbox, classInput);
+        const checkedbox = ["Unclassified to Secret"];
+        cy.verifyCheckBoxLabels(contractDetails.cdsCheckedbox, checkedbox);
+        const fs = "TestFS - " + randomAlphaNumeric(3);
+        cy.enterTextInTextField(contractDetails.projectedFSField, fs);
+        const anticipatedText = randomAlphaNumeric(17);
+        cy.enterTextInTextField(
+          contractDetails.anticipatedTxtbox,
+          anticipatedText
+        );
+        cy.findElement(contractDetails.entiredDurationNo).click({
+          force: true,
+        });
+        cy.btnClick(common.continueBtn, " Continue ");
+        cy.waitUntilElementIsGone(contractDetails.unclastoSecrCB);
+        cy.verifyPageHeader("Do you have a current contract for this effort?");
+      });
+  });
 
+  it("TC3: If Top Secret Class Level selected", () => {
+    cy.findCheckBox(contractDetails.tmCheckBox, "T&M")
+      .should("not.be.checked")
+      .check({ force: true })
+      .then(() => {
+        cy.findElement(contractDetails.tmTextFieldLabel).should("exist");
+        cy.textExists(
+          contractDetails.tmTextFieldLabel,
+          "Please provide justification for your T&M contract type."
+        );
+        cy.textExists(contractDetails.tmLearnMoreLink, "Learn more").should(
+          "exist"
+        );
+        const inputText = randomAlphaNumeric(8);
+        cy.enterTextInTextField(contractDetails.tmTextFieldInputBox, inputText);
+      });
+    cy.btnClick(common.continueBtn, " Continue ");
+    cy.waitUntilElementIsGone(contractDetails.ffpCheckBox, "FFP");
+    cy.verifyPageHeader(
+      " What classification level(s) will be required for your cloud resources and/or services? "
+    );
+    cy.selectSecretLevel(contractDetails.ts, alertMessage);
+    const expectedLabels = ["Top Secret"];
+    cy.verifyCheckBoxLabels(
+      contractDetails.checkedClassCheckBoxes,
+      expectedLabels
+    );
+    cy.wait(2000);
+    cy.btnClick(common.continueBtn, " Continue ");
+    cy.waitUntilElementIsGone(contractDetails.level4);
+    cy.verifyPageHeader("Let’s find out more about your security requirements");
+    cy.verifyTextMatches(
+      contractDetails.secretLabel,
+      securityReqDetails.topSecret.label
+    );
+    cy.verifyCheckBoxLabels(
+      contractDetails.secretCheckbox,
+      securityReqDetails.topSecret.checkboxes
+    );
+    cy.selectCheckBoxes(["#Checkbox_2", "#Checkbox_8"]);
+    cy.btnClick(common.continueBtn, " Continue ");
+    cy.waitUntilElementIsGone("#Checkbox_4");
+    cy.verifyPageHeader("Do you have a current contract for this effort?");
+  });
+
+  it("TC4: If Secret & Top Secret Class Level selected,no unclassified ", () => {
+    cy.findCheckBox(contractDetails.ffpCheckBox, "FFP")
+      .should("not.be.checked")
+      .check({ force: true });
+    cy.btnClick(common.continueBtn, " Continue ");
+    cy.waitUntilElementIsGone(contractDetails.ffpCheckBox, "FFP");
+    cy.verifyPageHeader(
+      " What classification level(s) will be required for your cloud resources and/or services? "
+    );
+    cy.selectCheckBoxes([contractDetails.level6, contractDetails.ts]);
+    const expectedLabels = ["Secret / Impact Level 6 (IL6)", "Top Secret"];
+    cy.verifyCheckBoxLabels(
+      contractDetails.checkedClassCheckBoxes,
+      expectedLabels
+    );
+    cy.wait(2000);
+    cy.btnClick(common.continueBtn, " Continue ");
+    cy.waitUntilElementIsGone(contractDetails.level4);
+    cy.verifyPageHeader("Let’s find out more about your security requirements");
+    cy.verifyTextMatches(
+      contractDetails.secretLabel,
+      securityReqDetails.secret.label
+    );
+    cy.findElement(contractDetails.tsLabel).scrollIntoView();
+    cy.verifyTextMatches(
+      contractDetails.tsLabel,
+      securityReqDetails.topSecret.label
+    );
+    //Temporarily commenting because need ids
+    // cy.verifyCheckBoxLabels(
+    //   contractDetails.tsCheckbox,
+    //   securityReqDetails.topSecret.checkboxes
+    // );
+    cy.selectCheckBoxes(["#Checkbox_5", "#Checkbox_9"]);
+    cy.btnClick(common.continueBtn, " Continue ");
+    cy.waitUntilElementIsGone("#Checkbox_4");
+    cy.wait(2000);
+    cy.verifyPageHeader("Do you require a cross-domain solution (CDS)?");
+    cy.findElement(contractDetails.cdsYesOption)
+      .click({ force: true })
+      .then(() => {
+        cy.findElement(contractDetails.cds).should("be.visible");
+        cy.textExists(contractDetails.cdsLabel, cdsLabelTxt);
+        cy.verifyCheckBoxLabels(
+          contractDetails.cdsCheckbox,
+          securityReqDetails.secToTS.checkboxes
+        );
+        cy.selectCheckBoxes([contractDetails.tsToS]);
+        cy.enterTextInTextField(contractDetails.cdsTxtbox, classInput);
+        const checkedbox = ["Top Secret to Secret"];
+        cy.verifyCheckBoxLabels(contractDetails.cdsCheckedbox, checkedbox);
+        const fs = "TestFS - " + randomAlphaNumeric(3);
+        cy.enterTextInTextField(contractDetails.projectedFSField, fs);
+        const anticipatedText = randomAlphaNumeric(17);
+        cy.enterTextInTextField(
+          contractDetails.anticipatedTxtbox,
+          anticipatedText
+        );
+        cy.findElement(contractDetails.entiredDurationNo).click({
+          force: true,
+        });
+        cy.btnClick(common.continueBtn, " Continue ");
+        cy.waitUntilElementIsGone(contractDetails.unclastoSecrCB);
+        cy.verifyPageHeader("Do you have a current contract for this effort?");
+      });
+  });
+
+  it("TC5: If unclassified, Secret & Top Secret Class Level selected", () => {
+    cy.findCheckBox(contractDetails.ffpCheckBox, "FFP")
+      .should("not.be.checked")
+      .check({ force: true });
+    cy.btnClick(common.continueBtn, " Continue ");
+    cy.waitUntilElementIsGone(contractDetails.ffpCheckBox, "FFP");
+    cy.verifyPageHeader(
+      " What classification level(s) will be required for your cloud resources and/or services? "
+    );
+    cy.selectCheckBoxes([
+      contractDetails.level2,
+      contractDetails.level6,
+      contractDetails.ts,
+    ]);
+    const expectedLabels = [
+      "Unclassified / Impact Level 2 (IL2)" +
+        " Accommodates DoD information approved for public release" +
+        " (Low Confidentiality and Moderate Integrity)",
+      "Secret / Impact Level 6 (IL6)",
+      "Top Secret",
+    ];
+    cy.verifyCheckBoxLabels(
+      contractDetails.checkedClassCheckBoxes,
+      expectedLabels
+    );
+    cy.wait(2000);
+    cy.btnClick(common.continueBtn, " Continue ");
+    cy.waitUntilElementIsGone(contractDetails.level4);
+    cy.verifyPageHeader("Let’s find out more about your security requirements");
+    cy.selectCheckBoxes(["#Checkbox_5", "#Checkbox_9"]);
+    cy.btnClick(common.continueBtn, " Continue ");
+    cy.waitUntilElementIsGone("#Checkbox_4");
+    cy.verifyPageHeader("Do you require a cross-domain solution (CDS)?");
+    cy.findElement(contractDetails.cdsYesOption)
+      .click({ force: true })
+      .then(() => {
+        cy.findElement(contractDetails.cds).should("be.visible");
+        cy.textExists(contractDetails.cdsLabel, cdsLabelTxt);
+        cy.verifyCheckBoxLabels(
+          contractDetails.cdsCheckbox,
+          securityReqDetails.classCDS.checkboxes
+        );
+        cy.selectCheckBoxes([
+          contractDetails.unclastoSecrCB,
+          contractDetails.tsToS,
+        ]);
+        cy.enterTextInTextField(contractDetails.cdsUtoSTxtbox, classInput);
+        cy.enterTextInTextField(contractDetails.cdsTStoSTxtbox, classInput);
+        const checkedbox = ["Unclassified to Secret", "Top Secret to Secret"];
+        cy.verifyCheckBoxLabels(contractDetails.cdsCheckedbox, checkedbox);
+        const fs = "TestFS - " + randomAlphaNumeric(3);
+        cy.enterTextInTextField(contractDetails.projectedFSField, fs);
+        const anticipatedText = randomAlphaNumeric(17);
+        cy.enterTextInTextField(
+          contractDetails.anticipatedTxtbox,
+          anticipatedText
+        );
+        cy.findElement(contractDetails.entiredDurationNo).click({
+          force: true,
+        });
+        cy.btnClick(common.continueBtn, " Continue ");
+        cy.waitUntilElementIsGone(contractDetails.unclastoSecrCB);
+        cy.verifyPageHeader("Do you have a current contract for this effort?");
+      });
+  });
 });
--- cypress/selectors/contractDetails.sel.js
+++ cypress/selectors/contractDetails.sel.js
@@ -1,95 +1,133 @@
 module.exports = {
-  // Let’s gather some details about the duration of your task order 
+  // Let’s gather some details about the duration of your task order
   popLearnMoreLink: "#PopLearnMore",
   popText: "p.mb-10",
   popLengthLabelText: "div.mb-4._semibold",
-  mainWrap:".v-main__wrap",
-  baseRow:"#BaseRow",
+  mainWrap: ".v-main__wrap",
+  baseRow: "#BaseRow",
   baseLabelText: "#BaseRow .duration",
   baseInputTxtBox: "#BaseDuration_text_field",
-  baseDropdownIcon: "#BaseDropdown_dropdown_field_control .v-input__control .v-input__append-inner",
-  baseDropdownDefault:"#BaseDropdown_dropdown_field_control .v-select__selections",
+  baseDropdownIcon:
+    "#BaseDropdown_dropdown_field_control .v-input__control .v-input__append-inner",
+  baseDropdownDefault:
+    "#BaseDropdown_dropdown_field_control .v-select__selections",
   baseDropdownList: "#BaseDropdown_dropdown_field_control  .v-list",
   baseDropdownYear: "#BaseDropdown_DropdownListItem_Year",
   baseDropdownMonth: "#BaseDropdown_DropdownListItem_Months",
   baseDropdownWeek: "#BaseDropdown_DropdownListItem_Weeks",
-  baseDropdownDays:"#BaseDropdown_DropdownListItem_Days",
+  baseDropdownDays: "#BaseDropdown_DropdownListItem_Days",
   basebutton: "#BaseButtons",
-  baseDuplicateButton:"#BaseCopy",
+  baseDuplicateButton: "#BaseCopy",
   baseDeleteButton: "#BaseDelete",
-  addOptionLink:"#AddOptionPeriodButton",
-  optionRow:"#Option1Row",
+  addOptionLink: "#AddOptionPeriodButton",
+  optionRow: "#Option1Row",
   optionalTextBox: "#Option1Duration_text_field",
-  optionDropdownIcon: "#Option1Dropdown_dropdown_field_control .v-input__control" +
+  optionDropdownIcon:
+    "#Option1Dropdown_dropdown_field_control .v-input__control" +
     " .v-input__append-inner",
-  optionOneDropdownDefault:"#Option1Dropdown_dropdown_field_control .v-select__selections",
-  optionDropdownList: "#Option1Dropdown_dropdown_dropdown_field_control .v-list",
+  optionOneDropdownDefault:
+    "#Option1Dropdown_dropdown_field_control .v-select__selections",
+  optionDropdownList:
+    "#Option1Dropdown_dropdown_dropdown_field_control .v-list",
   optionDropdownYear: "#Option1Dropdown_DropdownListItem_Year",
   optionDropdownMonth: "#Option1Dropdown_DropdownListItem_Months",
   optionDropdownWeek: "#Option1Dropdown_DropdownListItem_Weeks",
   optioneDropdownDays: "#Option1Dropdown_DropdownListItem_Days",
-  optionOneDropdownSelected:"#Option1Dropdown_dropdown_field_control .v-select__selection--comma",
+  optionOneDropdownSelected:
+    "#Option1Dropdown_dropdown_field_control .v-select__selection--comma",
   errorMessageText: ".field-error.ml-2",
   optionDeleteButton: "#Option1Delete",
   sourceItem: "#Option2Row",
   targetItem: "#BaseRow",
   optionalTwoTextBox: "#Option2Duration_text_field",
-  
+
   //Do you want to request a PoP start date?
   popStartDateYesRadioOption: "#Radio_YesStartDate",
   popStartDateNoRadioOption: "#Radio_NoStartDate",
   requestedStartDate: "p.mb-2",
-  requestedStartDropdownIcon: "#RequestDateOption_dropdown_field_control" +
-      " .v-input__control .v-input__append-inner",
-  requestedStartDropdownList:"#RequestDateOption_dropdown_field_control .v-list",
-  requestedStartDateNosoonerthan: "#RequestDateOption_DropdownListItem_Nosoonerthan",
-  requestedStartDateNotlaterthan: "#RequestDateOption_DropdownListItem_Notlaterthan",
+  requestedStartDropdownIcon:
+    "#RequestDateOption_dropdown_field_control" +
+    " .v-input__control .v-input__append-inner",
+  requestedStartDropdownList:
+    "#RequestDateOption_dropdown_field_control .v-list",
+  requestedStartDateNosoonerthan:
+    "#RequestDateOption_DropdownListItem_Nosoonerthan",
+  requestedStartDateNotlaterthan:
+    "#RequestDateOption_DropdownListItem_Notlaterthan",
   warningTextMessage: "#RequestDateAlert .mb-0",
   requestDatePicker: "#RequestDatePickerDatePickerTextField",
-  requestDatePickerError:"#RequestDatePickerDatePickerContainer .field-error",
+  requestDatePickerError: "#RequestDatePickerDatePickerContainer .field-error",
   calendarIcon: "#RequestDatePickerDatePickerButtonIcon",
-  datePicker:"#RequestDatePickerDatePicker",
+  datePicker: "#RequestDatePickerDatePicker",
   navigateCalendar: ".v-date-picker-header i",
   navigateNextMonth: "button[aria-label='Next month']",
   selectDate: ".v-date-picker-table button:not(.v-btn--disabled)",
-  popStartRadioError:"#PoPStartDate_radio_group_control .field-error",
+  popStartRadioError: "#PoPStartDate_radio_group_control .field-error",
 
   //will this be a future recurring requirement?
   recurringReqText: "p.mb-10",
   yesRadioOption: "#Radio_YesRecurring",
   noRadioOption: "#Radio_NoRecurring",
-  activeRadioOption:"#RecurringOptions_radio_group_control .v-item--active",
+  activeRadioOption: "#RecurringOptions_radio_group_control .v-item--active",
   popRadioGroup: "#PoPStartDate_radio_group_control",
-  futureRecurringRadioError: "#RecurringOptions_radio_group_control .field-error",
-  
+  futureRecurringRadioError:
+    "#RecurringOptions_radio_group_control .field-error",
+
   //which Contract type applies to this acquistion?
   introPText: "#IntroP",
   farLink: "._text-link",
   selectMessageText: "#SelectMessage",
-  selectedContractTypeOption:"#ContractTypesCheckboxes input[type=checkbox]:checked",
+  selectedContractTypeOption:
+    "#ContractTypesCheckboxes input[type=checkbox]:checked",
   ffpCheckBox: "#Checkbox_FFPCheckbox",
   tmCheckBox: "#Checkbox_TMCheckbox",
-  tmTextFieldLabel:"#JustificationForTM_text_field_label",
+  tmTextFieldLabel: "#JustificationForTM_text_field_label",
   tmTextFieldInputBox: "#JustificationForTM_text_area",
   tmLearnMoreLink: "#JustificationLearnMore",
-  
+
   //What classification level(s) will be required for your cloud resources and /or services?
   selectMess: "#SelectMessage",
   classCheckBoxes: "#ClassificationLevelCheckboxes input[type=checkbox]",
-  checkedClassCheckBoxes:"#ClassificationLevelCheckboxes input[type=checkbox]:checked",
+  checkedClassCheckBoxes:
+    "#ClassificationLevelCheckboxes input[type=checkbox]:checked",
   level2: "#Checkbox_IL2",
   level4: "#Checkbox_IL4",
   level5: "#Checkbox_IL5",
   level6: "#Checkbox_IL6",
-  ts:"#Checkbox_TS",
+  ts: "#Checkbox_TS",
   modalLevel2: "#Checkbox_IL2Modal",
   modalLevel4: "#Checkbox_IL4Modal",
   modalLevel5: "#Checkbox_IL5Modal",
   modalLevel6: "#Checkbox_IL6Modal",
   errorClassCheckBox: "#ClassificationLevelCheckboxes .field-error",
   alertMessage: "#ClassificationRequirementsAlert p",
-  activeCheckBox:"#ClassificationLevelCheckboxes .v-input--is-label-active",
+  activeCheckBox: "#ClassificationLevelCheckboxes .v-input--is-label-active",
+
+  //Security Requirements page
+  secretCheckbox: "#SecurityRequirements input[type=checkbox]",
+  secretCheckedbox: "#SecurityRequirements input[type=checkbox]:checked",
+  secretLabel: ":nth-child(1) > #MessageNote span",
+  tsLabel: ":nth-child(3) > #MessageNote span",
+  tsCheckbox: ":nth-child(2) #SecurityRequirements input[type=checkbox]",
+  secretErrorCheckbox:
+    "#SecurityRequirements input[type=checkbox] .field-error",
 
   //Cross Domain
-  cdsRadiogroup:"#needsCDSGroup_radio_group_control"
-}
+  cdsRadiogroup: "#needsCDSGroup_radio_group_control",
+  cdsYesOption: "#Radio_YesCdsSolution",
+  cdsNoOption: "#Radio_NoCdsSolution",
+  cdsLabel: "#cdsSolutionLabel",
+  cds: "#cdsSolutions",
+  cdsCheckbox: "#cdsSolutions input[type=checkbox]",
+  unclastoSecrCB: "#Checkbox_UtoS",
+  tsToUnclass: "#Checkbox_TStoUS",
+  tsToS: "#Checkbox_TStoS",
+  cdsUtoSTxtbox: "#cdsSolutions #cdsSolutions_TextField0_text_field",
+  cdsTxtbox: "#cdsSolutions #cdsSolutions_TextField1_text_field",
+  cdsTStoSTxtbox: "#cdsSolutions #cdsSolutions_TextField5_text_field",
+  cdsCheckedbox: "#cdsSolutions input[type=checkbox]:checked",
+  projectedFSField: "#projectedFileStreamType_text_field",
+  anticipatedTxtbox: "#AnticipatedNeedUsage_01_text_area",
+  entiredDurationNo: "#EntireDuration_01_radio_group_control #Radio_No",
+  entiredDurationYes: "#EntireDuration_01_radio_group_control #Radio_Yes",
+};
