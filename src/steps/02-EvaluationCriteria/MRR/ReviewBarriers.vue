6bd5bbc32f592df7e23d655be28882972d171990
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2023-05-10 08:11:12 -0500

    init

--- src/steps/02-EvaluationCriteria/MRR/ReviewBarriers.vue
+++ src/steps/02-EvaluationCriteria/MRR/ReviewBarriers.vue
@@ -7,8 +7,24 @@
             Let’s review your agency’s plans to remove barriers to fair opportunity
           </h1>
           <p>
-            Intro paragraph
+            Based on what you’ve told us, we’ve suggested language to describe the actions proposed
+            to remove barriers to fair opportunity. You can edit any details to meet your
+            requirements.
           </p>
+          <ATATTextArea
+            class="textarea-max-width"
+            id="ExceptionTextArea"
+            label="Briefly discuss any other
+              facts or details supporting the use of this exception"
+            :value.sync="exceptionDiscussion"
+            :maxChars="1000"
+            :rows="6"
+            :validateItOnBlur="true"
+            :rules="[
+                $validators.required('Enter a description for your other supporting factors.'),
+                $validators.maxLength(1000)
+            ]"
+          />
         </v-col>
       </v-row>
     </v-container>    
