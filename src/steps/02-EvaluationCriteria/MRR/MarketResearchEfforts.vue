e56440cb8b09e8d277bc4aee96ab7695cd719026
Ajay Nalamala <ajay.nalamala@hunterstrategy.net>
2023-05-11 09:51:31 -0400

    AT-9006: Complete all functionals requirements including UTs for the story

--- src/steps/02-EvaluationCriteria/MRR/MarketResearchEfforts.vue
+++ src/steps/02-EvaluationCriteria/MRR/MarketResearchEfforts.vue
@@ -7,7 +7,17 @@
             Let’s find out more about your market research efforts
           </h1>
           <p>
-            Intro paragraph
+            Content from AT-8838
+          </p>
+          <p>
+            <a id="LearnMoreIGCE"
+               role="button"
+               tabindex="0"
+               @click="openSlideoutPanel"
+               @keydown.enter="openSlideoutPanel"
+               @keydown.space="openSlideoutPanel">
+              <span class="">Learn more about market research for the JWCC Contract</span>
+            </a>
           </p>
         </v-col>
       </v-row>
@@ -18,8 +28,40 @@
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+import SlideoutPanel from "@/store/slideoutPanel";
+import {SlideoutPanelContent} from "../../../../types/Global";
+import MarketResearchEffortsLearnMore
+  from "@/steps/02-EvaluationCriteria/MRR/MarketResearchEffortsLearnMore.vue";
 
-@Component({})
+@Component({
+  components: {
+    MarketResearchEffortsLearnMore
+  },
+})
 
-export default class MarketResearchEfforts extends Vue {}
+export default class MarketResearchEfforts extends Vue {
+
+  /**
+   * Opens the slideout panel assuming that the panel is already initialized.
+   */
+  public async openSlideoutPanel(e: Event): Promise<void> {
+    const opener = e.currentTarget as HTMLElement;
+    SlideoutPanel.openSlideoutPanel(opener.id);
+  };
+
+  /**
+   * Initializes the slideout panel by setting the slideout panel component.
+   */
+  public async initializeSlideoutPanel(): Promise<void> {
+    const slideoutPanelContent: SlideoutPanelContent = {
+      component: MarketResearchEffortsLearnMore,
+      title: "Learn More",
+    };
+    await SlideoutPanel.setSlideoutPanelComponent(slideoutPanelContent);
+  }
+
+  public async mounted(): Promise<void> {
+    await this.initializeSlideoutPanel();
+  };
+}
 </script>
