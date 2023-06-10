32fda9ab7e8cb00b4d1f8246cae47af27641143f
Jeff Segal <jeffrey.t.segal.ctr@mail.mil>
2023-06-10 10:49:12 -0400

    AT-9200: Added a comment explaining our usage of the /upload API

--- src/api/attachments/index.ts
+++ src/api/attachments/index.ts
@@ -23,6 +23,12 @@ export class AttachmentApi extends TableApiBase<AttachmentDTO> {
     const formData = new FormData();
     
     const {table_name, table_sys_id, file_name} = data;
+    // SNOW offers two file upload APIs:
+    //    /file, which uses query parameters and whose POST body is the raw bytes of the file and
+    //    /upload, which consumes a multipart/form-data POST body
+    // We are using the latter here. See the following link for API documentation:
+    // eslint-disable-next-line max-len
+    // https://docs.servicenow.com/en-US/bundle/rome-application-development/page/integrate/inbound-rest/concept/c_AttachmentAPI.html#title_attachment-POST-upload
 
     formData.append('table_name',table_name)
     formData.append('table_sys_id',table_sys_id)
