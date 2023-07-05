34ff344d578f4e598f2cc7286d34f183d17ab2c1
ANTHONY SUMNER <anthony.l.sumner4.ctr@mail.mil>
2023-07-05 16:54:57 -0400

    testing getters() AT-9350

--- src/steps/03-Background/CurrentContract/CurrentContractDetails.spec.ts
+++ src/steps/03-Background/CurrentContract/CurrentContractDetails.spec.ts
@@ -3,9 +3,11 @@ import Vuetify from "vuetify";
 import {DefaultProps} from "vue/types/options";
 import Vue from "vue";
 import CurrentContractDetails from "@/steps/03-Background/CurrentContract/CurrentContractDetails.vue";
+import validators from "../../../plugins/validation";
 
 describe("Testing CurrentContractDetails Component", () => {
   const localVue = createLocalVue();
+  localVue.use(validators);
   let vuetify: Vuetify;
   let wrapper: Wrapper<DefaultProps & Vue>;
 
@@ -20,6 +22,61 @@ describe("Testing CurrentContractDetails Component", () => {
   it("renders successfully", async () => {
     expect(wrapper.exists()).toBe(true);
   });
+
+  describe("Testing Getters...", () => {
+    const isCurrent = {
+      currentContract:{
+        contract_order_start_date: "12/31/2022",
+        contract_order_expiration_date: "12/31/2027"
+      },
+      isCurrent: true
+    }
+
+    const isPrevious = {
+      currentContract:{
+        contract_order_expiration_date: "12/31/2022"
+      },
+      isCurrent: false
+    }
+
+    it("setHeadline()=> returns `current` headline ", async () => {
+      wrapper.setData(isCurrent);
+      wrapper.vm.setHeadline();
+      expect(wrapper.vm.$data.headline).toContain('current');
+    });
+
+    it("setHeadline()=> returns `previous` headline ", async () => {
+      wrapper.setData(isPrevious);
+      wrapper.vm.setHeadline();
+      expect(wrapper.vm.$data.headline).toContain('previous');
+    });
+
+    it("startDate()=> returns valid date ", async () => {
+      wrapper.setData({
+        currentContract:{
+          contract_order_start_date: "12/31/2022"
+        },
+      });
+      expect(wrapper.vm.startDate).toEqual("2022-12-31")
+    });
+
+    it("startDate()=> returns empty string ", async () => {
+      expect(wrapper.vm.startDate).toEqual("")
+    });
+
+    it("expirationDate()=> returns valid date ", async () => {
+      wrapper.setData({
+        currentContract:{
+          contract_order_expiration_date: "12/31/2022"
+        },
+      });
+      expect(wrapper.vm.expirationDate).toEqual("2022-12-31")
+    });
+
+    it("expirationDate()=> returns empty string ", async () => {
+      expect(wrapper.vm.expirationDate).toEqual("")
+    });
+  })
 
 
 });
