25309a631043cde6eda2211593e5951288096cd9
ANTHONY SUMNER <anthony.l.sumner4.ctr@mail.mil>
2023-07-10 14:35:53 -0400

    WIP

--- src/steps/03-Background/CurrentContract/ProcurementHistorySummary.spec.ts
+++ src/steps/03-Background/CurrentContract/ProcurementHistorySummary.spec.ts
@@ -2,9 +2,25 @@ import {createLocalVue, mount, Wrapper} from "@vue/test-utils";
 import Vuetify from "vuetify";
 import {DefaultProps} from "vue/types/options";
 import Vue from "vue";
-import ProcurementHistorySummary from "@/steps/03-Background/components/ProcurementHistorySummary.vue";
+import ProcurementHistorySummary from "@/steps/03-Background/CurrentContract/ProcurementHistorySummary.vue";
 
-describe("Testing TaskOrderNumber Component", () => {
+const currentContractDTO = {
+  instance_number: 1,
+  current_contract_exists: "YES",
+  incumbent_contractor_name: "incumbent_contractor_name",
+  contract_number: "123",
+  task_delivery_order_number: "123",
+  contract_order_expiration_date: "10/12/2039",
+  contract_order_start_date: "10/12/2023",
+  competitive_status: "cs",
+  business_size: "bs",
+  acquisition_package: "123",
+  is_valid: true,
+  sys_created_by: "me",
+  is_current: true,
+}
+
+describe("Testing ProcurementHistorySummary Component", () => {
   const localVue = createLocalVue();
   let vuetify: Vuetify;
   let wrapper: Wrapper<DefaultProps & Vue>;
@@ -20,5 +36,24 @@ describe("Testing TaskOrderNumber Component", () => {
   it("tests that component renders successfully", async () => {
     expect(wrapper.exists()).toBe(true);
   });
+
+  describe("Testing GETTERS", () => {
+    const localVue = createLocalVue();
+    let vuetify: Vuetify;
+    let wrapper: Wrapper<DefaultProps & Vue>;
+  
+    beforeEach(() => {
+      vuetify = new Vuetify();
+      wrapper = mount(ProcurementHistorySummary, {
+        localVue,
+        vuetify,
+      });
+    });
+  
+    it("hasContractData()=> returns true", async () => {
+      expect(wrapper.exists()).toBe(true);
+    });
+  
+   });
 
  });
