2a1805e054baa11ec3917d5e60d82ac53fec8c98
ANTHONY SUMNER <anthony.l.sumner4.ctr@mail.mil>
2023-07-10 14:13:28 -0400

    fixed linter issues

--- src/steps/03-Background/CurrentContract/CurrentContractDetails.spec.ts
+++ src/steps/03-Background/CurrentContract/CurrentContractDetails.spec.ts
@@ -1,14 +1,15 @@
-import {createLocalVue, mount, Wrapper} from "@vue/test-utils";
+/* eslint-disable camelcase */
+import { createLocalVue, mount, Wrapper } from "@vue/test-utils";
 import Vuetify from "vuetify";
-import {DefaultProps} from "vue/types/options";
+import { DefaultProps } from "vue/types/options";
 import Vue from "vue";
 import Vuex from "vuex";
-import CurrentContractDetails from "@/steps/03-Background/CurrentContract/CurrentContractDetails.vue";
+import CurrentContractDetails 
+  from "@/steps/03-Background/CurrentContract/CurrentContractDetails.vue";
 import validators from "../../../plugins/validation";
-import AcquisitionPackage,{ StoreProperties}
+import AcquisitionPackage, { StoreProperties }
   from "@/store/acquisitionPackage";
 import { CurrentContractDTO } from "@/api/models";
-import { isDate } from "lodash";
 
 describe("Testing CurrentContractDetails Component", () => {
   const localVue = createLocalVue();
@@ -18,14 +19,14 @@ describe("Testing CurrentContractDetails Component", () => {
   const store = new Vuex.Store(AcquisitionPackage)
 
   const populatedCurrentContract = {
-    currentContract:{
+    currentContract: {
       contract_order_start_date: "12/31/2022",
       contract_order_expiration_date: "12/31/2027"
     }
   }
 
   const emptyCurrentContract = {
-    currentContract:{
+    currentContract: {
       contract_order_start_date: "",
       contract_order_expiration_date: ""
     }
@@ -46,33 +47,33 @@ describe("Testing CurrentContractDetails Component", () => {
     sys_created_by: "me",
     is_current: true,
   }
-  
+
 
   beforeEach(() => {
     const formStub = {
-      render: () => {},
-      methods:{
+      render: () => { /** do nothing */},
+      methods: {
         validate: () => true,
       }
     }
 
     const startDatePickerStub = {
-      render: () => {},
-      methods:{
+      render: () => { /** do nothing */},
+      methods: {
         validate: () => false,
       },
-      ref:{
+      ref: {
         "atatDatePicker": Vue
       }
-      
+
     }
 
     const expirationDatePickerStub = {
-      render: () => {},
-      methods:{
+      render: () => { /** do nothing */},
+      methods: {
         validate: () => false,
       },
-      ref:{
+      ref: {
         "atatDatePicker": Vue
       }
     }
@@ -89,7 +90,7 @@ describe("Testing CurrentContractDetails Component", () => {
         'expirationDatePicker': expirationDatePickerStub,
       }
     });
-    
+
   });
 
   afterEach(() => {
@@ -104,20 +105,20 @@ describe("Testing CurrentContractDetails Component", () => {
   });
 
   describe("GETTERS", () => {
-  
+
     const isCurrent = {
-      currentContract:{
+      currentContract: {
         contract_order_start_date: "12/31/2022",
         contract_order_expiration_date: "12/31/2027"
       },
       isCurrent: true
     }
     const isPrevious = {
-      currentContract:{
+      currentContract: {
         contract_order_expiration_date: "12/31/2022"
       },
       isCurrent: false
-    }    
+    }
 
     it("todaysDateISO()=> is an ISO date ", async () => {
       const getterISO = wrapper.vm.todaysDateISO.split("-");
@@ -125,14 +126,14 @@ describe("Testing CurrentContractDetails Component", () => {
       expect(getterISO[1]).toHaveLength(2)
       expect(getterISO[2]).toHaveLength(2)
     });
-  
+
     it("todaysDateMMDDYYYY()=> returns 10 characters - MM/DD/YYYY ", async () => {
       expect(wrapper.vm.todaysDateMMDDYYYY).toHaveLength(10);
     });
 
     it("startDate()=> returns valid date ", async () => {
       wrapper.setData({
-        currentContract:{
+        currentContract: {
           contract_order_start_date: "12/31/2022"
         },
       });
@@ -145,7 +146,7 @@ describe("Testing CurrentContractDetails Component", () => {
 
     it("expirationDate()=> returns valid date ", async () => {
       wrapper.setData({
-        currentContract:{
+        currentContract: {
           contract_order_expiration_date: "12/31/2022"
         },
       });
@@ -166,73 +167,73 @@ describe("Testing CurrentContractDetails Component", () => {
       expect(wrapper.vm.isDatePickersEmpty).toBe(false);
     });
 
-    it("isExceptiontoFairOpp() => returns true", async () =>{
+    it("isExceptiontoFairOpp() => returns true", async () => {
       expect(wrapper.vm.isExceptiontoFairOpp).toBe(true);
     })
 
-    it("currentData() => returns currentData obj", async () =>{
+    it("currentData() => returns currentData obj", async () => {
       wrapper.setData(isCurrent);
       expect(wrapper.vm.currentData).toEqual(isCurrent.currentContract);
     })
 
-    it("currentData() => returns initialCurrentContract()", async () =>{
-      wrapper.setData({currentContract: undefined});
+    it("currentData() => returns initialCurrentContract()", async () => {
+      wrapper.setData({ currentContract: undefined });
       //initialContract will have all empty values;
-      expect(Object.values(wrapper.vm.currentData).every(v=>v===""))
+      expect(Object.values(wrapper.vm.currentData).every(v => v === ""))
     })
   })
-  
+
   describe("FUNCTIONS", () => {
     describe("Testing Error Messages functions()", () => {
       const value = ["Error Message 001"];
-   
+
       beforeEach(() => {
         jest.useFakeTimers();
       });
-  
-      afterEach(()=>{
+
+      afterEach(() => {
         jest.useRealTimers();
         jest.clearAllTimers();
       })
-  
+
       describe("returns custom error message ...", () => {
         beforeEach(() => {
           jest.spyOn(wrapper.vm, "removeSharedErrorMessages").mockImplementation()
           wrapper.setData(emptyCurrentContract);
         });
-        afterEach(()=>{
+        afterEach(() => {
           jest.advanceTimersByTime(3000);
           expect(wrapper.vm.$data.expirationDPSharedErrorMessages[0])
-             .toContain('PoP start and expiration dates');
+            .toContain('PoP start and expiration dates');
         })
-  
-        it("setStartDateErrorMessages(value)", async () =>{
+
+        it("setStartDateErrorMessages(value)", async () => {
           wrapper.vm.setStartDateErrorMessages(value)
         })
-  
-        it("setExpirationDateErrorMessages(value)", async () =>{
+
+        it("setExpirationDateErrorMessages(value)", async () => {
           wrapper.vm.setExpirationDateErrorMessages(value)
         })
       });
-  
+
       describe("returns `value` param as error message ...", () => {
         beforeEach(() => {
           jest.spyOn(wrapper.vm, "removeSharedErrorMessages").mockImplementation();
           wrapper.setData(populatedCurrentContract);
         });
-        afterEach(()=>{
+        afterEach(() => {
           jest.advanceTimersByTime(3000);
           expect(wrapper.vm.$data.expirationDPSharedErrorMessages[0]).toEqual(value[0]);
         })
-        it("setStartDateErrorMessages(value)", async () =>{
+        it("setStartDateErrorMessages(value)", async () => {
           wrapper.vm.setStartDateErrorMessages(value)
         })
-        it("setExpirationDateErrorMessages(value)", async () =>{
+        it("setExpirationDateErrorMessages(value)", async () => {
           wrapper.vm.setExpirationDateErrorMessages(value)
         })
       });
     })
-    describe("removeSharedErrorMessages(value)", () =>{
+    describe("removeSharedErrorMessages(value)", () => {
       beforeEach(() => {
         jest.useFakeTimers();
         wrapper = mount(CurrentContractDetails, {
@@ -241,13 +242,13 @@ describe("Testing CurrentContractDetails Component", () => {
           store,
         });
       });
-  
-      afterEach(()=>{
+
+      afterEach(() => {
         jest.useRealTimers();
         jest.clearAllTimers();
       })
 
-      it("validates emptied errorMessages arrays", async () =>{
+      it("validates emptied errorMessages arrays", async () => {
         wrapper.vm.removeSharedErrorMessages(true)
         jest.advanceTimersByTime(3000);
         expect(wrapper.vm.$data.startDPSharedErrorMessages).toEqual([]);
@@ -255,17 +256,17 @@ describe("Testing CurrentContractDetails Component", () => {
       })
     })
 
-    describe("setHeadline()", ()=>{
-      const isCurrent = {isCurrent: true}
-      const isPrevious = {isCurrent: false}
-      beforeEach(()=>{
+    describe("setHeadline()", () => {
+      const isCurrent = { isCurrent: true }
+      const isPrevious = { isCurrent: false }
+      beforeEach(() => {
         wrapper.setData({
-          currentContract:{
+          currentContract: {
             contract_order_expiration_date: "2019-12-31"
           }
         })
       })
-      afterEach(()=>{
+      afterEach(() => {
         wrapper.setData({
           currentContract: currentContractDTO
         })
@@ -276,7 +277,7 @@ describe("Testing CurrentContractDetails Component", () => {
         wrapper.vm.setHeadline();
         expect(wrapper.vm.$data.headline).toContain('current');
       });
-  
+
       it("returns headline with `previous` word", async () => {
         wrapper.setData(isPrevious);
         wrapper.vm.setHeadline();
@@ -287,8 +288,8 @@ describe("Testing CurrentContractDetails Component", () => {
 
     describe("loadContract()", () => {
       currentContractDTO.instance_number = 1;
-      it("returns current contract from contacts in store", async () =>{
-        wrapper.setData({currentContract: currentContractDTO})
+      it("returns current contract from contacts in store", async () => {
+        wrapper.setData({ currentContract: currentContractDTO })
         await AcquisitionPackage.doSetCurrentContracts([currentContractDTO])
         await wrapper.vm.loadContract();
         expect(wrapper.vm.$data.currentContract).toEqual(
@@ -296,15 +297,15 @@ describe("Testing CurrentContractDetails Component", () => {
         )
       })
 
-      it("returns new blank contract", async () =>{
+      it("returns new blank contract", async () => {
         currentContractDTO.instance_number = 2;
         await AcquisitionPackage.doSetCurrentContracts([currentContractDTO]);
         await wrapper.vm.loadContract();
-        expect(Object.values(wrapper.vm.$data.currentContract).every(v=>v===""))
-       
+        expect(Object.values(wrapper.vm.$data.currentContract).every(v => v === ""))
+
       })
 
-      it("ensures $data.isCurrent is set as expected", async () =>{
+      it("ensures $data.isCurrent is set as expected", async () => {
         currentContractDTO.instance_number = 2;
         await AcquisitionPackage.doSetCurrentContracts([currentContractDTO]);
         await wrapper.vm.loadContract();
@@ -312,26 +313,26 @@ describe("Testing CurrentContractDetails Component", () => {
       })
     })
 
-    it("loadOnEnter () => loads existing contract", async () =>{
-      wrapper.setData({currentContract: currentContractDTO})
+    it("loadOnEnter () => loads existing contract", async () => {
+      wrapper.setData({ currentContract: currentContractDTO })
       await AcquisitionPackage.doSetCurrentContracts([currentContractDTO])
       await wrapper.vm.loadOnEnter();
       expect(Object.keys(wrapper.vm.savedData).sort())
-      .toEqual(Object.keys(wrapper.vm.currentContract).sort());
+        .toEqual(Object.keys(wrapper.vm.currentContract).sort());
     })
 
     describe("hasChanged()", () => {
-      const currentData = {instance_number: 12};
-      const savedData = {instance_number: 13};
-      
-      it("returns true", async () =>{
-        await wrapper.setData({currentData, savedData})
+      const currentData = { instance_number: 12 };
+      const savedData = { instance_number: 13 };
+
+      it("returns true", async () => {
+        await wrapper.setData({ currentData, savedData })
         const hasChanged = await wrapper.vm.hasChanged();
         expect(hasChanged).toBe(true)
-      }) 
-      
-      it("returns false", async () =>{
-        await wrapper.setData({currentData, savedData: currentData})
+      })
+
+      it("returns false", async () => {
+        await wrapper.setData({ currentData, savedData: currentData })
         const hasChanged = await wrapper.vm.hasChanged();
         expect(hasChanged).toBe(false)
       })
@@ -343,75 +344,75 @@ describe("Testing CurrentContractDetails Component", () => {
         instance_number: 12,
         acquisition_package: acqPkgId
       } as CurrentContractDTO;
-      const savedData = {instance_number: 13};
-      
-      beforeEach(()=>{
+      const savedData = { instance_number: 13 };
+
+      beforeEach(() => {
         wrapper.setData({
-          currentData, 
+          currentData,
           savedData,
-      })
+        })
         AcquisitionPackage.doSetPackageId(acqPkgId);
         jest.useFakeTimers();
       })
 
-      afterEach(()=>{
+      afterEach(() => {
         savedData.instance_number = 1;
         jest.useRealTimers();
         jest.clearAllTimers();
 
       })
-  
-      
-      it("if current and saved data has changed", async () =>{
+
+
+      it("if current and saved data has changed", async () => {
         await wrapper.vm.saveOnLeave();
         expect(wrapper.vm.currentData.acquisition_package).toBe(acqPkgId);
         expect(wrapper.vm.currentData.is_valid).toBe(true);
-      }) 
+      })
 
-      it("if current and saved data and sets $data.isCurrent", async () =>{
+      it("if current and saved data and sets $data.isCurrent", async () => {
         wrapper.setData({
-          currentContract:{
+          currentContract: {
             contract_order_expiration_date: "2019-12-31"
           }
         })
         await wrapper.vm.saveOnLeave();
         expect(wrapper.vm.$data.isCurrent).toBe(false);
-      }) 
-      
-      it("if current and saved data has changed and NO exception to fair opportunity", async () =>{
+      })
+
+      it("if current and saved data has changed and NO exception to fair opportunity", async () => {
         await AcquisitionPackage.doSetFairOpportunity(
-            {exception_to_fair_opportunity: "NO_NONE"}
+          { exception_to_fair_opportunity: "NO_NONE" }
         )
         const mockFunction = jest.spyOn(AcquisitionPackage, "updateCurrentContractsSNOW")
-          .mockImplementation(([currenData])=>Promise.resolve())
+          .mockImplementation(([currenData]) => Promise.resolve())
         await wrapper.vm.saveOnLeave();
         jest.advanceTimersByTime(3000);
         expect(mockFunction).toHaveBeenCalled();
-      }) 
+      })
 
-      it("mocks an error", async () =>{
+      it("mocks an error", async () => {
         const errMessage = 'error occurred'
         const mockFunction = jest.spyOn(AcquisitionPackage, "updateCurrentContractsSNOW")
           .mockRejectedValue(errMessage)
         const saveOnLeave = await wrapper.vm.saveOnLeave();
         expect(wrapper.vm.saveOnLeaveError).toBe(errMessage);
-      }) 
+      })
     });
 
-    it("sortDataSource() => sorts data source items based on time created ", async () =>{
+    it("sortDataSource() => sorts data source items based on time created ", async () => {
       const currentContracts = [
-        {instance_number: 1, sys_created_on: "10/12/2023"},
-        {instance_number: 2, sys_created_on: "10/12/2021"}
+        { instance_number: 1, sys_created_on: "10/12/2023" },
+        { instance_number: 2, sys_created_on: "10/12/2021" }
       ]
-      wrapper.setData({currentContracts});
+      wrapper.setData({ currentContracts });
       await wrapper.vm.sortDataSource();
       expect(wrapper.vm.$data.currentContracts[0].sys_created_on).toBe("10/12/2021");
     })
 
-    it("setMinAndMaxDates() => returns dates as expected ", async () =>{
+    it("setMinAndMaxDates() => returns dates as expected ", async () => {
       await wrapper.vm.setMinAndMaxDates();
       expect(wrapper.vm.$data.startMinDate).toBe("");
-      expect(wrapper.vm.$data.startMaxDate.substring(0,4))
+      expect(wrapper.vm.$data.startMaxDate.substring(0, 4))
         .toBe((new Date()).getFullYear().toString());
       expect(wrapper.vm.$data.expMinDate).toBe("");
       expect(wrapper.vm.$data.expMaxDate).toBe("");
