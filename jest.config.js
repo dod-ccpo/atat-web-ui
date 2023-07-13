70547e3ed806809d78828e415fd592807d539bdc
Andrew Nickerl <andrew.nickerl@hunterstrategy.net>
2023-07-13 12:57:33 -0700

    Add unit tests for ConflictOfInterest and CoILearnMore

--- jest.config.js
+++ jest.config.js
@@ -61,6 +61,8 @@ module.exports = {
     "<rootDir>/src/steps/03-Background/CurrentEnvironment/UploadMigrationDocuments.spec.ts",
     "<rootDir>/src/steps/05-PerformanceRequirements/DOW/DOWLandingPage.spec.ts",
     "<rootDir>/src/steps/07-OtherContractConsiderations/Travel.spec.ts",
+    "<rootDir>/src/steps/07-OtherContractConsiderations/CoILearnMore.spec.ts",
+    "<rootDir>/src/steps/07-OtherContractConsiderations/ConflictOfInterest.spec.ts",
     // "<rootDir>/src/steps/**/*.spec.ts",0
     "<rootDir>/src/store/**/*.spec.ts",
     "<rootDir>/src/router/**/*.spec.ts",
---
+++ src/steps/07-OtherContractConsiderations/ConflictOfInterest.spec.ts
@@ -0,0 +1,75 @@
+/* eslint-disable camelcase */
+
+import Vue from "vue";
+import Vuetify from "vuetify";
+import {createLocalVue, mount, Wrapper} from "@vue/test-utils";
+import {DefaultProps} from "vue/types/options";
+import ConflictOfInterest from "@/steps/07-OtherContractConsiderations/ConflictOfInterest.vue";
+import validators from "@/plugins/validation";
+import {CountriesApi} from "@/api/countries";
+import AcquisitionPackage from "@/store/acquisitionPackage";
+
+Vue.use(Vuetify);
+
+describe("Testing Conflict of Interest Page", () => {
+  const localVue = createLocalVue();
+  localVue.use(validators);
+  let vuetify: Vuetify;
+  let wrapper: Wrapper<DefaultProps & Vue>;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+
+    wrapper = mount(ConflictOfInterest, {
+      vuetify,
+      localVue,
+    });
+  });
+
+  describe("testing Conflict of Interest Page", () => {
+    it("renders successfully", async () => {
+      expect(wrapper.exists()).toBe(true);
+    });
+
+    it("opens slideout panel on click", async () => {
+      const openSlideoutPanelSpy = jest
+        .spyOn(wrapper.vm, 'openSlideoutPanel');
+      wrapper.find('#CoILearnMore').trigger('keydown.enter');
+      expect(openSlideoutPanelSpy).toHaveBeenCalled();
+    });
+
+    it("changes explanation when hasConflict changes to 'NO'", async () => {
+      wrapper.setData({ hasConflict: 'YES', explanation: 'Test explanation' });
+      await wrapper.vm.$nextTick();
+      wrapper.setData({ hasConflict: 'NO' });
+      await wrapper.vm.$nextTick();
+      expect(wrapper.vm.explanation).toBe('');
+    });
+
+    it("saves data on leave when data has changed", async () => {
+      AcquisitionPackage.saveData = jest.fn();
+      const saveOnLeaveSpy = jest.spyOn(wrapper.vm, 'saveOnLeave');
+      const isChangedSpy = jest
+        .spyOn(wrapper.vm, 'isChanged').mockReturnValue(true);
+      await wrapper.vm.saveOnLeave();
+      expect(isChangedSpy).toHaveBeenCalled();
+      expect(saveOnLeaveSpy).toHaveBeenCalled();
+    });
+
+    it("loads data on enter", async () => {
+      AcquisitionPackage.loadData = jest.fn().mockReturnValue(true);
+      const loadOnEnterSpy = jest.spyOn(wrapper.vm, 'loadOnEnter');
+      await wrapper.vm.loadOnEnter();
+      expect(loadOnEnterSpy).toHaveBeenCalled();
+    });
+
+    it("saveOnLeave should catch and log error", async () => {
+      console.log = jest.fn();
+      jest.spyOn(AcquisitionPackage, 'saveData').mockImplementation(() => {
+        throw new Error("mock error");
+      });
+      await wrapper.vm.saveOnLeave();
+      expect(console.log).toHaveBeenCalledWith(Error("mock error"));
+    })
+  })
+})
