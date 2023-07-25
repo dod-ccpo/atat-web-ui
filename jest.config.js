50d997f681bf315727f77b960855896c535db63c
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2023-07-25 13:38:35 -0500

    init

--- jest.config.js
+++ jest.config.js
@@ -61,6 +61,7 @@ module.exports = {
     "<rootDir>/src/steps/03-Background/CurrentEnvironment/CurrentEnvironment.spec.ts",
     "<rootDir>/src/steps/03-Background/CurrentEnvironment/UploadMigrationDocuments.spec.ts",
     "<rootDir>/src/steps/03-Background/CurrentEnvironment/CurrentEnvironmentLocation.spec.ts",
+    "<rootDir>/src/steps/03-Background/CurrentEnvironment/EnvironmentSummary.spec.ts",
     "<rootDir>/src/steps/03-Background/CurrentEnvironment/InstanceDetails.spec.ts",
     "<rootDir>/src/steps/05-PerformanceRequirements/DOW/DOWLandingPage.spec.ts",
     "<rootDir>/src/steps/07-OtherContractConsiderations/*.spec.ts",
