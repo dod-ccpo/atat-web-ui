3c0a1d1c57060d74fd82b4d0e960d646b43e5b19
Logan Johnston <logan.johnston@hunterstrategy.net>
2023-07-26 16:30:35 -0600

    AT-9402: updating markup and styling

--- src/portfolios/portfolio/CreateFirstPortfolio.vue
+++ src/portfolios/portfolio/CreateFirstPortfolio.vue
@@ -1,93 +1,80 @@
 <template>
-  <div class="pa-5">
-    <v-main>
-      <v-row>
-        <v-col>
-          <div>
-            <div class="d-flex">
-              <div>
-                <h1 class="page-header">
-                  Ready to create your first ATAT portfolio?
-                </h1>
-                <v-row class="d-flex v-list">
-                  <v-col class="mr-15" style="max-width: 545px">
-                    <div class="copy-max-width">
-                      <p>
-                        We’ll guide you through steps to start provisioning
-                        cloud resources and building an ATAT portfolio. Once set
-                        up, you’ll be provided a for viewing funding reports and
-                        managing access to your cloud console.
-                      </p>
-                      <p class="h4">You can expect to:</p>
-                      <v-list class="_atat-stepper mb-10">
-                        <v-list-item-group>
-                          <v-list-item
-                            v-for="(stepText, index) in provisionStepsText"
-                            :key="index"
-                          >
-                            <span class="_step-circle">{{ index + 1 }}</span>
-                            <v-list-item-content>
-                              {{ stepText }}
-                            </v-list-item-content>
-                          </v-list-item>
-                        </v-list-item-group>
-                      </v-list>
-                      <div class="mb-6 pa-6 rounded-lg _card-soft-shadow">
-                        <TaskOrderSearch
-                          label="Get started by adding your task order number"
-                          labelClass="h3"
-                          :TONumber.sync="TONumber"
-                          @startProvisionWorkflow="startProvisionWorkflow"
-                          :wrapperWidth="searchWidth"
-                          :width="searchWidth"
-                        />
-                      </div>
-                      <ATATExpandableLink aria-id="DataRetrievedFrom">
-                        <template v-slot:header>
-                          What if I don’t have a task order?
-                        </template>
-                        <template v-slot:content>
-                          <p class="mb-4">
-                            First, you will need to
-                            <a
-                              id="createAPackageLink"
-                              role="button"
-                              @click="startNewAcquisition"
-                              @keydown.enter="startNewAcquisition"
-                              @keydown.space="startNewAcquisition"
-                              >create a package</a
-                            >
-                            using the DISA Acquisition Package Preparation
-                            System (DAPPS). Upon submission of your requirements
-                            package, your Contracting Office will review the
-                            completed documents and begin the procurement
-                            process.
-                          </p>
-                          <p>
-                            Your Contracting Office will notify you when a task
-                            order has been awarded for your requirement.
-                          </p>
-                        </template>
-                      </ATATExpandableLink>
-                    </div>
-                  </v-col>
-                  <v-col>
-                    <ATATSVGIcon
-                      width="545"
-                      height="481"
-                      name="startProvisioning"
-                      color="primary"
-                    />
-                  </v-col>
-                </v-row>
-              </div>
-            </div>
-            <ATATFooter />
+  <v-main>
+    <div class="container-max-width-wide" style="margin: 0 auto;">
+    <h1 class="page-header">Ready to create your first ATAT portfolio?</h1>
+    <v-row>
+      <v-col col="7" class="mr-15" style="max-width: 545px">
+        <div class="copy-max-width">
+          <p>
+            We’ll guide you through steps to start provisioning cloud resources
+            and building an ATAT portfolio. Once set up, you’ll be provided a
+            for viewing funding reports and managing access to your cloud
+            console.
+          </p>
+          <p class="h4">You can expect to:</p>
+          <v-list class="_atat-stepper mb-10">
+            <v-list-item-group>
+              <v-list-item
+                v-for="(stepText, index) in provisionStepsText"
+                :key="index"
+              >
+                <span class="_step-circle">{{ index + 1 }}</span>
+                <v-list-item-content>
+                  {{ stepText }}
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+          <div class="mb-6 pa-6 rounded-lg _card-soft-shadow">
+            <TaskOrderSearch
+              label="Get started by adding your task order number"
+              labelClass="h3"
+              :TONumber.sync="TONumber"
+              @startProvisionWorkflow="startProvisionWorkflow"
+              :wrapperWidth="searchWidth"
+              :width="searchWidth"
+            />
           </div>
-        </v-col>
-      </v-row>
-    </v-main>
+          <ATATExpandableLink aria-id="DataRetrievedFrom">
+            <template v-slot:header>
+              What if I don’t have a task order?
+            </template>
+            <template v-slot:content>
+              <p class="mb-4">
+                First, you will need to
+                <a
+                  id="createAPackageLink"
+                  role="button"
+                  @click="startNewAcquisition"
+                  @keydown.enter="startNewAcquisition"
+                  @keydown.space="startNewAcquisition"
+                  >create a package</a
+                >
+                using the DISA Acquisition Package Preparation System (DAPPS).
+                Upon submission of your requirements package, your Contracting
+                Office will review the completed documents and begin the
+                procurement process.
+              </p>
+              <p>
+                Your Contracting Office will notify you when a task order has
+                been awarded for your requirement.
+              </p>
+            </template>
+          </ATATExpandableLink>
+        </div>
+      </v-col>
+      <v-col cols="5" > 
+        <ATATSVGIcon
+          width="545"
+          height="481"
+          name="startProvisioning"
+          color="primary"
+        />
+      </v-col>
+    </v-row>
+      <ATATFooter/>
   </div>
+  </v-main>
 </template>
 
 <script lang="ts">
