371688c72db88aef0c2d40bbabe6439268fed08e
Logan Johnston <logan.johnston@hunterstrategy.net>
2023-07-28 10:21:38 -0600

    AT-9361: removing some tests to check if tests are causing exit code

--- src/steps/08-StandardsAndCompliance/BAA.spec.ts
+++ src/steps/08-StandardsAndCompliance/BAA.spec.ts
@@ -24,9 +24,9 @@ describe("Testing BAA Page", () => {
   });
 
   describe("testing BAA.vue", () => {
-    // it("renders successfully", async () => {
-    //   expect(wrapper.exists()).toBe(true);
-    // });
+    it("renders successfully", async () => {
+      expect(wrapper.exists()).toBe(true);
+    });
 
     // it("opens slideout panel", () => {
     //   jest.spyOn(SlideoutPanel, "openSlideoutPanel").mockImplementation();
@@ -37,54 +37,54 @@ describe("Testing BAA Page", () => {
     //   );
     // });
 
-    it("testing @keydown.space to trigger openSlideoutPanel ", async () => {
-      const anchorLink = wrapper.find("#LearnMoreBAA");
-      anchorLink.trigger('keydown.space'); // trigger openSlideoutPanel;
-      const currentTargetId = 1;
-      wrapper.vm.openSlideoutPanel({ currentTarget: { id: currentTargetId } });
-      expect(SlideoutPanel.openSlideoutPanel).toHaveBeenCalledWith(
-        currentTargetId
-      );
-    });
+    // it("testing @keydown.space to trigger openSlideoutPanel ", async () => {
+    //   const anchorLink = wrapper.find("#LearnMoreBAA");
+    //   anchorLink.trigger('keydown.space'); // trigger openSlideoutPanel;
+    //   const currentTargetId = 1;
+    //   wrapper.vm.openSlideoutPanel({ currentTarget: { id: currentTargetId } });
+    //   expect(SlideoutPanel.openSlideoutPanel).toHaveBeenCalledWith(
+    //     currentTargetId
+    //   );
+    // });
 
-    it("testing @keydown.enter to trigger openSlideoutPanel ", async () => {
-      const anchorLink = wrapper.find("#LearnMoreBAA");
-      anchorLink.trigger('keydown.enter'); // trigger openSlideoutPanel;
-      const currentTargetId = 1;
-      wrapper.vm.openSlideoutPanel({ currentTarget: { id: currentTargetId } });
-      expect(SlideoutPanel.openSlideoutPanel).toHaveBeenCalledWith(
-        currentTargetId
-      );
-    });
+    // it("testing @keydown.enter to trigger openSlideoutPanel ", async () => {
+    //   const anchorLink = wrapper.find("#LearnMoreBAA");
+    //   anchorLink.trigger('keydown.enter'); // trigger openSlideoutPanel;
+    //   const currentTargetId = 1;
+    //   wrapper.vm.openSlideoutPanel({ currentTarget: { id: currentTargetId } });
+    //   expect(SlideoutPanel.openSlideoutPanel).toHaveBeenCalledWith(
+    //     currentTargetId
+    //   );
+    // });
 
-    it("gets current data", () => {
-      const mockPackageId = "1";
-      AcquisitionPackage.doSetPackageId(mockPackageId)
-      const currentData = wrapper.vm.currentData;
-      expect(currentData.baa_required).toBe("");
-      expect(currentData.acquisition_package).toBe(mockPackageId);
-    });
+    // it("gets current data", () => {
+    //   const mockPackageId = "1";
+    //   AcquisitionPackage.doSetPackageId(mockPackageId)
+    //   const currentData = wrapper.vm.currentData;
+    //   expect(currentData.baa_required).toBe("");
+    //   expect(currentData.acquisition_package).toBe(mockPackageId);
+    // });
 
-    it("checks for changes", async () =>{
-      await wrapper.setData(
-        {
-          currentData:{
-            "baa_required": "true",
-            "acquisition_package": "currentPackageId"
-          },
-          savedData:{
-            "baa_required": "false",
-            "acquisition_package": "savedPackageId"
-          }
-        }
-      )
-      const hasChanges = wrapper.vm.hasChanged()
-      expect(hasChanges).toBe(true)
-    })
+    // it("checks for changes", async () =>{
+    //   await wrapper.setData(
+    //     {
+    //       currentData:{
+    //         "baa_required": "true",
+    //         "acquisition_package": "currentPackageId"
+    //       },
+    //       savedData:{
+    //         "baa_required": "false",
+    //         "acquisition_package": "savedPackageId"
+    //       }
+    //     }
+    //   )
+    //   const hasChanges = wrapper.vm.hasChanged()
+    //   expect(hasChanges).toBe(true)
+    // })
 
-    it("checks saveOnLeave", async () =>{
-      const hasChanges = await wrapper.vm.saveOnLeave()
-      expect(hasChanges).toBe(true)
-    })
+    // it("checks saveOnLeave", async () =>{
+    //   const hasChanges = await wrapper.vm.saveOnLeave()
+    //   expect(hasChanges).toBe(true)
+    // })
   });
 });
