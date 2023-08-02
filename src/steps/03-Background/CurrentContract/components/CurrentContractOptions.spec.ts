1c41a7aa6e23e943d85b663062bafb64e663e1ff
ANTHONY SUMNER <anthony.l.sumner4.ctr@mail.mil>
2023-08-02 09:28:54 -0400

    [x] completed CurrentContractOptions test AT-7281

--- src/steps/03-Background/CurrentContract/components/CurrentContractOptions.spec.ts
+++ src/steps/03-Background/CurrentContract/components/CurrentContractOptions.spec.ts
@@ -15,13 +15,6 @@ describe("Testing CurrentContractOptions Component", () => {
   let wrapper: Wrapper<DefaultProps & Vue>;
   localVue.use(validators);
 
-    // object to determine an exception to fair opportunity
-    const hasEFO =  {exception_to_fair_opportunity: "YES_FAR_16_505_B_2_I_C"};
-    // object to determine NO exception to fair opportunity
-    const hasNoEFO = {exception_to_fair_opportunity: "NO_NONE"};
-    // object to determine EMPTY exception to fair opportunity
-    const hasEmptyEFO = {exception_to_fair_opportunity: ""};
-
   beforeEach(() => {
     vuetify = new Vuetify();
     wrapper = mount(CurrentContractOptions, {
@@ -30,35 +23,42 @@ describe("Testing CurrentContractOptions Component", () => {
     });
   });
 
-  afterEach(() => {
-  });
-
   it("renders successfully", async () => {
     expect(wrapper.exists()).toBe(true);
   });
 
-  describe("DATA ()=>", ()=>{
-    it("expected currentContractOptions if $prop isWizard === true", async()=>{
-      await wrapper.setProps({
-        isWizard: true,
-        hasExceptionToFairOpportunity: true
+  describe("GETTERS()=>", ()=>{
+    describe("currentContractOptions ()=>", ()=>{
+      it("expected currentContractOptions.label if $prop isWizard", async()=>{
+        await wrapper.setProps({
+          isWizard: true,
+          hasExceptionToFairOpportunity: true
+        })
+        const currentContractOptions = wrapper.vm.currentContractOptions;
+        // ensures both labels are not `Yes.` or `No.`
+        const expectedLabelsIfIsWizard = currentContractOptions.every(
+          ((cco: RadioButton) => {
+            return cco.label.length > 4
+          }
+        ));
+        expect(expectedLabelsIfIsWizard).toBe(true)
       })
-      expect(wrapper.vm.$data.currentContractOptions.every(
-        ((cco: RadioButton) => {
-          console.log(cco.label)
-          return cco.label.indexOf('previous') > 0
-        }
-      ))).toBe(false)
+
+      it("expected currentContractOptions.label if $prop !isWizard", async()=>{
+        await wrapper.setProps({
+          isWizard: false,
+        })
+        const currentContractOptions = wrapper.vm.currentContractOptions;
+        // ensures both labels are either `Yes.` or `No.`
+        const expectedLabelsIfIsWizard = currentContractOptions.every(
+          ((cco: RadioButton) => {
+            return cco.label.length <= 4
+          }
+        ));
+        expect(expectedLabelsIfIsWizard).toBe(true)
+      })
     })
-    // it("getYesLabel()=> returns string WITHOUT `previous`", async()=>{
-    //   await wrapper.setProps({
-    //     hasExceptionToFairOpportunity: false
-    //   })
-    //   expect(wrapper.vm.getYesLabel()).not.toContain(`previous`);
-    // })
   })
-
-
 
   describe("FUNCTIONS ()=>", ()=>{
     it("getYesLabel()=> returns string WITH `previous`", async()=>{
