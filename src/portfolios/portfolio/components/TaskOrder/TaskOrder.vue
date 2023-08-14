ed82772955a61759d1e788318b7f650cd32a4e70
Logan Johnston <logan.johnston@hunterstrategy.net>
2023-08-14 09:05:39 -0600

    AT-9599: updaing the financial alert into a component and putting it into the Task order page

--- src/portfolios/portfolio/components/TaskOrder/TaskOrder.vue
+++ src/portfolios/portfolio/components/TaskOrder/TaskOrder.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
+  <div class="container-max-width">
     <div v-if="!showDetails">
+      <div class="mb-10">
+        <FinancialDetailsAlert />
+      </div>
       <h2 class="pb-3">All task orders</h2>
       <div class="d-flex justify-space-between">
         <p class="mr-10">
@@ -36,6 +39,7 @@
 /* eslint-disable camelcase */
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+import FinancialDetailsAlert from "../../FinancialDetailsAlert.vue";
 import TaskOrderCard from "@/portfolios/portfolio/components/TaskOrder/TaskOrderCard.vue";
 import {TaskOrderCardData} from "../../../../../types/Global";
 import TaskOrderDetails from "@/portfolios/portfolio/components/TaskOrder/TaskOrderDetails.vue";
@@ -47,7 +51,8 @@ import PortfolioStore from "@/store/portfolio";
 @Component({
   components: {
     TaskOrderCard,
-    TaskOrderDetails
+    TaskOrderDetails,
+    FinancialDetailsAlert
   }
 })
 export default class TaskOrder extends Vue {
