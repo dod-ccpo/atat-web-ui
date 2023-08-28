8f94a798b8d34acaec94c7d0dc456b5450469b94
Andrew Nickerl <andrew.nickerl@hunterstrategy.net>
2023-08-28 13:06:31 -0700

    Stub out Step 8 Summary Page and complete navigation requirements

---
+++ src/steps/10-FinancialDetails/SummaryStepEight.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container fluid class="container-max-width">
+    <v-row>
+      <v-col class="col-12">
+        <h1 class="page-header mb-3">
+          Your Financial Details Summary
+        </h1>
+        <div class="copy-max-width">
+          <p class="mb-10">
+            {{ description }}
+          </p>
+        </div>
+
+        <ATATSummaryItems
+            :summaryItems = summaryItems>
+        </ATATSummaryItems>
+      </v-col>
+    </v-row>
+
+  </v-container>
+</template>
+
+<script lang="ts">
+/* eslint camelcase: 0, prefer-const: 1 */
+import { Component, Mixins} from "vue-property-decorator";
+import { SummaryItem } from "types/Global";
+import ATATSummaryItems from "@/components/ATATSummaryItem.vue";
+import Vue from "vue";
+import Summary, { getSummaryItemsforStep, isStepComplete } from "@/store/summary";
+import SaveOnLeave from "@/mixins/saveOnLeave";
+import AcquisitionPackage from "@/store/acquisitionPackage";
+
+@Component({
+  components: {
+    ATATSummaryItems
+  },
+})
+export default class SummaryStepEight extends Mixins(SaveOnLeave){
+  public summaryItems: SummaryItem[] = [];
+
+  get description():string{
+    return (isStepComplete(8))
+      ? "NEEDS COMPLETED PARAGRAPH"
+      : "We need some more details for this section. You can add info now, or come back to make " +
+      "edits at any time. When you are ready to wrap up this section, we will generate documents " +
+      "to complete your acquisition package."
+  }
+  /*
+   */
+
+  public async mounted(): Promise<void>{
+    await Summary.validateStepEight();
+    this.summaryItems = await getSummaryItemsforStep(8);
+    await Summary.toggleButtonColor(8);
+  }
+
+
+  protected async saveOnLeave(): Promise<boolean> {
+    await Summary.toggleButtonColor(-1);
+    return true;
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
