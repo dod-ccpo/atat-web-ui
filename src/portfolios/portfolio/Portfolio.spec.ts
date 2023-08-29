8e0fa045977bdbeb0289f11bc2e68350c34b6f6c
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2023-08-28 21:10:39 -0400

    Code cleanup

--- src/portfolios/portfolio/Portfolio.spec.ts
+++ src/portfolios/portfolio/Portfolio.spec.ts
@@ -4,19 +4,8 @@ import Vuetify from "vuetify";
 import { createLocalVue, mount, shallowMount, Wrapper } from "@vue/test-utils";
 import { DefaultProps } from "vue/types/options";
 import Portfolio from "./Portfolio.vue";
-import { AlertDTO } from "@/api/models";
-//@ericyoungquist-ccpo 
-//import { FundingAlertData } from "@/store/portfolio";
 import ATATCharts from "@/store/charts";
 import dashboardMocks from "@/dashboards/__tests__/dashboardMocks..json";
-
-
-const FundingAlertTypes = {
-  POPExpiresSoonNoTOClin: "POPExpiresSoonDaysNoTOClin",
-  POPExpiresSoonWithTOClin: "POPExpiresSoonWithTOClin",
-  POPExpiresSoonWithLowFunds: "POPExpiresSoonWithLowFunds",
-  POPExpired: "POPExpired",
-};
 
 Vue.use(Vuetify);
 
@@ -24,84 +13,6 @@ describe("Testing Portfolio", () => {
   const localVue = createLocalVue();
   let vuetify: Vuetify;
   let wrapper: Wrapper<DefaultProps & Vue, Element>;
-  const alertKeys = {
-    SixtyDaysSeventyFivePercent: "SixtyDaysSeventyFivePercent",
-    Expired: "Expired",
-  };
-
-  let alertsKey = alertKeys.SixtyDaysSeventyFivePercent;
-
-  const alerts_60days_75percent: AlertDTO[] = [
-    {
-      clin: "",
-      task_order: "tsk_12345678",
-      active: "true",
-      alert_type: "SPENDING_ACTUAL",
-      threshold_violation_amount: "75",
-      last_notification_date: "",
-      portfolio: "",
-    },
-    {
-      clin: "",
-      task_order: "tsk_12345678919",
-      active: "true",
-      alert_type: "TIME_REMAINING",
-      threshold_violation_amount: "60",
-      last_notification_date: "",
-      portfolio: "",
-    },
-  ];
-
-  const alerts_expired: AlertDTO[] = [
-    {
-      clin: "",
-      task_order: "tsk_12345678919",
-      active: "true",
-      alert_type: "TIME_REMAINING",
-      threshold_violation_amount: "-30",
-      last_notification_date: "",
-      portfolio: "",
-    },
-  ];
-
-  // const getFundingTrackerAlertMock = () => {
-  //   const fundingAlertData: FundingAlertData = {
-  //     alerts: [],
-  //     daysRemaining: 0,
-  //     spendingViolation: 0,
-  //     fundingAlertType: "",
-  //     hasLowFundingAlert: false,
-  //   };
-
-  //   switch (alertsKey) {
-  //   case alertKeys.SixtyDaysSeventyFivePercent:
-  //     fundingAlertData.alerts = alerts_60days_75percent;
-  //     fundingAlertData.daysRemaining = 60;
-  //     fundingAlertData.spendingViolation = 75;
-  //     fundingAlertData.hasLowFundingAlert = true;
-  //     fundingAlertData.fundingAlertType =
-  //         FundingAlertTypes.POPExpiresSoonWithLowFunds;
-  //     break;
-  //   case alertKeys.Expired:
-  //     fundingAlertData.alerts = alerts_expired;
-  //     fundingAlertData.daysRemaining = -30;
-  //     fundingAlertData.spendingViolation = 0;
-  //     fundingAlertData.hasLowFundingAlert = false;
-  //     fundingAlertData.fundingAlertType = FundingAlertTypes.POPExpired;
-  //   }
-
-  //   return fundingAlertData;
-  // };
-
-  // jest.mock("@/store/portfolio", () => ({
-  //   ...jest.requireMock("@/store/portfolio"),
-  //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  //   getFundingTrackerAlert: (
-  //     taskOrderNumber: string
-  //   ): Promise<FundingAlertData> => {
-  //     return new Promise((resolve) => resolve(getFundingTrackerAlertMock()));
-  //   },
-  // }));
 
   beforeEach(() => {
     vuetify = new Vuetify();
@@ -186,31 +97,9 @@ describe("Testing Portfolio", () => {
       expect(wrapper.vm.$data.fundsSpent).toBe(550);
     });
 
-    // it("Test calculateTimeToExpiration", async () => {
-    //   vuetify = new Vuetify();
-
-    //   alertsKey = alertKeys.Expired;
-    //   wrapper = shallowMount(Portfolio, {
-    //     vuetify,
-    //     localVue,
-    //   });
-
-    //   await wrapper.setData({
-    //     taskOrder: {
-    //       pop_end_date: '2022-12-31'
-    //     }
-    //   });
-
-    //   wrapper.vm.calculateTimeToExpiration();
-    //   const runOutOfFundsDate = wrapper.vm.$data.runOutOfFundsDate;
-    //   expect(runOutOfFundsDate.length).toBeGreaterThan(0);
- 
-    // });
-
     it("Test loadOnEnter", async () => {
       vuetify = new Vuetify();
 
-      alertsKey = alertKeys.Expired;
       wrapper = shallowMount(Portfolio, {
         vuetify,
         localVue,
