d213e214e9f685f9172c5a57272f363451f83c98
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2023-09-11 11:37:35 -0500

    fix description for step 4

--- src/store/summary/index.ts
+++ src/store/summary/index.ts
@@ -1263,9 +1263,8 @@ export class SummaryStore extends VuexModule {
     const prevContracts = currentContracts?.length === 1
       ? `${currentContracts?.length} previous contract:\n${contractNumbers}`
       : `${currentContracts?.length} previous contracts:\n${contractNumbers}`
-
-    const description = isTouched && currentContracts && currentContracts.length > 0
-      && currentContracts[0].contract_number ?
+    debugger
+    const description = isTouched && currentContracts && currentContracts.length > 0?
       hasCurrentOrPreviousContract === "YES"
         ? prevContracts
         : "No previous contracts"
