ad41f85628931cb835407f6644bfec91a89fa0b2
DGabriel-CCPO <devonte.gabriel-ctr@ccpo.mil>
2023-09-15 12:22:39 -0500

    fix for removing classification on edit and showing as missing data

--- src/steps/03-Background/CurrentEnvironment/InstanceDetails.vue
+++ src/steps/03-Background/CurrentEnvironment/InstanceDetails.vue
@@ -401,7 +401,9 @@ export default class InstanceDetails extends Mixins(SaveOnLeave) {
       this.classificationRadioOptions = buildClassificationCheckboxList(
         filteredClassificationObjects, "", false, true, "short"
       );
-
+      if(!envClassificationLevelSysIds.includes(this.instanceData.classification_level)){
+        this.instanceData.classification_level = ""
+      }
       this.setClassificationLabels();
     }   
   }
