d1be910b57c94082662e725c0e17b7a4fc8cede7
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2023-09-27 19:49:52 -0400

    Added logic for burn lines in spend chart

--- src/api/helpers/index.ts
+++ src/api/helpers/index.ts
@@ -1,4 +1,4 @@
-import { BaseTableDTO, ReferenceColumn } from "../models"
+import { BaseTableDTO, DisplayColumn, ReferenceColumn } from "../models"
 
 export const convertColumnReferencesToValues = <TableDTO extends BaseTableDTO> 
   (DTO: TableDTO): TableDTO => {
@@ -12,8 +12,29 @@ export const convertColumnReferencesToValues = <TableDTO extends BaseTableDTO>
     if (keyValue) {
       const refColumn = (keyValue as unknown) as ReferenceColumn;
 
-      if (refColumn.value) {
+      if (refColumn.value !== undefined) {
         record[key.toString()] = refColumn.value;
+      }  
+    }
+  }
+
+  return JSON.parse(JSON.stringify(record)) as TableDTO;
+}
+
+export const convertDisplayColumnsToDisplayValues = <TableDTO extends BaseTableDTO> 
+  (DTO: TableDTO, columns?: string[]): TableDTO => {
+
+  const record = (DTO as unknown) as Record<string, string>;
+
+  let key: keyof TableDTO;
+  for (key in DTO) {  
+    const keyValue = DTO[key]; 
+    
+    if (keyValue) {
+      const refColumn = (keyValue as unknown) as DisplayColumn;
+
+      if (refColumn.display_value !== undefined && (!columns || columns.includes(key))) {
+        record[key.toString()] = refColumn.display_value;
       }  
     }
   }
