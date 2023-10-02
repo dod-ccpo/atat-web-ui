deada055de1a547dcaf6ad9d12454a9474dec6ad
davidpietrzak-ccpo <david.t.pietrzak.ctr@mail.mil>
2023-10-02 09:23:01 -0500

    Added jest tests
    Tests check the most basic summary item functionality, and acor contact data

---
+++ src/steps/01-AcquisitionPackageDetails/SummaryStepOne.spec.ts
@@ -0,0 +1,54 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import { createLocalVue, mount, Wrapper } from "@vue/test-utils";
+import { DefaultProps } from "vue/types/options";
+import SummaryStepOne from "@/steps/01-AcquisitionPackageDetails/SummaryStepOne.vue"
+import Summary, * as SummaryExportedFunctions from "@/store/summary";
+
+Vue.use(Vuetify);
+
+describe("Testing SummaryStepOne Component", () => {
+  const localVue = createLocalVue();
+  let vuetify: Vuetify;
+  let wrapper: Wrapper<DefaultProps & Vue, Element>;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    wrapper = mount(SummaryStepOne, {
+      vuetify,
+      localVue
+    });
+  });
+
+  describe("testing SummaryStepOne render", () => {
+    it("renders successfully", async () => {
+      expect(wrapper.exists()).toBe(true);
+    });
+  })
+
+  describe("GETTERS", () => {
+    describe("introParagraph()=> ", () => {
+      it("returns `We need some more details` statement", async () => {
+        jest.spyOn(SummaryExportedFunctions,"isStepComplete").mockReturnValueOnce(false);
+        wrapper.vm.setIntroParagraph()
+        expect(wrapper.vm.$data.introParagraph).toContain("We need some more details");
+      });
+      it("returns `You are all done` statement", async () => {
+        jest.spyOn(SummaryExportedFunctions,"isStepComplete").mockReturnValueOnce(true);
+        wrapper.vm.setIntroParagraph()
+        expect(wrapper.vm.$data.introParagraph).toContain("You are all done");
+      });
+    })
+  })
+
+  describe("FUNCTIONS", () => {
+    it("saveOnLeave()=> expect function to be called", async () => {
+      const toggleButtonColorMock = jest.spyOn(Summary, "toggleButtonColor").mockImplementation(
+        ()=> Promise.resolve()
+      );
+      await wrapper.vm.saveOnLeave();
+      expect(toggleButtonColorMock).toHaveBeenCalled();
+    });
+  })
+
+})
