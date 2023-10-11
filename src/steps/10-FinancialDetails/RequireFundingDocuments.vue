396b3a1eae7f21f01196eed8eb0da4065bbf5c4e
davidpietrzak-ccpo <david.t.pietrzak.ctr@mail.mil>
2023-10-11 08:38:25 -0500

    finished page UI and added stubs for funcitons

--- src/steps/10-FinancialDetails/RequireFundingDocuments.vue
+++ src/steps/10-FinancialDetails/RequireFundingDocuments.vue
@@ -14,6 +14,16 @@
             or Military Interdepartmental Purchase Request (MIPR)).
           </p>
         </div>
+        <div style="max-width: 180px">
+          <ATATRadioGroup
+            :card="true"
+            class="max-width-640"
+            id="GInvoicingOptions"
+            :items="requireFundingOptions"
+            :value.sync="requireFundingSelection"
+            :rules="[$validators.required('Please select an option')]"
+          />
+        </div>
       </v-col>
     </v-row>
   </v-container>
@@ -22,6 +32,8 @@
 import { Component, Mixins } from "vue-property-decorator";
 import ATATRadioGroup from "@/components/ATATRadioGroup.vue";
 import SaveOnLeave from "@/mixins/saveOnLeave";
+import { RadioButton } from "types/Global";
+import FinancialDetails from "@/store/financialDetails";
 
 @Component({
   components: {
@@ -29,6 +41,28 @@ import SaveOnLeave from "@/mixins/saveOnLeave";
   },
 })
 export default class RequireFundingDocuments extends Mixins(SaveOnLeave) {
+  private requireFundingSelection = "YES";
+  private requireFundingOptions: RadioButton[] = [
+    {
+      id: "Yes",
+      label: "Yes.",
+      value: "YES",
+    },
+    {
+      id: "No",
+      label: "No.",
+      value: "NO",
+    },
+  ];
+
+  async loadFundingRequestData(): Promise<void> {
+    await FinancialDetails.loadIFPData();
+  }
+
+  public async mounted(): Promise<void> {
+    console.log("mounted");
+  }
+
   protected async saveOnLeave(): Promise<boolean> {
     return true;
   }
