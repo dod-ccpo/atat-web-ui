73b8f21dd5db5163c13b5ce6485ea85f7446cd41
ericyoungquist-ccpo <eric.youngquist-ctr@ccpo.mil>
2023-10-18 21:43:04 -0400

    Added unit test

--- src/store/acquisitionPackage/index.spec.ts
+++ src/store/acquisitionPackage/index.spec.ts
@@ -2,11 +2,14 @@
 import {createLocalVue} from "@vue/test-utils";
 import Vuex, {Store} from "vuex";
 import {getModule} from "vuex-module-decorators";
-import { 
+import AcquisitionPackage, { 
   AcquisitionPackageStore, 
   initialEvaluationPlan 
 } from "@/store/acquisitionPackage/index";
-import { EvaluationPlanDTO } from "@/api/models";
+import { ContactDTO, EvaluationPlanDTO, FundingRequirementDTO } from "@/api/models";
+import FinancialDetails from "../financialDetails";
+import api from "@/api";
+import Vue from "vue";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
@@ -27,6 +30,41 @@ describe("AcquisitionPackage Store", () => {
     standard_specifications: "",
     custom_specifications: "",    
   } 
+
+  const emptyContact: ContactDTO = {
+    type:  "", 
+    role:  "", 
+    rank_components:  "",
+    salutation:  "",
+    first_name:  "John",
+    last_name:  "Doe",
+    middle_name:  "",
+    formal_name:  "",
+    suffix:  "",
+    title:  "",
+    phone:  "",
+    phone_extension:  "",
+    email:  "",
+    grade_civ:  "",
+    dodaac:  "",
+    can_access_package:  "",
+    manually_entered:  "",
+    acquisition_package:  "",
+  };
+
+  const emptyFundingRequirementDTO: FundingRequirementDTO = {
+    acquisition_package:  "",
+    funding_plan:  "",
+    funding_request:  "",
+    funds_obligated:  "",
+    funds_total:  "",
+    has_funding:  "",
+    incrementally_funded:  "",
+    pop_start_date:  "",
+    pop_end_date:  "",
+    task_order_number:  "",
+    financial_poc:  "",
+  }
 
   beforeEach(() => {
     const createStore = (storeOptions = {}): Store<{ 
@@ -70,5 +108,25 @@ describe("AcquisitionPackage Store", () => {
     const evalPlan = acqPackageStore.getEvaluationPlan;
     expect(evalPlan?.source_selection).toBe("TECH_PROPOSAL");
   });
+
+  it ("saveContactInfo()", async () => {
+    const testData = {
+      data: emptyContact,
+      type: "Financial POC"
+    }
+    AcquisitionPackage.setContact(testData);
+    await Vue.nextTick();
+    const mockSetFinancialPOC = jest.spyOn(FinancialDetails, "setFinancialPOC")
+      .mockImplementation();
+    jest.spyOn(api.contactsTable, "create").mockImplementation(
+      () => Promise.resolve(emptyContact));
+    jest.spyOn(AcquisitionPackage, "updateAcquisitionPackage").mockImplementation();
+    jest.spyOn(api.fundingRequirementTable, "update").mockImplementation();
+    jest.spyOn(FinancialDetails, "getFundingRequirement").mockImplementation(
+      () => Promise.resolve(emptyFundingRequirementDTO)
+    )
+    await AcquisitionPackage.saveContactInfo(testData);
+    expect(mockSetFinancialPOC).toBeCalled();
+  })
 
 });
