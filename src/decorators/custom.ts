6728243c1ff91f1dcfb0342e384db8a6c7b70816
Tony Sumner (Hunter Strategy) <84856468+tonysumner-ccpo@users.noreply.github.com>
2023-10-20 10:18:23 -0400

    PR 2073 - Feature/at 9947 replacing prop sync (#2073)
    adding decorator PropSync

---
+++ src/decorators/custom.ts
@@ -0,0 +1,57 @@
+import { createDecorator} from 'vue-facing-decorator'
+//TODO: REFACTOR AFTER VUE 3 UPGRADE
+
+//https://morioh.com/a/9876aadb338e/vuejs-and-property-decorator#PropSync
+//https://github.com/kaorun343/vue-property-decorator/blob/master/src/decorators/PropSync.ts     
+
+export function PropSync(prefix: string) {
+  return createDecorator(function (options, key) {
+    // prototype
+    // export default {
+    //     props: {
+    //       name: {
+    //         type: String,
+    //       },
+    //     },
+    //     computed: {
+    //       syncedName: {
+    //         get() {
+    //           return this.name
+    //         },
+    //         set(value) {
+    //           this.$emit('update:name', value)
+    //         },
+    //       },
+    //     },
+    //   }
+
+    //from old propsync 
+    // export function PropSync(
+    //     propName: string,
+    //     options: PropOptions | Constructor[] | Constructor = {},
+    //   ) {
+    //     return (target: Vue, key: string) => {
+    //       applyMetadata(options, target, key)
+    //       createDecorator((componentOptions, k) => {
+    //         ;(componentOptions.props || (componentOptions.props = {} as any))[
+    //           propName
+    //         ] = options
+    //         ;(componentOptions.computed || (componentOptions.computed = {}))[k] = {
+    //           get() {
+    //             return (this as any)[propName]
+    //           },
+    //           set(this: Vue, value) {
+    //             this.$emit(`update:${propName}`, value)
+    //           },
+    //         }
+    //       })(target, key)
+    //     }
+    //   }
+
+  }, {
+    preserve: true
+  })
+}
+
+
+
