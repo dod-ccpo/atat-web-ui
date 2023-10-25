09a05f0ca3a250f9698a98bdc41fbb3532f5bc67
torinharthcock-ccpo <137221301+torinharthcock-ccpo@users.noreply.github.com>
2023-10-25 13:59:10 -0400

    Update index.ts

--- src/store/acquisitionPackageSummary/index.ts
+++ src/store/acquisitionPackageSummary/index.ts
@@ -54,7 +54,7 @@ export class AcquisitionPackageSummaryStore extends VuexModule {
     try {
       const sessionDataObject = JSON.parse(sessionData);
       Object.keys(sessionDataObject).forEach((property) => {
-        (this as any)[property] = sessionDataObject[property];
+        (this as unknown as Record<string, string>)[property] = sessionDataObject[property];
       });
     } catch (error) {
       throw new Error("error restoring session for acquisition package summary data store");
