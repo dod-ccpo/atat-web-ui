3dcd0410935e422d34ac8692fdf942cfcde3fdf4
ANTHONY SUMNER <anthony.l.sumner4.ctr@mail.mil>
2023-10-25 21:28:46 -0400

    vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,1 +1,12 @@
 
+import { defineConfig } from "vitest/config";
+import Vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [Vue()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+   root: ".", //Define the root
+});
