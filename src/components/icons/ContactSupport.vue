820513c2447f95a9bd2e63c9dcf706f1642f4afc
davidpietrzak-ccpo <david.t.pietrzak.ctr@mail.mil>
2023-10-31 11:16:26 -0500

    added toNative to exports

--- src/components/icons/ContactSupport.vue
+++ src/components/icons/ContactSupport.vue
@@ -14,5 +14,5 @@ import { Component, Prop, Vue, toNative } from "vue-facing-decorator";
 class ContactSupport extends Vue {
   @Prop({ default: "61686c", required: true }) private color!: string;
 }
-export default ContactSupport;
+export default toNative(ContactSupport)
 </script>
