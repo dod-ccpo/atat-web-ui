f66ad4d30c0a23f7eb55c42d60bfdc7fe25b4576
Tom Arnold (Hunter Strategy) <thomas.m.arnold42.ctr@mail.mil>
2023-11-01 12:34:40 -0400

    include config files

--- vite.config.js
+++ vite.config.js
@@ -157,7 +157,6 @@ export default defineConfig(({command, mode}) => {
 			//Default exclude: node_modules/, dist/, cypress/, *.config.*, **/.{idea,git,cache,output,temp} 
 			exclude: [
 				'node_modules/**',
-				'*.config.*',
 				'src/api/gInvoicing/index.spec.ts',
 				'src/api/portfolio/index.spec.ts',
 				'src/AppPackageBuilder.spec.ts',
