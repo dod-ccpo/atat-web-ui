6087a2bf7de43f6ed1e29dbd8af8c3da5ffcc1f2
Tom Arnold (Hunter Strategy) <thomas.m.arnold42.ctr@mail.mil>
2023-11-01 01:24:56 -0400

    configure vitest

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
@@ -5,6 +6,7 @@ import {checker} from 'vite-plugin-checker'
 import pkg from './package.json'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import vuetify from 'vite-plugin-vuetify'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import dotenv from 'dotenv'
@@ -53,7 +55,7 @@ export default defineConfig(({command, mode}) => {
 		plugins: [
 			VueDevTools({analyze: true}), 
 			vue(),
-
+			vuetify(),
 			Components({
 				dts: true,
 				directives: false,
@@ -121,6 +123,42 @@ export default defineConfig(({command, mode}) => {
 				}
 			}
 		},
+		test: {
+			globals: true,
+			environment: 'jsdom',
+			coverage: { 
+			  enabled: true,
+			  provider: 'v8', 
+			  clean: true,
+			  reportOnFailure: true,
+			  skipFull: true,
+			  perFile: true,
+			//   lines: 80,
+			//   functions: 80,
+			//   branches: 80,
+			//   statements: 80,
+			  reporter: ['text','html', 'lcov'],
+			  restoreMocks: true,
+			  maxConcurrency: 10,
+			  concurrent: true,
+			  typecheck: {
+				enabled: true,
+				checker: 'vue-tsc'
+			  },
+			//  bail: 10,
+
+			},
+			server: {
+			  deps: {
+				inline: ['vuetify'],
+				optimizer: 'web'
+			  }
+			},
+			root: '.',
+			ui: true,
+			//Default exclude: node_modules/, dist/, cypress/, *.config.*, **/.{idea,git,cache,output,temp} 
+			//exclude: []
+		  },
 		minify: 'esbuild',
 		commonjsOptions: {
 			esmExternals: false
