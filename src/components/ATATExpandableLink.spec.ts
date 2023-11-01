e13e7ba85351684599e4ab2054765d9a84cd68d6
Tom Arnold (Hunter Strategy) <thomas.m.arnold42.ctr@mail.mil>
2023-11-01 16:59:24 -0400

    updated test file to vitest

--- src/components/ATATExpandableLink.spec.ts
+++ src/components/ATATExpandableLink.spec.ts
@@ -1,50 +1,58 @@
-import Vue from "vue";
-import Vuetify from "vuetify";
-import { createLocalVue, mount, Wrapper } from "@vue/test-utils";
+import { describe, it, expect, vi } from 'vitest';
+import { shallowMount, VueWrapper } from "@vue/test-utils";
 import ATATExpandableLink from "@/components/ATATExpandableLink.vue";
 import { DefaultProps } from "vue/types/options";
-Vue.config.productionTip = false;
-Vue.use(Vuetify);
 
 describe("Testing ATATSelect Component", () => {
-  const localVue = createLocalVue();
-  let vuetify: Vuetify;
-  let wrapper: Wrapper<DefaultProps & Vue, Element>;
+ 
+  let wrapper:VueWrapper
   const ariaId = "MyId";
-  let expandLink: Wrapper<DefaultProps & Vue, Element>
+  let expandLink: any
 
-  beforeEach(() => {
-    vuetify = new Vuetify();
-    wrapper = mount(ATATExpandableLink, {
-      localVue,
-      vuetify,
-      propsData: {
-        ariaId,
-      }
-    });
-    expandLink = wrapper.find(".expandable-content-opener");
-  });
+  // beforeEach(() => {
+  //   vuetify = new Vuetify();
+  //   wrapper = mount(ATATExpandableLink, {
+  //     localVue,
+  //     vuetify,
+  //     propsData: {
+  //       ariaId,
+  //     }
+  //   });
+  //   
+  // });
+  
 
   describe("INITIALIZATION", () => { 
+    const wrapper:VueWrapper = shallowMount(ATATExpandableLink, {
+      props: {
+        ariaId
+      },
+      global: {
+        plugins: []
+      }
+    })
+
     it("renders successfully", async () => {
       expect(wrapper.exists()).toBe(true);
     });
-  });
+  
 
-  describe("EVENTS", () => {
-    it("test click event to expand -- link class will contain open", async () => {
-      wrapper.setData({ "isOpen": true });
-      await wrapper.vm.$nextTick();
-      expect(expandLink.classes()).toContain("open");
+    describe("EVENTS", () => {
+      it("test click event to expand -- link class will contain open", async () => {
+        (wrapper.vm as any).isOpen = true;
+        await wrapper.vm.$nextTick();
+        expandLink = wrapper.find(".expandable-content-opener");
+        console.log(expandLink)
+        expect(expandLink.classes()).toContain("open");
+      });
+
+      it("test click event to expand -- link class will contain closed", async () => {
+        (wrapper.vm as any).isOpen = true;
+        await wrapper.vm.$nextTick();
+        expandLink.trigger("keydown.enter");
+        await wrapper.vm.$nextTick();
+        expect(expandLink.classes()).toContain("closed");
+      });
     });
-
-    it("test click event to expand -- link class will contain closed", async () => {
-      wrapper.setData({ "isOpen": true });
-      await wrapper.vm.$nextTick();
-      expandLink.trigger("keydown.enter");
-      await wrapper.vm.$nextTick();
-      expect(expandLink.classes()).toContain("closed");
-    });
   });
-
 });
